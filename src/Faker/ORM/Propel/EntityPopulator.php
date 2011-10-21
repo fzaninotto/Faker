ec5d905c48b1bd875757e764ae1dcd7349e84602
Francois Zaninotto <fzaninotto@gmail.com>
2011-10-21 22:27:17 +0200

    Refactor Propel ORM populator

---
+++ src/Faker/ORM/Propel/EntityPopulator.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Faker\ORM\Propel;
+
+include __DIR__ . '/../../Guesser/Name.php';
+
+/**
+ * Service class for populating a table through a Propel ActiveRecord class.
+ */
+class EntityPopulator
+{
+	protected $class;
+	protected $columnFormatters = array();
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param string $class A Propel ActiveRecord classname
+	 */
+	public function __construct($class)
+	{
+		$this->class = $class;
+	}
+	
+	public function getClass()
+	{
+		return $this->class;
+	}
+
+	public function setColumnFormatters($columnFormatters)
+	{
+		$this->columnFormatters = array_merge($columnFormatters, $this->columnFormatters);
+	}
+
+	public function getColumnFormatters()
+	{
+		return $this->columnFormatters;
+	}
+
+	public function guessColumnFormatters(\Faker\Generator $generator)
+	{
+		$formatters = array();
+		$class = $this->class;
+		$peerClass = $class::PEER;
+		$tableMap = $peerClass::getTableMap();
+		$nameGuesser = new \Faker\Guesser\Name($generator);
+		foreach ($tableMap->getColumns() as $columnMap) {
+			if ($columnMap->isForeignKey()) {
+				$relatedClass = $columnMap->getRelation()->getForeignTable()->getPhpName();
+				$formatters[$columnMap->getPhpName()] = function($inserted) use($relatedClass) { return isset($inserted[$relatedClass]) ? $inserted[$relatedClass][mt_rand(0, count($inserted[$relatedClass]) - 1)] : null; };
+				continue;
+			}
+			if ($columnMap->isPrimaryKey()) {
+				continue;
+			}
+			if ($formatter = $nameGuesser->guessFormat($columnMap->getPhpName())) {
+				$formatters[$columnMap->getPhpName()] = $formatter;
+				continue;
+			}
+			// TODO: PropelColumnTypeGuesser
+		}
+		return $formatters;
+	}
+	
+	/**
+	 * Insert one new record using the Entity class.
+	 */
+	public function execute($con, $insertedEntities)
+	{
+		$obj = new $this->class();
+		foreach ($this->columnFormatters as $column => $format) {
+			if (null !== $column) {
+				$obj->setByName($column, is_callable($format) ? $format($insertedEntities) : $format);
+			}
+		}
+		$obj->save($con);
+		
+		return $obj->getPrimaryKey();
+	}
+
+}
--- test/test2.php
+++ test/test2.php
@@ -4,15 +4,15 @@ require_once dirname(__FILE__) . '/../../Propel/runtime/lib/Propel.php';
 set_include_path(dirname(__FILE__) . '/../../Propel/test/fixtures/bookstore/build/classes' . PATH_SEPARATOR . get_include_path());
 Propel::init(dirname(__FILE__) . '/../../Propel/test/fixtures/bookstore/build/conf/bookstore-conf.php');
 
-require_once '../src/Faker/ORM/Propel.php';
+require_once '../src/Faker/ORM/Propel/Populator.php';
 require_once '../src/Faker/Factory.php';
 
 BookQuery::create()->deleteAll();
 AuthorQuery::create()->deleteAll();
-
-$inserted = Faker\ORM\Propel::populateClasses(Faker\Factory::create(), array(
-	'Author' => 5,
-	'Book'   => 10
-));
+$generator = \Faker\Factory::create();
+$populator = new Faker\ORM\Propel\Populator($generator);
+$populator->addEntity('Author', 5);
+$populator->addEntity('Book', 10);
+$insertec = $populator->execute();
 
 echo BookQuery::create()->joinWith('Book.Author')->find();
