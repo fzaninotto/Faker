75e8ad03d5c718acdd3d8e4435ffe80a227f5708
yohang <yohan@giarelli.org>
2011-10-25 16:12:42 +0200

    Added small autoloader

---
+++ src/autoload.php
@@ -0,0 +1,15 @@
+<?php
+
+spl_autoload_register(function($className) {
+	$className = ltrim($className, '\\');
+	$fileName = '';
+	$namespace = '';
+	if ($lastNsPos = strripos($className, '\\')) {
+		$namespace = substr($className, 0, $lastNsPos);
+		$className = substr($className, $lastNsPos + 1);
+		$fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+	}
+	$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+	require __DIR__.'/'.$fileName;
+});
