ccbf8553ac50d57c6f1f5407e06e79ffeafe4310
yohang <yohan@giarelli.org>
2011-10-25 18:38:55 +0200

    Added reference to the PSR-0 standard and the SplClassLoader RFC

--- src/autoload.php
+++ src/autoload.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * Simple autoload that follow the PHP Standards Recommendation #0 (PSR-0)
+ * See https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-0.md for more informations.
+ *
+ * Code from the SplClassLoader RFC, see https://wiki.php.net/rfc/splclassloader#example_implementation
+ * 
+ */
 spl_autoload_register(function($className) {
 	$className = ltrim($className, '\\');
 	$fileName = '';
