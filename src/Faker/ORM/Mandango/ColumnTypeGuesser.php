b7f9b616b01bef94db3b4ec29ddc89e9282f1401
Christoph Schaefer <christoph.schaefer@sinntax.com>
2011-11-28 21:36:33 +0100

    ColumnTypeGuesser 32 bit platform support

--- src/Faker/ORM/Mandango/ColumnTypeGuesser.php
+++ src/Faker/ORM/Mandango/ColumnTypeGuesser.php
@@ -18,9 +18,9 @@ class ColumnTypeGuesser
 			case 'boolean':
 				return function() use ($generator) { return $generator->boolean; };
 			case 'integer':
-				return function() { return mt_rand(0,4294967295); };
+				return function() { return mt_rand(0,intval('4294967295')); };
 			case 'float':
-				return function() { return mt_rand(0,4294967295)/mt_rand(1,4294967295); };
+				return function() { return mt_rand(0,intval('4294967295'))/mt_rand(1,intval('4294967295')); };
 			case 'string':
 				return function() use ($generator) { return $generator->text(255); };
 			case 'string':
