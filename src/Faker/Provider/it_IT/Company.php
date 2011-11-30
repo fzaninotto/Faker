557b40a8218e1a418e5ff2b996da18acf38b072a
matteosister <matteog@gmail.com>
2011-11-30 01:41:52 +0100

    bs reorder

--- src/Faker/Provider/it_IT/Company.php
+++ src/Faker/Provider/it_IT/Company.php
@@ -25,14 +25,14 @@ class Company extends \Faker\Provider\Company
     );
 	
 	protected static $bsWords = array(
+        array(
+            'synergies','web-readiness','paradigms','markets','partnerships','infrastructures','platforms','initiatives','channels','eyeballs','communities','ROI','solutions','e-tailers','e-services','action-items','portals','niches','technologies','content','vortals','supply-chains','convergence','relationships','architectures','interfaces','e-markets','e-commerce','systems','bandwidth','infomediaries','models','mindshare','deliverables','users','schemas','networks','applications','metrics','e-business','functionalities','experiences','webservices','methodologies'
+        ),
 		array(
 			'implement','utilize','integrate','streamline','optimize','evolve','transform','embrace','enable','orchestrate','leverage','reinvent','aggregate','architect','enhance','incentivize','morph','empower','envisioneer','monetize','harness','facilitate','seize','disintermediate','synergize','strategize','deploy','brand','grow','target','syndicate','synthesize','deliver','mesh','incubate','engage','maximize','benchmark','expedite','reintermediate','whiteboard','visualize','repurpose','innovate','scale','unleash','drive','extend','engineer','revolutionize','generate','exploit','transition','e-enable','iterate','cultivate','matrix','productize','redefine','recontextualize'
 		),
 		array(
 			'clicks-and-mortar','value-added','vertical','proactive','robust','revolutionary','scalable','leading-edge','innovative','intuitive','strategic','e-business','mission-critical','sticky','one-to-one','24/7','end-to-end','global','B2B','B2C','granular','frictionless','virtual','viral','dynamic','24/365','best-of-breed','killer','magnetic','bleeding-edge','web-enabled','interactive','dot-com','sexy','back-end','real-time','efficient','front-end','distributed','seamless','extensible','turn-key','world-class','open-source','cross-platform','cross-media','synergistic','bricks-and-clicks','out-of-the-box','enterprise','integrated','impactful','wireless','transparent','next-generation','cutting-edge','user-centric','visionary','customized','ubiquitous','plug-and-play','collaborative','compelling','holistic','rich'
-		),
-		array(
-			'synergies','web-readiness','paradigms','markets','partnerships','infrastructures','platforms','initiatives','channels','eyeballs','communities','ROI','solutions','e-tailers','e-services','action-items','portals','niches','technologies','content','vortals','supply-chains','convergence','relationships','architectures','interfaces','e-markets','e-commerce','systems','bandwidth','infomediaries','models','mindshare','deliverables','users','schemas','networks','applications','metrics','e-business','functionalities','experiences','webservices','methodologies'
 		)
 	);
 
