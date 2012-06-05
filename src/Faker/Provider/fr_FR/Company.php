055f4e9e8bb96363cc75333ad97452ae1957efd8
Geoffrey Brier <geoffrey@widop.com>
2012-06-05 21:22:25 +0200

    Added siren and siret formatters (fr_FR)

--- src/Faker/Provider/fr_FR/Company.php
+++ src/Faker/Provider/fr_FR/Company.php
@@ -12,4 +12,22 @@ class Company extends \Faker\Provider\Company
 	);
 
 	protected static $companySuffix = array('SA', 'S.A.', 'SARL', 'S.A.R.L.', 'S.A.S.');
+
+	protected static $sirenFormat = "### ### ###";
+
+	public static function siret($maxSequentialDigits = 2)
+	{
+		if ($maxSequentialDigits > 4 || $maxSequentialDigits <= 0) {
+			$maxSequentialDigits = 2;
+		}
+
+		$sequentialNumber = str_pad(static::randomNumber($maxSequentialDigits), 4, '0', STR_PAD_LEFT);
+
+		return  static::numerify(static::siren() . ' ' . $sequentialNumber . '#');
+	}
+
+	public static function siren()
+	{
+		return static::numerify(static::$sirenFormat);
+	}
 }
