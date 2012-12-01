b0b6eb6029af2281742ff4375e945a6297f317a9
Roman Vasilyev <rsvasilyev@gmail.com>
2012-12-01 20:52:23 +0200

    Added ua_UA provider

---
+++ src/Faker/Provider/ua_UA/Company.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Faker\Provider\ua_UA;
+
+class Company extends \Faker\Provider\Base
+{
+	protected static $formats = array(
+		'{{companyName}}',                                      // Вектор
+		'{{companyPrefix}} "{{companyName}}"',                  // ТОВ "Інфоком"
+		'{{companyName}}-{{companySuffix}}"',                   // Сервіс-Плюс
+		'{{companyPrefix}} "{{companyName}}-{{companySuffix}}"',// ПАТ "Альфа-Стиль"
+	);
+
+    protected static $urlFormats = array(
+        '{{companyName}}',
+        '{{companyName}}-{{companySuffix}}',
+    );
+
+    protected static $companyPrefix = array('ТОВ', 'ПП', 'ПАТ','ПрАТ');
+	protected static $companySuffix = array('Сервіс','Плюс', 'Груп', 'Стиль', 'Дизайн');
+
+    protected static $companyName = array(
+        'Вектор', 'Едельвейс', 'Смарт', 'Альфа', 'Система', 'Універсал',
+        'Інфоком', 'Макс', 'Планета', 'Вектор', 'Приват', 'Еко', 'Мега',
+        'Мегамакс', 'Мульти', 'Майнер'
+    );
+
+    public function company()
+    {
+        $format = static::randomElement(static::$formats);
+        return $this->generator->parse($format);
+    }
+
+    public function companyUrl()
+    {
+        $format = static::randomElement(static::$urlFormats);
+        return $this->generator->parse($format);
+    }
+
+    public static function companyPrefix()
+    {
+        return static::randomElement(static::$companyPrefix);
+    }
+
+    public static function companySuffix()
+    {
+        return static::randomElement(static::$companySuffix);
+    }
+
+    public static function companyName()
+    {
+        return static::randomElement(static::$companyName);
+    }
+}
