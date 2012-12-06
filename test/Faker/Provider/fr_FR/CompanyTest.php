583092c394ff61ef62709f2678e53c363f322443
Alexandre Segura <mex.zktk@gmail.com>
2012-12-06 21:16:26 +0100

    Custom SIRET implementation, compliant with $maxSequentialDigits Added PHPUnit Constraint to validate a SIREN Better implementation of Luhn algorithm check

--- test/Faker/Provider/fr_FR/CompanyTest.php
+++ test/Faker/Provider/fr_FR/CompanyTest.php
@@ -7,31 +7,65 @@ use Faker\Provider\fr_FR\Company;
 /**
  * @see http://www.pixelastic.com/blog/13:validation-et-generation-de-numero-de-siret
  * @see http://fr.wikipedia.org/wiki/Syst%C3%A8me_d%E2%80%99identification_du_r%C3%A9pertoire_des_%C3%A9tablissements
+ * @see http://fr.wikipedia.org/wiki/SIREN
  */
-class IsValidSiret extends \PHPUnit_Framework_Constraint
+
+abstract class IsValidSirenSiret extends \PHPUnit_Framework_Constraint 
 {
 	
-    protected function matches($other)
+	protected function matches($other)
     {
     	
-    	$siret = str_replace(' ', '', $other);
+    	$code = str_replace(' ', '', $other);
     	
-    	if (strlen($siret) != 14)
+    	if (strlen($code) != $this->getLength())
     		return false;
 
     	$sum = 0;
-		for ($i = 0; $i < 14; $i++) {
-			$isEven = (($i % 2) === 0);
-			$tmp = $isEven ? $siret[$i] * 2 : $siret[$i];
-			if ($tmp >= 10) $tmp -= 9;
+    	// IMPORTANT : from right to left
+    	for ($i = strlen($code) - 1; $i >= 0; $i--) {
+    		$isEven = (($i % 2) === 0);
+    		$tmp = $isEven ? $code[$i] * 2 : $code[$i];
+    		if ($tmp >= 10) $tmp -= 9;
 			$sum += $tmp;
-		}
-		return ($sum % 10 === 0);
+    	}
+    	return ($sum % 10 === 0);
+    	
     }
     
     public function toString() {
-    	return 'is a valid SIRET number';
+    	return sprintf('is a valid %s number', $this->getName());
     }
+    
+    abstract protected function getLength();
+    
+    abstract protected function getName();
+
+}
+
+class IsValidSiret extends IsValidSirenSiret
+{
+	
+	protected function getLength() {
+		return 14;
+	}
+    
+	protected function getName() {
+		return 'SIRET';
+	}
+    
+}
+
+class IsValidSiren extends IsValidSirenSiret
+{
+	
+	protected function getLength() {
+		return 9;
+	}
+    
+	protected function getName() {
+		return 'SIREN';
+	}
     
 }
 
@@ -43,7 +77,12 @@ class CompanyTest extends \PHPUnit_Framework_TestCase
         return new IsValidSiret();
     }
 	
-	public function testParagraphWithNegativeNbDigitsReturnsAWellFormattedSiret()
+	private static function isValidSiren()
+    {
+        return new IsValidSiren();
+    }
+    
+	public function testParagraphWithNegativeNbDigitsReturnsAValidAndWellFormattedSiret()
 	{
 		$siret = Company::siret(-1);
 		
@@ -51,11 +90,20 @@ class CompanyTest extends \PHPUnit_Framework_TestCase
 		$this->assertRegExp("/[\d]{3} [\d]{3} [\d]{3} 00[\d]{3}/", $siret);
 	}
 
-	public function testParagraphWithInvalidNbDigitsReturnsAWellFormattedSiret()
+	public function testParagraphWithInvalidNbDigitsReturnsAValidAndWellFormattedSiret()
 	{
 		$siret = Company::siret(6);
-
+		
+		$this->assertThat($siret, self :: isValidSiret());
 		$this->assertRegExp("/[\d]{3} [\d]{3} [\d]{3} 00[\d]{3}/", $siret);
+	}
+	
+	public function testSirenReturnsAValidAndWellFormattedSiren()
+	{
+		$siret = Company::siren();
+		
+		$this->assertThat($siret, self :: isValidSiren());
+		$this->assertRegExp("/[\d]{3} [\d]{3} [\d]{3}/", $siret);
 	}
 
 	public function testParagraphWithValidNbDigitsReturnsAWellFormattedSiret()
