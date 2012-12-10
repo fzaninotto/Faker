1f638a8947e00b4fe1e4a877c91460731b75079b
Ferid Mövsümov <faridmovsumov@gmail.com>
2012-12-10 16:37:44 +0200

    Fixed problems

--- src/Faker/Provider/tr_TR/PhoneNumber.php
+++ src/Faker/Provider/tr_TR/PhoneNumber.php
@@ -5,12 +5,4 @@ namespace Faker\Provider\tr_TR;
 class PhoneNumber extends \Faker\Provider\PhoneNumber
 {
 	protected static $formats = array('0##########','05#########');
-
-	/**
-	 * @example '05544397503'
-	 */
-	public static function phoneNumber()
-	{
-		return static::numerify(static::randomElement(static::$formats));
-	}
 }
