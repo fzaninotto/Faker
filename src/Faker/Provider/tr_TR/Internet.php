a35ab4064aa73e847b79de21417a55cbe9bd1f9a
Francois Zaninotto <fzaninotto@gmail.com>
2012-12-10 16:15:00 +0100

    Fix cs in previous commit

--- src/Faker/Provider/tr_TR/Internet.php
+++ src/Faker/Provider/tr_TR/Internet.php
@@ -4,5 +4,5 @@ namespace Faker\Provider\tr_TR;
 
 class Internet extends \Faker\Provider\Internet
 {
-	protected static $tld = array('com', 'com', 'com', 'com', 'com', 'com.tr', 'biz', 'info', 'net', 'org','az');
+    protected static $tld = array('com', 'com', 'com', 'com', 'com', 'com.tr', 'biz', 'info', 'net', 'org','az');
 }
--- src/Faker/Provider/tr_TR/PhoneNumber.php
+++ src/Faker/Provider/tr_TR/PhoneNumber.php
@@ -4,5 +4,5 @@ namespace Faker\Provider\tr_TR;
 
 class PhoneNumber extends \Faker\Provider\PhoneNumber
 {
-	protected static $formats = array('0##########','05#########');
+    protected static $formats = array('0##########','05#########');
 }
