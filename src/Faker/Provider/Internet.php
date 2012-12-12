a52a44508bbed675f549e9681785b52e27fad7c3
Francois Zaninotto <fzaninotto@gmail.com>
2012-12-12 09:00:07 +0100

    Fix issue in ipv4 formatter on 32 platforms
    Patch from @pokmot.
    
    Closes #93

--- src/Faker/Provider/Internet.php
+++ src/Faker/Provider/Internet.php
@@ -119,7 +119,7 @@ class Internet extends \Faker\Provider\Base
      */
     public function ipv4()
     {
-        return long2ip(mt_rand(-2147483648, 2147483647));
+        return long2ip(mt_rand(0, 1) == 0 ? mt_rand(-2147483648, 0) : mt_rand(1, 2147483647));
     }
 
     /**
