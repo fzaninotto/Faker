91e87aa0e089e285fa94c5a5b4a3a0b351fe9ccc
Alexandre Segura <mex.zktk@gmail.com>
2012-12-18 20:37:52 +0100

    Company Provider for french locale now returns valid SIREN/SIRET codes which verify the Luhn algorithm. Added PHPUnit constraint to assert that a string is a valid code.

---
+++ test/Faker/PHPUnit/Framework/Constraint/IsValidSiren.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Faker\PHPUnit\Framework\Constraint;
+
+class IsValidSiren extends IsValidSirenSiret
+{
+	
+	protected function getLength() {
+		return 9;
+	}
+    
+	protected function getName() {
+		return 'SIREN';
+	}
+    
+}
--- test/Faker/Provider/fr_FR/CompanyTest.php
+++ test/Faker/Provider/fr_FR/CompanyTest.php
@@ -3,20 +3,37 @@
 namespace Faker\Test\Provider\fr_FR;
 
 use Faker\Provider\fr_FR\Company;
+use Faker\PHPUnit\Framework\Constraint as Constraint;
+
+require_once __DIR__ . '/../../PHPUnit/Framework/Constraint/IsValidSirenSiret.php';
+require_once __DIR__ . '/../../PHPUnit/Framework/Constraint/IsValidSiret.php';
+require_once __DIR__ . '/../../PHPUnit/Framework/Constraint/IsValidSiren.php';
 
 class CompanyTest extends \PHPUnit_Framework_TestCase
 {
+    private static function isValidSiret()
+    {
+        return new Constraint\IsValidSiret();
+    }
+    
+    private static function isValidSiren()
+    {
+        return new Constraint\IsValidSiren();
+    }
+    
     public function testParagraphWithNegativeNbDigitsReturnsAWellFormattedSiret()
     {
         $siret = Company::siret(-1);
-
+        
+        $this->assertThat($siret, self :: isValidSiret());
         $this->assertRegExp("/[\d]{3} [\d]{3} [\d]{3} 00[\d]{3}/", $siret);
     }
 
     public function testParagraphWithInvalidNbDigitsReturnsAWellFormattedSiret()
     {
         $siret = Company::siret(6);
-
+        
+        $this->assertThat($siret, self :: isValidSiret());
         $this->assertRegExp("/[\d]{3} [\d]{3} [\d]{3} 00[\d]{3}/", $siret);
     }
 
@@ -26,11 +43,23 @@ class CompanyTest extends \PHPUnit_Framework_TestCase
         $siret2 = Company::siret(2);
         $siret3 = Company::siret(3);
         $siret4 = Company::siret(4);
-
+        
+        $this->assertThat($siret1, self :: isValidSiret());
         $this->assertRegExp("/[\d]{3} [\d]{3} [\d]{3} 000[\d]{2}/", $siret1);
+        $this->assertThat($siret2, self :: isValidSiret());
         $this->assertRegExp("/[\d]{3} [\d]{3} [\d]{3} 00[\d]{3}/", $siret2);
+        $this->assertThat($siret3, self :: isValidSiret());
         $this->assertRegExp("/[\d]{3} [\d]{3} [\d]{3} 0[\d]{4}/", $siret3);
+        $this->assertThat($siret4, self :: isValidSiret());
         $this->assertRegExp("/[\d]{3} [\d]{3} [\d]{3} [\d]{5}/", $siret4);
+    }
+    
+    public function testSirenReturnsAValidAndWellFormattedSiren()
+    {
+        $siret = Company::siren();
+        
+        $this->assertThat($siret, self :: isValidSiren());
+        $this->assertRegExp("/[\d]{3} [\d]{3} [\d]{3}/", $siret);
     }
 
     public function testCatchPhraseValidationReturnsFalse()
