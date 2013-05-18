9ef7d037615e115b71ce9b6cca9e988253facc3e
Mark van der Velden <mark@dynom.nl>
2013-05-18 17:39:10 +0200

    Fixing the test.

--- test/Faker/Provider/nl_NL/PersonTest.php
+++ test/Faker/Provider/nl_NL/PersonTest.php
@@ -8,41 +8,39 @@ namespace Faker\Test\Provider\nl_NL;
 class PersonTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * Return the nl_NL provider
-     *
-     * @return \Faker\Provider\nl_NL\Person
+     * @var \Faker\Generator
      */
-    private function getProvider()
+    private $faker;
+
+    public function setUp()
     {
         $faker = new \Faker\Generator();
-        return new \Faker\Provider\nl_NL\Person($faker);
-    }
+        $faker->addProvider(new \Faker\Provider\nl_NL\Person($faker));
 
+        $this->faker = $faker;
+    }
 
     public function testGeneratingFirstNames()
     {
-        $provider = $this->getProvider();
         for ($i=0; $i<50; $i++)
         {
-            $this->assertInternalType('string', $provider->firstName());
+            $this->assertInternalType('string', $this->faker->firstName());
         }
     }
 
     public function testGeneratingLastNames()
     {
-        $provider = $this->getProvider();
         for ($i=0; $i<50; $i++)
         {
-            $this->assertInternalType('string', $provider->lastName());
+            $this->assertInternalType('string', $this->faker->lastName());
         }
     }
 
     public function testGeneratingNames()
     {
-        $provider = $this->getProvider();
         for ($i=0; $i<50; $i++)
         {
-            $this->assertInternalType('string', $provider->name());
+            $this->assertInternalType('string', $this->faker->name());
         }
     }
 }
