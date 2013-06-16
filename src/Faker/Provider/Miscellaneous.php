2a786fb43c9bcd39c07e331a0a6fdaf2c415e081
lsv <martin.aarhof@gmail.com>
2013-06-17 00:20:30 +0200

    Added hexcolor, rgbcolor and rgbColorAsArray Changed readme, with the 3 new methods Added 3 new tests

--- src/Faker/Provider/Miscellaneous.php
+++ src/Faker/Provider/Miscellaneous.php
@@ -148,4 +148,35 @@ class Miscellaneous extends \Faker\Provider\Base
     {
         return static::randomElement(static::$languageCode);
     }
+
+	/**
+	 * @example '
+	 */
+	public static function hexColor()
+	{
+		$hexcodes = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f');
+		return sprintf('#%s%s%s%s%s%s',
+			static::randomElement($hexcodes),
+			static::randomElement($hexcodes),
+			static::randomElement($hexcodes),
+			static::randomElement($hexcodes),
+			static::randomElement($hexcodes),
+			static::randomElement($hexcodes)
+		);
+	}
+
+	public static function rgbColorAsArray()
+	{
+		$rgb = array();
+		$color = str_replace('#', '', static::hexColor());
+		$rgb[] = hexdec(substr($color,0,2));
+		$rgb[] = hexdec(substr($color,2,2));
+		$rgb[] = hexdec(substr($color,4,2));
+		return $rgb;
+	}
+
+	public static function rgbColor()
+	{
+		return implode(',', static::rgbColorAsArray());
+	}
 }
