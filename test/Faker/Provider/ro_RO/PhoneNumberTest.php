8b4c0cdb4e939888b1cb177643c1e674cae7074f
Mihai Zaharie <mihai@zaharie.ro>
2014-01-05 14:11:35 +0200

    Add phone number (landline/mobile, toll-free, premium-rate) formatters

---
+++ test/Faker/Provider/ro_RO/PhoneNumberTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Faker\Test\Provider\ro_RO;
+
+use Faker\Generator;
+use Faker\Provider\ro_RO\PhoneNumber;
+
+class PhoneNumberTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $faker = new Generator();
+        $faker->addProvider(new PhoneNumber($faker));
+        $this->faker = $faker;
+    }
+
+    public function testPhoneNumberReturnsNormalPhoneNumber()
+    {
+        $this->assertRegExp('/^0(?:[23][13-7]|7\d)\d{7}$/', $this->faker->phoneNumber());
+    }
+
+    public function testTollFreePhoneNumberReturnsTollFreePhoneNumber()
+    {
+        $this->assertRegExp('/^08(?:0[1267]|70)\d{6}$/', $this->faker->tollFreePhoneNumber());
+    }
+
+    public function testPremiumRatePhoneNumberReturnsPremiumRatePhoneNumber()
+    {
+        $this->assertRegExp('/^090[036]\d{6}$/', $this->faker->premiumRatePhoneNumber());
+    }
+}
