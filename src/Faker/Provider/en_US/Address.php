a9634623ded6f59864e5f66d4b18a46b2a3c27c8
Jonathan Garbee <jonathan@garbee.me>
2014-01-05 08:11:31 -0500

    Update Address.php
    Updated en_US provider to use ISO 3166-2 codes.

--- src/Faker/Provider/en_US/Address.php
+++ src/Faker/Provider/en_US/Address.php
@@ -15,7 +15,7 @@ class Address extends \Faker\Provider\Address
         'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'NewHampshire', 'NewJersey', 'NewMexico', 'NewYork', 'NorthCarolina', 'NorthDakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'RhodeIsland', 'SouthCarolina', 'SouthDakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'WestVirginia', 'Wisconsin', 'Wyoming'
     );
     protected static $stateAbbr = array(
-        'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY', 'AE', 'AA', 'AP'
+       'AS', 'GA', 'KS', 'MN', 'TX', 'UM', 'AK', 'AL', 'CO', 'KY', 'MP', 'NY', 'RI', 'SC', 'SD', 'WA', 'DC', 'GU', 'IA', 'OH', 'OR', 'CA', 'DE', 'HI', 'MA', 'MT', 'NC', 'NE', 'NJ', 'VI', 'CT', 'FL', 'IN', 'OK', 'UT', 'WI', 'WV', 'ID', 'IL', 'MD', 'ME', 'MO', 'MS', 'NM', 'PA', 'VT', 'WY', 'AZ', 'LA', 'ND', 'NH', 'NV', 'VA', 'AR', 'MI', 'PR', 'TN'
     );
     protected static $country = array(
         'Afghanistan', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antarctica (the territory South of 60 deg S)', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan',
