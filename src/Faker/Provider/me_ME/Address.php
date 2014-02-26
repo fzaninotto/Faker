ca14c7ba6b591642cb4adbbc1d5eb0fa72695c88
ognjen miletic <ognjen.miletic@gmail.com>
2014-02-26 11:23:00 +0100

    latitude and Longitude added

--- src/Faker/Provider/me_ME/Address.php
+++ src/Faker/Provider/me_ME/Address.php
@@ -108,4 +108,21 @@ class Address extends \Faker\Provider\Address
     {
         return static::randomElement(static::$cityNames);
     }
+
+    /**
+     * @example '77.147489'
+     */
+    public static function latitude()
+    {
+        return number_format(mt_rand(42430000, 42450000)/1000000, 6);
+    }
+
+    /**
+     * @example '86.211205'
+     */
+    public static function longitude()
+    {
+        return number_format(mt_rand(19260000, 19270000)/1000000, 6);
+    }
+
 }
