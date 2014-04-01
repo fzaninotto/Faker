40d8116e26518b6dbe386f58ce2c5fc9c7a73bb9
Bjorn Theart <bjorn@double-eye.com>
2014-04-01 11:07:36 +0200

    fix typo in en_ZA Internet provider

--- src/Faker/Provider/en_ZA/Internet.php
+++ src/Faker/Provider/en_ZA/Internet.php
@@ -5,5 +5,5 @@ namespace Faker\Provider\en_ZA;
 class Internet extends \Faker\Provider\Internet
 {
     protected static $freeEmailDomain = array('gmail.com', 'yahoo.com', 'hotmail.com', 'webmail.co.za', 'vodamail.co.za');
-    protected static $tld = array('co.za', 'co.za', 'co.za', '.co.za', 'com', 'com', 'net');
+    protected static $tld = array('co.za', 'co.za', 'co.za', 'co.za', 'com', 'com', 'net');
 }
