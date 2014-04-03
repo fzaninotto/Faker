c7efb355823627d8060af9df6912ab2a8a359a17
cinan <cinan6@gmail.com>
2014-04-03 13:10:02 +0200

    FIX: invalid email domain

--- src/Faker/Provider/sk_SK/Internet.php
+++ src/Faker/Provider/sk_SK/Internet.php
@@ -5,7 +5,7 @@ namespace Faker\Provider\sk_SK;
 class Internet extends \Faker\Provider\Internet
 {
 
-    protected static $freeEmailDomain = array('gmail.com', 'yahoo.com', 'zoznam.sk', 'atlas.', 'centrum.sk', 'azet.sk', 'post.sk');
+    protected static $freeEmailDomain = array('gmail.com', 'yahoo.com', 'zoznam.sk', 'atlas.sk', 'centrum.sk', 'azet.sk', 'post.sk');
 
     protected static $tld = array('sk', 'sk', 'sk', 'sk', 'sk', 'sk', 'eu', 'com', 'info', 'net', 'org');
 
