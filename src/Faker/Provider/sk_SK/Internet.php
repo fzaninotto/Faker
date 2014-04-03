123b2484704a5fa3864ab79d3c961add9c22fa33
cinan <cinan6@gmail.com>
2014-04-03 13:07:31 +0200

    Missing some letters containing diacritics
    Letter 'ľ' and 'Ľ' were missing

--- src/Faker/Provider/sk_SK/Internet.php
+++ src/Faker/Provider/sk_SK/Internet.php
@@ -10,14 +10,15 @@ class Internet extends \Faker\Provider\Internet
     protected static $tld = array('sk', 'sk', 'sk', 'sk', 'sk', 'sk', 'eu', 'com', 'info', 'net', 'org');
 
     /**
-     * Converts czech characters to their ASCII representation
+     * Converts czech and slovak characters to their ASCII representation
      *
+     * @param string
      * @return string
      */
     private function toAscii($string)
     {
-        $from = array('Ě', 'ě', 'Š', 'š', 'Č', 'č', 'Ř', 'ř', 'Ž', 'ž', 'Ý', 'ý', 'Á', 'á', 'Í', 'í', 'É', 'é', 'Ó', 'ó', 'Ú', 'ú', 'Ů', 'ů', 'Ď', 'ď', 'Ť', 'ť', 'Ň', 'ň');
-        $to		= array('E', 'e', 'S', 's', 'C', 'c', 'R', 'r', 'Z', 'z', 'Y', 'y', 'A', 'a', 'I', 'i', 'E', 'e', 'O', 'o', 'U', 'u', 'U', 'u', 'D', 'd', 'T', 't', 'N', 'n');
+        $from = array('Ě', 'ě', 'Š', 'š', 'Č', 'č', 'Ř', 'ř', 'Ž', 'ž', 'Ý', 'ý', 'Á', 'á', 'Í', 'í', 'É', 'é', 'Ó', 'ó', 'Ú', 'ú', 'Ů', 'ů', 'Ď', 'ď', 'Ť', 'ť', 'Ň', 'ň', 'ľ', 'Ľ');
+        $to	= array('E', 'e', 'S', 's', 'C', 'c', 'R', 'r', 'Z', 'z', 'Y', 'y', 'A', 'a', 'I', 'i', 'E', 'e', 'O', 'o', 'U', 'u', 'U', 'u', 'D', 'd', 'T', 't', 'N', 'n', 'l', 'l');
 
         return str_replace($from, $to, $string);
     }
