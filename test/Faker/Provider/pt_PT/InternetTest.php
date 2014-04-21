b00358942f3c162ba9dc327cf0fcf6f95123d401
Hugo Fonseca <hugo.fonseca@beubi.com>
2014-04-21 22:58:40 +0100

    added tests for Internet , add pt_pt Person , add pt_pt Internet and tests

---
+++ test/Faker/Provider/pt_PT/InternetTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Faker\Test\Provider\pt_PT;
+
+use Faker\Generator;
+use Faker\Provider\pt_PT\Person;
+use Faker\Provider\pt_PT\Internet;
+use Faker\Provider\Company;
+
+class InternetTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+        $faker = new Generator();
+        $faker->addProvider(new Person($faker));
+        $faker->addProvider(new Internet($faker));
+        $faker->addProvider(new Company($faker));
+        $this->faker = $faker;
+    }
+
+    /**
+     * @link http://stackoverflow.com/questions/12026842/how-to-validate-an-email-address-in-php
+     */
+    public function testEmailIsValid()
+    {
+        $pattern = '/^(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){255,})(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){65,}@)(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22))(?:\\.(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-+[a-z0-9]+)*\\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-+[a-z0-9]+)*)|(?:\\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\\]))$/iD';
+        $emailaddress = $this->faker->email();
+        $this->assertSame(preg_match($pattern, $emailaddress), 1, $emailaddress);
+    }
+    public function testUsernameIsValid()
+    {
+        $pattern = '/^[A-Za-z0-9_.]+$/';
+        $emailaddress = $this->faker->username();
+        $this->assertSame(preg_match($pattern, $emailaddress), 1, $emailaddress);
+    }
+}
