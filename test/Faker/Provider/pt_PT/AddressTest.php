8dcc6528596fbdc350f0afb2ebcb8f25cce96455
Hugo Fonseca <hugo.fonseca@beubi.com>
2014-04-23 22:39:05 +0100

    added address for pt_PT and fixed name for pt_PT

---
+++ test/Faker/Provider/pt_PT/AddressTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Faker\Test\Provider\pt_PT;
+
+use Faker\Generator;
+use Faker\Provider\pt_PT\Address;
+
+class AddressTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var Generator
+     */
+    private $faker;
+    
+    public function setUp()
+    {
+        $faker = new Generator();
+        $faker->addProvider(new Address($faker));
+        $this->faker = $faker;
+    }
+
+    public function testPostCodeIsValid()
+    {
+        $main = '[1-9]{1}[0-9]{2}[0,1,4,5,9]{1}';
+        $pattern = "/^($main)|($main-[0-9]{3})+$/";
+        $postcode = $this->faker->postcode();
+        $this->assertSame(preg_match($pattern, $postcode), 1, $postcode);
+    }
+}
