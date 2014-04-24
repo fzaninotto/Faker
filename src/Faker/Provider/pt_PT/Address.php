d0d712a0d6e1c42fecb07bac09b8fb4af331f3b0
Hugo Fonseca <hugo.fonseca@beubi.com>
2014-04-24 11:08:35 +0100

    added links to pt_PT adress

--- src/Faker/Provider/pt_PT/Address.php
+++ src/Faker/Provider/pt_PT/Address.php
@@ -20,10 +20,12 @@ class Address extends \Faker\Provider\Address
         '{{streetName}}, {{buildingNumber}}, {{secondaryAddress}}',
     );
 
+    /** @link http://www.univ-ab.pt/PINTAC/carta_normas.htm address example in letters **/
     protected static $addressFormats = array(
         "{{streetAddress}}\n{{postcode}} {{city}}",
     );
 
+    /** @link http://www.mapadeportugal.net/indicecidades.asp **/
     private static $cities = array(
         'Abrantes', 'Agualva-Cacém', 'Águeda', 'Albufeira', 'Alcácer do Sal', 'Alcobaça', 'Almada', 'Almeirim', 'Alverca do Ribatejo', 'Amadora', 'Amarante', 'Amora', 'Anadia', 'Angra do Heroísmo', 'Aveiro', 'Barcelos', 'Barreiro',
         'Beja', 'Braga', 'Bragança', 'Caldas da Rainha', 'Camara de Lobos', 'Cantanhede', 'Cartaxo', 'Castelo Branco', 'Chaves', 'Coimbra', 'Covilhã', 'Elvas', 'Entroncamento', 'Ermesinde', 'Esmoriz', 'Espinho', 'Esposende', 'Estarreja',
@@ -59,7 +61,8 @@ class Address extends \Faker\Provider\Address
     {
         return static::numerify(static::randomElement(static::$secondaryAddressFormats));
     }
-    
+
+    /** @link http://www.indexmundi.com/pt/ **/
     protected static $country = array(
         'Afeganistão', 'África do Sul', 'Albânia', 'Alemanha', 'Andorra',
         'Angola', 'Antigua e Barbuda', 'Arabia Saudita', 'Argélia',
