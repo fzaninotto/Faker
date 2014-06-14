15289c83f359d9d0dbe56aae3bb9c3b18e451d34
zaid-86 <zaid@wewebit.com>
2014-06-14 15:46:57 +0300

    reverting change of test.php

--- test/test.php
+++ test/test.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__ .'/../vendor/autoload.php';
 
-$faker = Faker\Factory::create('ar_JO');
+$faker = Faker\Factory::create();
 $faker->seed(5);
 
 echo '<?xml version="1.0" encoding="UTF-8"?>';
