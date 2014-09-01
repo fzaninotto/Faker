33bbb9bc78fc3f33eeab8a2e4467b4fc6b71da02
Francois Zaninotto <fzaninotto@gmail.com>
2014-09-01 17:20:05 +0200

    Add Luhn util class

---
+++ src/Faker/Util/Luhn.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Faker\Util;
+
+/**
+ * Utility class for generating Luhn checksum and validating a number
+ *
+ * Luhn algorithm is used to validate credit card numbers, IMEI numbers, and
+ * National Provider Identifier numbers.
+ *
+ * @see http://en.wikipedia.org/wiki/Luhn_algorithm
+ */
+class Luhn
+{
+    /**
+     * @return int
+     */
+    private static function checksum($number) 
+    {
+        $sum = 0;
+        foreach (str_split(strrev((string) $number)) as $i => $digit) {
+            if ($i % 2 !== 0) {
+                $digit = $digit * 2;
+                if ($digit > 9) {
+                    $digit = (int) ($digit / 10) + $digit % 10;
+                }
+            }
+            $sum += $digit;
+        }
+
+        return $sum % 10;
+    }
+
+    /**
+     * @return string
+     */
+    public static function computeCheckDigit($partialNumber)
+    {
+        $checkDigit = self::checksum($partialNumber * 10);
+        if ($checkDigit === 0) {
+            return 0;
+        }
+
+        return (string) 10 - $checkDigit;
+    }
+
+    /**
+     * Checks whether a number (partial number + check digit) is Luhn compliant
+     *
+     * @return boolean
+     */
+    public static function isValid($number)
+    {
+        return self::checksum($number) === 0;
+    }
+}
---
+++ test/Faker/Util/LuhnTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Faker\Test\Util;
+
+use Faker\Util\Luhn;
+
+class LuhnTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function checkDigitProvider()
+    {
+        return [
+            ['7992739871', '3'],
+            ['3852000002323', '7'],
+            ['37144963539843', '1'],
+            ['561059108101825', '0'],
+            ['601100099013942', '4'],
+            ['510510510510510', '0']
+        ];
+    }
+
+    /**
+     * @dataProvider checkDigitProvider
+     */
+    public function testComputeCheckDigit($partialNumber, $checkDigit)
+    {
+        $this->assertEquals($checkDigit, Luhn::computeCheckDigit($partialNumber));
+    }
+
+    public function validatorProvider()
+    {
+        return [
+            ['79927398710', false],
+            ['79927398711', false],
+            ['79927398712', false],
+            ['79927398713', true],
+            ['79927398714', false],
+            ['79927398715', false],
+            ['79927398716', false],
+            ['79927398717', false],
+            ['79927398718', false],
+            ['79927398719', false]
+        ];
+    }
+
+    /**
+     * @dataProvider validatorProvider
+     */
+    public function testIsValid($number, $isValid)
+    {
+        $this->assertEquals($isValid, Luhn::isValid($number));
+    }
+}
