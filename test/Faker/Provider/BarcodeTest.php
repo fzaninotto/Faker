7ee62100bfc79baffdb899991f7e94ef24e203f5
Francois Zaninotto <fzaninotto@gmail.com>
2014-09-01 15:20:02 +0200

    Improve barcode formatter speed, add tests for it

--- test/Faker/Provider/BarcodeTest.php
+++ test/Faker/Provider/BarcodeTest.php
@@ -7,20 +7,39 @@ use Faker\Provider\Barcode;
 
 class BarcodeTest extends \PHPUnit_Framework_TestCase
 {
+    private $faker;
+
     public function setUp()
     {
         $faker = new Generator();
         $faker->addProvider(new Barcode($faker));
+        $faker->seed(0);
         $this->faker = $faker;
     }
 
     public function testEan8()
     {
-        $this->assertRegExp('/[\d]{8}/', $this->faker->ean8());
+        $code = $this->faker->ean8();
+        $this->assertRegExp('/^\d{8}$/i', $code);
+        $codeWitoutChecksum = substr($code, 0, -1);
+        $checksum = substr($code, -1);
+        $this->assertEquals(TestableBarcode::eanChecksum($codeWitoutChecksum), $checksum);
     }
 
     public function testEan13()
     {
-        $this->assertRegExp('/[\d]{13}/', $this->faker->ean13());
+        $code = $this->faker->ean13();
+        $this->assertRegExp('/^\d{13}$/i', $code);
+        $codeWitoutChecksum = substr($code, 0, -1);
+        $checksum = substr($code, -1);
+        $this->assertEquals(TestableBarcode::eanChecksum($codeWitoutChecksum), $checksum);
+    }
+}
+
+class TestableBarcode extends Barcode
+{
+    public static function eanChecksum($input)
+    {
+        return parent::eanChecksum($input);
     }
 }
