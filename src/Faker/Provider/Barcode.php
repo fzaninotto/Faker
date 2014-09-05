d97a9bfb1f2dc1c00fa001111b917692b3916f91
Matt Lunsford <TheLoneUs@users.noreply.github.com>
2014-09-05 14:26:45 -0700

    Formatting fixes.

--- src/Faker/Provider/Barcode.php
+++ src/Faker/Provider/Barcode.php
@@ -5,9 +5,11 @@ namespace Faker\Provider;
 /**
  * @see http://en.wikipedia.org/wiki/EAN-13
  */
-class Barcode extends \Faker\Provider\Base {
+class Barcode extends \Faker\Provider\Base
+{
 
-    private function ean($length = 13) {
+    private function ean($length = 13)
+    {
         $code = '';
         for( $i=0; $i<$length-1; $i++ ){
             $code .= mt_rand(0,9);
@@ -19,7 +21,8 @@ class Barcode extends \Faker\Provider\Base {
     /**
      * Utility function for computing EAN checksums
      */
-    protected static function eanChecksum($input) {
+    protected static function eanChecksum($input)
+    {
         $sequence = (strlen($input) + 1) == 8 ? array(3, 1) : array(1, 3);
         $sums = 0;
         foreach (str_split($input) as $n => $digit) {
@@ -28,7 +31,8 @@ class Barcode extends \Faker\Provider\Base {
         return (10 - $sums % 10) % 10;
     }
 
-    private function code39generate($length) {
+    private function code39generate($length)
+    {
         $code39chars = str_split('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789$/+%*',1); // allowed code39 characters
 
         $barcode = '';
@@ -44,7 +48,8 @@ class Barcode extends \Faker\Provider\Base {
      * @return string
      * @example '4006381333931'
      */
-    public function ean13() {
+    public function ean13()
+    {
         return $this->ean(13);
     }
 
@@ -53,7 +58,8 @@ class Barcode extends \Faker\Provider\Base {
      * @return string
      * @example '73513537'
      */
-    public function ean8() {
+    public function ean8()
+    {
         return $this->ean(8);
     }
 
@@ -62,7 +68,8 @@ class Barcode extends \Faker\Provider\Base {
      * @return string
      * @example '4C567DKLH3'
      */
-    public function code39($length = 10) {
+    public function code39($length = 10)
+    {
         if (!is_numeric($length)) {
             $length = 10;
         }
