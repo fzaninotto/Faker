15296ed731ba8b94a9189cc42beadb250a1149e5
Ankit Pokhrel <ankitpokhrel@gmail.com>
2014-09-06 17:39:57 +0545

    Added Nepali Provider

---
+++ src/Faker/Provider/ne_NP/PhoneNumber.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Faker\Provider\en_US;
+
+class PhoneNumber extends \Faker\Provider\PhoneNumber
+{
+    protected static $formats = array(
+        '01-4######',
+        '01-5######',
+        '01-6######',
+        '9841######',
+        '98510#####',
+        '9803######',
+        '9808######',
+        '9813######',
+        '9818######',
+    );
+}
