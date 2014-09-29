b88345c1f2e5e200e6eceaf6124226c35292bb86
JouniM <jouni@twinkle.fi>
2014-09-29 14:12:30 +0300

    ASCII-7 chars to email only. (fix to previous commit)

--- src/Faker/Provider/fi_FI/Internet.php
+++ src/Faker/Provider/fi_FI/Internet.php
@@ -7,6 +7,19 @@ class Internet extends \Faker\Provider\Internet
     protected static $freeEmailDomain = array('gmail.com', 'yahoo.com', 'hotmail.com', 'jippii.fi', 'luukku.com');
     protected static $tld = array('com', 'com', 'com', 'com', 'com', 'com', 'biz', 'info', 'net', 'org', 'fi');
 
+	/**
+	 * Converts czech characters to their ASCII representation
+	 * @param string $string
+	 * @return string
+	 */
+	private function toAscii($string)
+	{
+		$from = array('?', '?', '?', '?', '?', '?', '?', '?', '?', '?', 'Ý', 'ý', 'Á', 'á', 'Í', 'í', 'É', 'é', 'Ó', 'ó', 'Ú', 'ú', '?', '?', '?', '?', '?', '?', '?', '?');
+		$to = array('E', 'e', 'S', 's', 'C', 'c', 'R', 'r', 'Z', 'z', 'Y', 'y', 'A', 'a', 'I', 'i', 'E', 'e', 'O', 'o', 'U', 'u', 'U', 'u', 'D', 'd', 'T', 't', 'N', 'n');
+
+		return str_replace($from, $to, $string);
+	}
+
 	public function email()
 	{
 		return $this->toAscii(parent::email());
