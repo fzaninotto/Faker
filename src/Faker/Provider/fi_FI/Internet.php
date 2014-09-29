9b73b890e170bc50f5a3af0a57605b225313be4e
JouniM <jouni@twinkle.fi>
2014-09-29 17:02:21 +0300

    code to  conform psr-2

--- src/Faker/Provider/fi_FI/Internet.php
+++ src/Faker/Provider/fi_FI/Internet.php
@@ -4,22 +4,25 @@ namespace Faker\Provider\fi_FI;
 
 class Internet extends \Faker\Provider\Internet
 {
-    protected static $freeEmailDomain = array('gmail.com', 'yahoo.com', 'hotmail.com', 'jippii.fi', 'luukku.com');
-    protected static $tld = array('com', 'com', 'com', 'com', 'com', 'com', 'biz', 'info', 'net', 'org', 'fi');
+    protected static $freeEmailDomain = array( 'gmail.com', 'yahoo.com', 'hotmail.com', 'jippii.fi', 'luukku.com' );
+    protected static $tld = array( 'com', 'com', 'com', 'com', 'com', 'com', 'biz', 'info', 'net', 'org', 'fi' );
 
-	/**
-	 * Converts czech characters to their ASCII representation
-	 * @param string $string
-	 * @return string
-	 */
-	private function toAscii($string)
-	{
-		return preg_replace('/[^a-zA-Z0-9@_\-.]/', '', str_replace(array('ä','ö','å','Ä','Ö','Å'), array('a','o','a','A','O','A'), $string));
-		//return str_replace($from, $to, $string);
-	}
+    /**
+     * Converts czech characters to their ASCII representation
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    private function toAscii( $string )
+    {
+        return preg_replace( '/[^a-zA-Z0-9@_\-.]/', '',
+            str_replace( array( 'ä', 'ö', 'å', 'Ä', 'Ö', 'Å' ), array( 'a', 'o', 'a', 'A', 'O', 'A' ), $string ) );
+        //return str_replace($from, $to, $string);
+    }
 
-	public function email()
-	{
-		return $this->toAscii(parent::email());
-	}
+    public function email()
+    {
+        return $this->toAscii( parent::email() );
+    }
 }
