a2fa7475f8bf4bc6919783b25944251be206df1d
Huy TQ <trinhquanghuy95@gmail.com>
2014-10-29 20:49:46 +0700

    Move preg_replace outside loop and remove loop.

--- src/Faker/Provider/vi_VN/Internet.php
+++ src/Faker/Provider/vi_VN/Internet.php
@@ -4,37 +4,7 @@ namespace Faker\Provider\vi_VN;
 
 class Internet extends \Faker\Provider\Internet
 {
-    protected static $freeEmailDomain = array('gmail.com', 'yahoo.com', 'hotmail.com');
     protected static $tld = array('com', 'com', 'com', 'com', 'com', 'com', 'biz', 'info', 'net', 'org', 'vn', 'com.vn', 'biz.vn', 'edu.vn', 'gov.vn', 'net.vn', 'org.vn', 'int.vn', 'ac.vn', 'pro.vn', 'info.vn', 'health.vn', 'name.vn', 'mil.vn');
-
-    protected static $userNameFormats = array(
-        '{{lastName}}_{{firstName}}',
-        '{{firstName}}_{{lastName}}',
-        '{{lastName}}{{middleName}}{{firstName}}',
-        '{{firstName}}##',
-        '?{{lastName}}',
-        '{{lastName}}{{middleName}}{{firstName}}##',
-    );
-    protected static $emailFormats = array(
-        '{{userName}}@{{domainName}}',
-        '{{userName}}@{{freeEmailDomain}}',
-        '{{userName}}@{{freeEmailDomain}}',
-        '{{userName}}@{{freeEmailDomain}}',
-    );
-    protected static $urlFormats = array(
-        'http://www.{{domainName}}/',
-        'http://{{domainName}}/',
-        'https://www.{{domainName}}/',
-        'https://{{domainName}}/',
-        'http://www.{{domainName}}/{{slug}}',
-        'http://www.{{domainName}}/{{slug}}',
-        'https://www.{{domainName}}/{{slug}}',
-        'http://www.{{domainName}}/{{slug}}.html',
-        'http://{{domainName}}/{{slug}}',
-        'http://{{domainName}}/{{slug}}',
-        'http://{{domainName}}/{{slug}}.html',
-        'https://{{domainName}}/{{slug}}.html',
-    );
 
     /**
      * Convert Vietnamese characters to their ASCII representation
@@ -44,21 +14,18 @@ class Internet extends \Faker\Provider\Internet
      */
     private static function toAscii($string)
     {
-        $arr = array(
-            'a'=>'á|à|ả|ã|ạ|ă|ắ|ặ|ằ|ẳ|ẵ|â|ấ|ầ|ẩ|ẫ|ậ|Á|À|Ả|Ã|Ạ|Ă|Ắ|Ặ|Ằ|Ẳ|Ẵ|Â|Ấ|Ầ|Ẩ|Ẫ|Ậ',
-            'd'=>'đ|Đ',
-            'e'=>'é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ|É|È|Ẻ|Ẽ|Ẹ|Ê|Ế|Ề|Ể|Ễ|Ệ',
-            'i'=>'í|ì|ỉ|ĩ|ị|Í|Ì|Ỉ|Ĩ|Ị',
-            'o'=>'ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ|Ó|Ò|Ỏ|Õ|Ọ|Ô|Ố|Ồ|Ổ|Ỗ|Ộ|Ơ|Ớ|Ờ|Ở|Ỡ|Ợ',
-            'u'=>'ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự|Ú|Ù|Ủ|Ũ|Ụ|Ư|Ứ|Ừ|Ử|Ữ|Ự',
-            'y'=>'ý|ỳ|ỷ|ỹ|ỵ|Ý|Ỳ|Ỷ|Ỹ|Ỵ',
+        $from = array(
+            '/á|à|ả|ã|ạ|ă|ắ|ặ|ằ|ẳ|ẵ|â|ấ|ầ|ẩ|ẫ|ậ|Á|À|Ả|Ã|Ạ|Ă|Ắ|Ặ|Ằ|Ẳ|Ẵ|Â|Ấ|Ầ|Ẩ|Ẫ|Ậ/',
+            '/đ|Đ/',
+            '/é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ|É|È|Ẻ|Ẽ|Ẹ|Ê|Ế|Ề|Ể|Ễ|Ệ/',
+            '/í|ì|ỉ|ĩ|ị|Í|Ì|Ỉ|Ĩ|Ị/',
+            '/ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ|Ó|Ò|Ỏ|Õ|Ọ|Ô|Ố|Ồ|Ổ|Ỗ|Ộ|Ơ|Ớ|Ờ|Ở|Ỡ|Ợ/',
+            '/ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự|Ú|Ù|Ủ|Ũ|Ụ|Ư|Ứ|Ừ|Ử|Ữ|Ự/',
+            '/ý|ỳ|ỷ|ỹ|ỵ|Ý|Ỳ|Ỷ|Ỹ|Ỵ/',
         );
+        $to = array('a', 'd', 'e', 'i', 'o', 'u', 'y');
 
-        foreach ($arr as $ascii => $uni) {
-            $string = preg_replace("/($uni)/i", $ascii, $string);
-        }
-
-        return $string;
+        return preg_replace($from, $to, $string);
     }
 
     public function userName()
