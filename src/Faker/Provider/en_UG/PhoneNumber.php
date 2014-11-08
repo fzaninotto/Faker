7dca6083aeb63fccaecbe4d57dcf62d1a15ecf1f
tharoldD <tharoldd@gmail.com>
2014-11-08 11:26:56 +0300

    Updated uganda provider

--- src/Faker/Provider/en_UG/PhoneNumber.php
+++ src/Faker/Provider/en_UG/PhoneNumber.php
@@ -5,8 +5,10 @@ namespace Faker\Provider\en_UG;
 class PhoneNumber extends \Faker\Provider\PhoneNumber
 {
 	protected static $formats = array(
-		'+256 ### ### ###',
-		'+256#########',
+		'+256 7## ### ###',
+		'+2567########',
+		'+256 4## ### ###',
+		'+2564########',
 		'07## ### ###',
 		'07########',
 		'04## ### ###',
