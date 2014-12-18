fdf8a1f5324da909ba2916c7901328ce97e5befe
Juan Manuel Garcia Olivares <jmgolivares@gmail.com>
2014-12-17 21:56:21 -0300

    fix typo

--- src/Faker/Provider/Internet.php
+++ src/Faker/Provider/Internet.php
@@ -30,7 +30,7 @@ class Internet extends \Faker\Provider\Base
         'https://{{domainName}}/{{slug}}.html',
     );
 
-     protected static $tiwtterFormats = array(
+     protected static $twitterFormats = array(
         '@{{userName}}'
     );
 
