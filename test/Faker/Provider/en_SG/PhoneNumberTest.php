cd1dd144e2fc3879ce76098486f76cf09ca975a6
KimSia Sim <kimcity@gmail.com>
2014-12-28 14:10:19 +0800

    ensure that PhoneNumberTest passes

--- test/Faker/Provider/en_SG/PhoneNumberTest.php
+++ test/Faker/Provider/en_SG/PhoneNumberTest.php
@@ -19,7 +19,13 @@ class PhoneNumberTest extends \PHPUnit_Framework_TestCase
     // x means 0 to 9
     public function testMobilePhoneNumberStartWith9Returns9yxxxxxx()
     {
-        $this->assertRegExp('/^(\+65|65)?\s*9\s*[0-8]{3}\s*\d{4}$/', $this->faker->phoneNumber());
+        $startsWith9 = false;
+        while (!$startsWith9) {
+            $mobileNumber = $this->faker->mobileNumber();
+            $startsWith9 = preg_match('/^(\+65|65)?\s*9/', $mobileNumber);
+        }
+
+        $this->assertRegExp('/^(\+65|65)?\s*9\s*[0-8]{3}\s*\d{4}$/', $mobileNumber);
     }
 
     // http://en.wikipedia.org/wiki/Telephone_numbers_in_Singapore#Numbering_plan
@@ -27,6 +33,11 @@ class PhoneNumberTest extends \PHPUnit_Framework_TestCase
     // x means 0 to 9
     public function testMobilePhoneNumberStartWith7Or8Returns7Or8zxxxxxx()
     {
+        $startsWith7Or8 = false;
+        while (!$startsWith7Or8) {
+            $mobileNumber = $this->faker->mobileNumber();
+            $startsWith7Or8 = preg_match('/^(\+65|65)?\s*[7-8]/', $mobileNumber);
+        }
         $this->assertRegExp('/^(\+65|65)?\s*[7-8]\s*[1-9]{3}\s*\d{4}$/', $this->faker->phoneNumber());
     }
 }
