a928c0e053d9fb2ec07c29a88c26c06c6da8f22f
Mohammad Zolfaghari <zidom72@gmail.com>
2015-01-23 10:27:09 +0330

    custom realText function for fa_IR provider

--- src/Faker/Provider/fa_IR/Text.php
+++ src/Faker/Provider/fa_IR/Text.php
@@ -5,6 +5,62 @@ namespace Faker\Provider\fa_IR;
 class Text extends \Faker\Provider\Text
 {
     /**
+     * generates text string in arabic
+     *
+     * @example 'از تاریخ‌الشعرا را بکوبند روی نبش دیوار کوچه‌شان. تابلوی مدرسه.'
+     * @param  integer                   $maxNbChars
+     * @param  integer                   $indexSize
+     * @return string
+     * @throws \InvalidArgumentException
+     */
+    public function realText($maxNbChars = 200, $indexSize = 2)
+    {
+        if ($maxNbChars < 10) {
+            throw new \InvalidArgumentException('maxNbChars must be at least 10');
+        }
+
+        if ($indexSize < 1) {
+            throw new \InvalidArgumentException('indexSize must be at least 1');
+        }
+
+        if ($indexSize > 5) {
+            throw new \InvalidArgumentException('indexSize must be at most 5');
+        }
+
+        $words = $this->getConsecutiveWords($indexSize);
+        $result = array();
+        $resultLength = 0;
+        // take a random starting point
+        $next = static::randomKey($words);
+        while ($resultLength < $maxNbChars && isset($words[$next])) {
+            // fetch a random word to append
+            $word = static::randomElement($words[$next]);
+
+            // calculate next index
+            $currentWords = explode(' ', $next);
+
+            $currentWords[] = $word;
+            array_shift($currentWords);
+            $next = implode(' ', $currentWords);
+
+            if ($resultLength == 0 && !preg_match('/^[\x{0600}-\x{06FF}]/u', $word)) {
+                continue;
+            }
+            // append the element
+            $result[] = $word;
+            $resultLength += strlen($word) + 1;
+        }
+
+        // remove the element that caused the text to overflow
+        array_pop($result);
+
+        // build result
+        $result = implode(' ', $result);
+
+        return $result.'.';
+    }
+
+    /**
      * License: Creative Commons Attribution-ShareAlike License
      *
      * Title: مدیر مدرسه
