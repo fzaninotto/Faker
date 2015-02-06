a4238ce853aee7ac864b67a4a0efcc8b790f0dd8
Peter Haza <peter.haza@gmail.com>
2015-02-06 10:06:36 +0100

    Add Norwegian provider

---
+++ src/Faker/Provider/no_NO/PhoneNumber.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Faker\Provider\no_NO;
+
+class PhoneNumber extends \Faker\Provider\PhoneNumber
+{
+    /**
+    * @var array Norwegian phone number formats
+    */
+    protected static $formats = array(
+        '+47#########',
+        '+47 ## ## ## ##',
+        '## ## ## ##',
+        '## ## ## ##',
+        '########',
+        '########',
+        '9## ## ###',
+        '4## ## ###',
+        '9#######',
+        '4#######',
+    );
+}
