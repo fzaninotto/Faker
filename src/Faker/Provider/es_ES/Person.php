b758754a3d457c18af17336f3bdccafb716fd8ed
Xabier Fernández Rodríguez <jjbier@gmail.com>
2015-03-11 22:41:49 +0100

    add generator for nif

--- src/Faker/Provider/es_ES/Person.php
+++ src/Faker/Provider/es_ES/Person.php
@@ -68,4 +68,72 @@ class Person extends \Faker\Provider\Person
     {
         return static::randomElement(static::$suffix);
     }
+    protected static $firstEightDigitsFormat = array(
+        '1#######',
+        '2#######',
+        '3#######',
+        '45######',
+        '5#######',
+        '6#######',
+        '70######',
+        '71######',
+        '72######',
+        '77######',
+        '79######',
+        '8#######' ,
+        '90######',
+        '98######',
+        '99######'
+    );
+
+    /**
+     * Taxpayer Identification Number (NIF in Spain)
+     *
+     * @link http://es.wikipedia.org/wiki/N%C3%BAmero_de_identificaci%C3%B3n_fiscal
+     *
+     * @return string 9 digit number
+     */
+    public static function nif()
+    {
+        $firstEightDigits = static::numerify(static::randomElement(static::$firstEightDigitsFormat));
+        $lastDigit = static::dvCalcMod11($firstEightDigits);
+
+        return $firstEightDigits.$lastDigit;
+    }
+
+    /**
+     * Generate module
+     *
+     * @link http://pt.wikipedia.org/wiki/D%C3%ADgito_verificador
+     *
+     * @param string $number number
+     *
+     * @return int
+     */
+    public static function dvCalcMod11($number)
+    {
+        $base = 9;
+        $sum = 0;
+        $factor = 2;
+
+        for ($i = strlen($number); $i > 0; $i--) {
+            $numbers[$i] = substr($number, $i - 1, 1);
+            $partial[$i] = $numbers[$i] * $factor;
+            $sum += $partial[$i];
+            if ($factor == $base) {
+                $factor = 1;
+            }
+            $factor++;
+        }
+        $res = $sum % 11;
+
+        if ($res == 0 || $res == 1) {
+            $digit = 0;
+        } else {
+            $digit = 11 - $res;
+        }
+
+        return $digit;
+    }
 }
+
