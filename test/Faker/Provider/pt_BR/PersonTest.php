6aeea1f2a6c4a50d49acbf4b02271b1a68d0f147
Igor Santos <igorsantos07@gmail.com>
2015-04-29 00:24:41 -0300

    removing debug statements

--- test/Faker/Provider/pt_BR/PersonTest.php
+++ test/Faker/Provider/pt_BR/PersonTest.php
@@ -26,10 +26,8 @@ class PersonTest extends \PHPUnit_Framework_TestCase
     public function testRgFormatIsValid()
     {
         $rg = $this->faker->rg(false);
-        echo $rg."\n";
         $this->assertRegExp('/\d{8}\d/', $rg);
         $rg = $this->faker->rg(true);
-        echo $rg."\n";
         $this->assertRegExp('/\d{2}\.\d{3}\.\d{3}-[0-9X]/', $rg);
     }
 }
