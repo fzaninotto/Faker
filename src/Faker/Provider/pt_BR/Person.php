5e630a8748c8ffff1a588dc185a0583a558fb2d1
acfreitas <acarlossw@gmail.com>
2015-05-13 10:21:32 -0300

    Added generator random of CNPJ and CPF valid for Brazilian's guys #573

--- src/Faker/Provider/pt_BR/Person.php
+++ src/Faker/Provider/pt_BR/Person.php
@@ -99,4 +99,93 @@ class Person extends \Faker\Provider\Person
     {
         return static::randomElement(static::$suffix);
     }
+
+    /**
+     * Natural Persons Register (CPF in Brazil)
+     *
+     * @link http://en.wikipedia.org/wiki/Cadastro_de_Pessoas_F%C3%ADsicas
+     * @example '361.413.683-36'
+     * @example '37151166842'
+     * @param  boolean $mask
+     * @return string $cpf
+     */
+    public static function naturalPersonsRegister($mask = true)
+    {
+        $n1 = rand(0, 9);
+        $n2 = rand(0, 9);
+        $n3 = rand(0, 9);
+        $n4 = rand(0, 9);
+        $n5 = rand(0, 9);
+        $n6 = rand(0, 9);
+        $n7 = rand(0, 9);
+        $n8 = rand(0, 9);
+        $n9 = rand(0, 9);
+        $d1 = $n9 * 2 + $n8 * 3 + $n7 * 4 + $n6 * 5 + $n5 * 6 + $n4 * 7 + $n3 * 8 + $n2 * 9 + $n1 * 10;
+        $d1 = 11 - (self::mod($d1, 11) );
+        if ($d1 >= 10) {
+            $d1 = 0;
+        }
+        $d2 = $d1 * 2 + $n9 * 3 + $n8 * 4 + $n7 * 5 + $n6 * 6 + $n5 * 7 + $n4 * 8 + $n3 * 9 + $n2 * 10 + $n1 * 11;
+        $d2 = 11 - (self::mod($d2, 11) );
+        if ($d2 >= 10) {
+            $d2 = 0;
+        }
+        $cpf = '';
+        if ($mask == 1) {
+            $cpf = '' . $n1 . $n2 . $n3 . "." . $n4 . $n5 . $n6 . "." . $n7 . $n8 . $n9 . "-" . $d1 . $d2;
+        } else {
+            $cpf = '' . $n1 . $n2 . $n3 . $n4 . $n5 . $n6 . $n7 . $n8 . $n9 . $d1 . $d2;
+        }
+        return $cpf;
+    }
+
+    /**
+     * National Registry of Legal Entities (CNPJ in Brazil)
+     *
+     * @link http://en.wikipedia.org/wiki/CNPJ
+     * @example '97.531.288/0001-62'
+     * @example '21871213000169'
+     * @param  boolean $mask
+     * @return string $cnpj
+     */
+    public static function nationalRegistryLegalEntities($mask = true) {
+        $n1 = rand(0, 9);
+        $n2 = rand(0, 9);
+        $n3 = rand(0, 9);
+        $n4 = rand(0, 9);
+        $n5 = rand(0, 9);
+        $n6 = rand(0, 9);
+        $n7 = rand(0, 9);
+        $n8 = rand(0, 9);
+        $n9 = 0;
+        $n10 = 0;
+        $n11 = 0;
+        $n12 = 1;
+        $d1 = $n12 * 2 + $n11 * 3 + $n10 * 4 + $n9 * 5 + $n8 * 6 + $n7 * 7 + $n6 * 8 + $n5 * 9 + $n4 * 2 + $n3 * 3 + $n2 * 4 + $n1 * 5;
+        $d1 = 11 - (self::mod($d1, 11) );
+        if ($d1 >= 10) {
+            $d1 = 0;
+        }
+        $d2 = $d1 * 2 + $n12 * 3 + $n11 * 4 + $n10 * 5 + $n9 * 6 + $n8 * 7 + $n7 * 8 + $n6 * 9 + $n5 * 2 + $n4 * 3 + $n3 * 4 + $n2 * 5 + $n1 * 6;
+        $d2 = 11 - (self::mod($d2, 11) );
+        if ($d2 >= 10) {
+            $d2 = 0;
+        }
+        $cnpj = '';
+        if ($mask == 1) {
+            $cnpj = '' . $n1 . $n2 . "." . $n3 . $n4 . $n5 . "." . $n6 . $n7 . $n8 . "/" . $n9 . $n10 . $n11 . $n12 . "-" . $d1 . $d2;
+        } else {
+            $cnpj = '' . $n1 . $n2 . $n3 . $n4 . $n5 . $n6 . $n7 . $n8 . $n9 . $n10 . $n11 . $n12 . $d1 . $d2;
+        }
+        return $cnpj;
+    }
+
+    /**
+    * @param int $dividend
+    * @param int $divider
+    * @return int
+    */
+    private static function mod($dividend, $divider) {
+        return round($dividend - (floor($dividend / $divider) * $divider));
+    }
 }
