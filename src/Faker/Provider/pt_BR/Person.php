a80cbdafaa673c46602e5f3ab85268c03a40266d
acfreitas <acarlossw@gmail.com>
2015-05-13 11:02:29 -0300

    Alter rand to mt_rand

--- src/Faker/Provider/pt_BR/Person.php
+++ src/Faker/Provider/pt_BR/Person.php
@@ -111,15 +111,15 @@ class Person extends \Faker\Provider\Person
      */
     public static function naturalPersonsRegister($mask = true)
     {
-        $n1 = rand(0, 9);
-        $n2 = rand(0, 9);
-        $n3 = rand(0, 9);
-        $n4 = rand(0, 9);
-        $n5 = rand(0, 9);
-        $n6 = rand(0, 9);
-        $n7 = rand(0, 9);
-        $n8 = rand(0, 9);
-        $n9 = rand(0, 9);
+        $n1 = mt_rand(0, 9);
+        $n2 = mt_rand(0, 9);
+        $n3 = mt_rand(0, 9);
+        $n4 = mt_rand(0, 9);
+        $n5 = mt_rand(0, 9);
+        $n6 = mt_rand(0, 9);
+        $n7 = mt_rand(0, 9);
+        $n8 = mt_rand(0, 9);
+        $n9 = mt_rand(0, 9);
         $d1 = $n9 * 2 + $n8 * 3 + $n7 * 4 + $n6 * 5 + $n5 * 6 + $n4 * 7 + $n3 * 8 + $n2 * 9 + $n1 * 10;
         $d1 = 11 - (self::mod($d1, 11) );
         if ($d1 >= 10) {
@@ -150,14 +150,14 @@ class Person extends \Faker\Provider\Person
      */
     public static function nationalRegistryLegalEntities($mask = true)
     {
-        $n1 = rand(0, 9);
-        $n2 = rand(0, 9);
-        $n3 = rand(0, 9);
-        $n4 = rand(0, 9);
-        $n5 = rand(0, 9);
-        $n6 = rand(0, 9);
-        $n7 = rand(0, 9);
-        $n8 = rand(0, 9);
+        $n1 = mt_rand(0, 9);
+        $n2 = mt_rand(0, 9);
+        $n3 = mt_rand(0, 9);
+        $n4 = mt_rand(0, 9);
+        $n5 = mt_rand(0, 9);
+        $n6 = mt_rand(0, 9);
+        $n7 = mt_rand(0, 9);
+        $n8 = mt_rand(0, 9);
         $n9 = 0;
         $n10 = 0;
         $n11 = 0;
