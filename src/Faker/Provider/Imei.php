54b08dd879049fa429ae7571621404e890e4f591
Antônio Carlos <acarlossw@gmail.com>
2015-05-19 10:32:10 -0300

    Edited link from Wikipedia to English
    The older link was in pt_BT

--- src/Faker/Provider/Imei.php
+++ src/Faker/Provider/Imei.php
@@ -7,7 +7,7 @@ class Imei
     /**
      * International Mobile Equipment Identity (IMEI)
      *
-     * @link http://pt.wikipedia.org/wiki/International_Mobile_Equipment_Identity
+     * @link http://en.wikipedia.org/wiki/International_Mobile_Station_Equipment_Identity
      * @link http://imei-number.com/imei-validation-check/
      * @example '720084494799532'
      * @return int $imei
