c6ba54e8684962f33c89405f8c7cc1c7623f7fd7
acfreitas <acarlossw@gmail.com>
2015-06-11 15:07:58 -0300

    #574 move to PhoneNumber provider

--- src/Faker/Provider/PhoneNumber.php
+++ src/Faker/Provider/PhoneNumber.php
@@ -2,7 +2,9 @@
 
 namespace Faker\Provider;
 
-class PhoneNumber extends \Faker\Provider\Base
+use Faker\Calculator\Luhn;
+
+class PhoneNumber extends Base
 {
     protected static $formats = array('###-###-###');
 
@@ -12,5 +14,20 @@ class PhoneNumber extends \Faker\Provider\Base
     public static function phoneNumber()
     {
         return static::numerify(static::randomElement(static::$formats));
+    }
+
+    /**
+     * International Mobile Equipment Identity (IMEI)
+     *
+     * @link http://en.wikipedia.org/wiki/International_Mobile_Station_Equipment_Identity
+     * @link http://imei-number.com/imei-validation-check/
+     * @example '720084494799532'
+     * @return int $imei
+     */
+    public function imei()
+    {
+        $imei = (string) static::numerify('##############');
+        $imei .= Luhn::computeCheckDigit($imei);
+        return $imei;
     }
 }
--- test/Faker/Provider/PhoneNumberTest.php
+++ test/Faker/Provider/PhoneNumberTest.php
@@ -4,16 +4,16 @@ namespace Faker\Test\Provider;
 
 use Faker\Generator;
 use Faker\Calculator\Luhn;
-use Faker\Provider\Imei as ImeiProvider;
+use Faker\Provider\PhoneNumber as PhoneNumberProvider;
 
-class ImeiTest extends \PHPUnit_Framework_TestCase
+class PhoneNumberTest extends \PHPUnit_Framework_TestCase
 {
     private $faker;
 
     public function setUp()
     {
         $faker = new Generator();
-        $faker->addProvider(new ImeiProvider($faker));
+        $faker->addProvider(new PhoneNumberProvider($faker));
         $this->faker = $faker;
     }
 
