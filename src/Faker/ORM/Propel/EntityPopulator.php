28979e86b96a91ddd5d5c82ce2fba9ecaaec618f
Andy Waterman <andrew.waterman@switchconcepts.com>
2015-06-14 12:47:00 +0100

    Expand NameGuesser to take account of field length when available (Doctrine, Propel) and adds a few more field types.

--- src/Faker/ORM/Propel/EntityPopulator.php
+++ src/Faker/ORM/Propel/EntityPopulator.php
@@ -67,7 +67,7 @@ class EntityPopulator
             if ($columnMap->isPrimaryKey()) {
                 continue;
             }
-            if ($formatter = $nameGuesser->guessFormat($columnMap->getPhpName())) {
+            if ($formatter = $nameGuesser->guessFormat($columnMap->getPhpName(), $columnMap->getSize())) {
                 $formatters[$columnMap->getPhpName()] = $formatter;
                 continue;
             }
