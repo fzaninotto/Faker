cc795429d5be3619cf11fd58338c957db5104bcc
DessertCat <DesertCat@users.noreply.github.com>
2015-06-27 16:07:29 +0300

    Made female middle names more consistent

--- src/Faker/Provider/uk_UA/Person.php
+++ src/Faker/Provider/uk_UA/Person.php
@@ -4,14 +4,15 @@ namespace Faker\Provider\uk_UA;
 
 class Person extends \Faker\Provider\Person
 {
+
     protected static $maleNameFormats = array(
-        '{{firstNameMale}} {{middleName}} {{lastName}}',
-        '{{lastName}} {{firstNameMale}} {{middleName}}',
+        '{{firstNameMale}} {{middleNameMale}} {{lastName}}',
+        '{{lastName}} {{firstNameMale}} {{middleNameMale}}',
     );
 
     protected static $femaleNameFormats = array(
-        '{{lastName}} {{firstNameFemale}} {{middleName}}',
-        '{{firstNameFemale}} {{middleName}} {{lastName}}',
+        '{{lastName}} {{firstNameFemale}} {{middleNameFemale}}',
+        '{{firstNameFemale}} {{middleNameFemale}} {{lastName}}',
     );
 
     protected static $firstNameMale = array(
@@ -19,7 +20,7 @@ class Person extends \Faker\Provider\Person
         'Василь', 'Віталій', 'Володимир', 'Владислав', 'Геннадій', 'Георгій', 'Григорій', 'Данил', 'Данило', 'Денис', 'Дмитро',
         'Євген', 'Іван', 'Ігор', 'Йосип', 'Кирил', 'Костянтин', 'Лев', 'Леонід', 'Максим', 'Мирослав', 'Михайло', 'Назар',
         'Микита', 'Микола', 'Олег', 'Павло', 'Роман', 'Руслан', 'Сергій', 'Станіслав', 'Тарас', 'Тимофій', 'Федір',
-        'Юрій', 'Ярослав', 'Богдан', 'Болеслав', 'B’ячеслав',' Bалерій',' Bсеволод',' Bіктор', 'Ілля'
+        'Юрій', 'Ярослав', 'Богдан', 'Болеслав', 'B’ячеслав','Bалерій','Bсеволод','Bіктор', 'Ілля'
     );
 
     protected static $firstNameFemale = array(
@@ -29,11 +30,18 @@ class Person extends \Faker\Provider\Person
         'Юлія', 'Ярослава'
     );
 
-    protected static $middleName = array(
+    protected static $middleNameMale = array(
         'Олександрович', 'Олексійович', 'Андрійович', 'Євгенович', 'Сергійович', 'Іванович',
         'Федорович', 'Тарасович', 'Васильович', 'Романович', 'Петрович', 'Миколайович',
         'Борисович', 'Йосипович', 'Михайлович', 'Валентинович', 'Янович', 'Анатолійович',
-        'Євгенійович', 'Володимирович'
+        'Євгенович', 'Володимирович'
+    );
+
+    protected static $middleNameFemale = array(
+        'Олександрівна', 'Олексіївна', 'Андріївна', 'Євгенівна', 'Сергіївна', 'Іванівна',
+        'Федорівна', 'Тарасівна', 'Василівна', 'Романівна', 'Петрівна', 'Миколаївна',
+        'Борисівна', 'Йосипівна', 'Михайлівна', 'Валентинівна', 'Яновна', 'Анатоліївна',
+        'Євгенівна', 'Володимирівна'
     );
 
     protected static $lastName = array(
@@ -45,13 +53,26 @@ class Person extends \Faker\Provider\Person
     );
 
     /**
-     * Return middle name
+     * Return male middle name
      * @example 'Іванович'
      * @access public
-     * @return string Middle name
+     * @return string Male middle name
      */
-    public function middleName()
+    public function middleNameMale()
     {
-        return static::randomElement(static::$middleName);
+        return static::randomElement(static::$middleNameMale);
     }
+
+    /**
+     * Return female middle name
+     * @example 'Миколаївна'
+     * @access public
+     * @return string Female middle name
+     */
+    public function middleNameFemale()
+    {
+        return static::randomElement(static::$middleNameFemale);
+    }
+
+
 }
