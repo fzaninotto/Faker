d736863000bbe652cf02c19cd929956a94627d89
Rahman Mousavian <rahman.hi@gmail.com>
2015-08-06 17:54:55 +0800

    Add PhoneNumber class for fa_IR

---
+++ src/Faker/Provider/fa_IR/PhoneNumber.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Faker\Provider\fa_IR;
+
+class PhoneNumber extends \Faker\Provider\PhoneNumber
+{
+    protected static $formats = array(
+    	// Mobile
+        '091########',
+        '092########',
+        '093########',
+        '+9891########',
+        '+9892########',
+        '+9893########',
+        '091# ### ####',
+        '092# ### ####',
+        '093# ### ####',
+        
+        // Land lines
+        '########',
+        '0##-########',
+        '(0##)########',
+        '+98##########',
+        '0098##########',
+
+        //International
+        '00##-##########',
+    );
+}
