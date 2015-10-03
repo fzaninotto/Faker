b330c8354b4696cf239dacd8c7bc1179c5c73445
nickwebcouk <nick@nick-web.co.uk>
2015-10-03 21:02:33 +0100

    Update PhoneNumber.php
    Added new number formats for 4 digit area code locations (Glasgow, Edinburgh etc).

--- src/Faker/Provider/en_GB/PhoneNumber.php
+++ src/Faker/Provider/en_GB/PhoneNumber.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Faker\Provider\en_GB;
-
 class PhoneNumber extends \Faker\Provider\PhoneNumber
 {
     protected static $formats = array(
@@ -13,10 +11,13 @@ class PhoneNumber extends \Faker\Provider\PhoneNumber
         '0#########',
         '0#### ######',
         '0#### #####',
+        '0### ### ####',
+        '0### #######',
         '(0####) ######',
         '(0####) #####',
+        '(0###) ### ####',
+        '(0###) #######',
     );
-
     /**
      * An array of en_GB mobile (cell) phone number formats
      * @var array
@@ -27,7 +28,6 @@ class PhoneNumber extends \Faker\Provider\PhoneNumber
       '07### ######',
       '07### ### ###'
     );
-
     /**
      * Return a en_GB mobile phone number
      * @return string
