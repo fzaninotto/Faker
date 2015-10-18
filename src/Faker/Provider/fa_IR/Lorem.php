e68131c5e9200ebf6d6eb74b95f7c26e77662682
= <=>
2015-10-18 09:21:31 +0330

    Add Lorem to Persian (fa_IR) language.

---
+++ src/Faker/Provider/fa_IR/Lorem.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace Faker\Provider\fa_IR;
+
+class Lorem extends \Faker\Provider\Lorem
+{
+    protected static $wordList = array(
+        'آن', 'در', 'آنها', 'آوری', 'ابزارهایشان', 'اخیرا', 'ارائه', 'ارتباط', 'از', 'استفاده', 'امنیت', 'این', 'نرم افزارها', 'جا', 'جمع', 'اساسا', 'است', 'استفاده', 'می کند', 'اسمارت', 'اطلاعات', 'درباره', 'افزارها', 'الکترونیکی', 'اما', 'امنیت', 'امنیتی', 'انتشار', 'اندکی', 'انگشت', 'او', 'بیش از', 'لورم ایپسوم', 'اکثریت', 'اگر', 'ایرانشهر', 'این', 'فراهم', 'نرم فزار', 'نقاط', 'اینترنت', 'با', 'ضعف', 'وجود', 'بانک', 'شبکه', 'باتری', 'باجه های', 'بازی', 'باعث', 'باقی مانده', 'بانکداری', 'باور', 'باید', 'بدون', 'برابر', 'برای', 'اولین بار', 'تولید', 'برخی', 'برروی', 'برند', 'بسیاری', 'بشود', 'بعضی', 'بلکه', 'به', 'احراز', 'اینترنت', 'هتل', 'کارگرفت', 'بود', 'بوده است', 'بوده باشد', 'بیش از', 'تا', 'تایپ', 'تحقیقات', 'تدابیر', 'تزریق', 'تصادفی', 'تصویب', 'تصویربرداری', 'تعداد', 'زیادی', 'تغییر', 'شد', 'تغییرات', 'تلفن', 'تمایل', 'تنها', 'تهران', 'توسط', 'تولید', 'تکرار', 'شناسایی', 'تکه های', 'تیم', 'جان', 'جدی', 'جریان', 'جمله', 'جهت', 'جهش', 'حاصل', 'حاوی', 'حتی', 'حروفچینی', 'حفره های', 'حملات', 'حمله', 'حوزه', 'خارج', 'خصوصی', 'خود', 'خودروهای', 'گوگل', 'خودپرداز', 'دات کام', 'دارد', 'دارند', 'در', 'ارزیابی', 'گذاشتن', 'لزوم', 'نتیجه', 'وسط', 'درجه', 'بالا', 'درهم', 'دسترس', 'دلیل', 'دوربین', 'دوربین های', 'دیزاین', 'دیگران', 'را', 'راحتی', 'رایانه ای', 'ردی', 'رنج', 'رومیزی', 'زمان', 'زمانی', 'ساخت', 'ساختارهای', 'ساختگی', 'سالم', 'سبز', 'ستونی', 'ارتباط', 'سرگرمی', 'سنگاپور', 'سکیوریتی', 'سیار', 'سیم', 'شاید', 'شبکه', 'شخصی', 'شده', 'شده اند', 'شرم آور', 'شرکت', 'شما', 'شماری', 'شناخت', 'شناسایی', 'شود', 'صفحه', 'صفحه بندی', 'صفر', 'صنعت', 'صورت', 'ضمن اینکه', 'طبیعی', 'طراح', 'وب سایت', 'طراحی', 'طریق', 'طنز', 'ظاهرا', 'کامپیوتر', 'عادی', 'عاری', 'علاوه بر', 'عمدتا', 'غیر', 'فارسی', 'فرانسوی', 'فرم', 'فرهنگ', 'فرهنگ لغت', 'فروشگاه', 'فوتبال', 'فکری', 'فیفا', 'قرن هجدهم', 'لاتین', 'لغات', 'متن', 'محبوبیت', 'محترم', 'محتویات', 'محتویاتی', 'محصولات', 'مدتی', 'مدل', 'مراجعه', 'مشخصه', 'مطالب', 'مطمئن', 'معابر', 'معنایی', 'منتسب به', 'منطقی', 'موقعیت', 'می توان', 'می خواهد', 'می شود', 'می کنند', 'میزان', 'نام', 'نتیجه', 'نحوه', 'ندارد', 'نداشت', 'نرم افزار', 'نسخه های', 'نشان می دهد', 'نشده', 'نشر', 'نظام', 'نظیر', 'نفوذ', 'نقد', 'نمونه', 'نه تنها', 'نوع', 'نوعی', 'نگاه', 'نیازی', 'خیابان', 'هر چیزی', 'هزار سال', 'هستند', 'همانند', 'همراه', 'با', 'همزمان', 'همه', 'همیشه', 'هویت', 'هکر', 'هکرها', 'و', 'آدرس', 'و غیره ...', 'و یا', 'وابسته', 'وارد', 'واقعی', 'وب سایت', 'وجود', 'ندارد', 'ورق', 'پارسی', 'پرداخت', 'پرینتر', 'پزشک', 'پس', 'پنج', 'پنهان', 'پی', 'تعریف شده', 'چاپگر', 'چاپی', 'چیزی', 'ژنراتور', 'کار', 'کاربر', 'کاربران', 'کارشناس', 'کامل', 'کامپیوتر', 'کتاب', 'کرده است', 'کلمات', 'کنار', 'کنید', 'که', 'اتصال', 'استفاده', 'می رسد', 'تحقیق', 'هر چیزی', 'گذاشته', 'گوگل', 'یا', 'شهر', 'یورو', 'یک', 'تلفن','موبایل','کرد','سرویس','ساخت','شوید','شده است','می برد','دارند','صورت'
+
+
+    );
+
+    /**
+     * @example 'Lorem'
+     * @return string
+     */
+    public static function word()
+    {
+        return static::randomElement(static::$wordList);
+    }
+
+    /**
+     * Generate an array of random words
+     *
+     * @example array('Lorem', 'ipsum', 'dolor')
+     * @param  integer      $nb     how many words to return
+     * @param  bool         $asText if true the sentences are returned as one string
+     * @return array|string
+     */
+    public static function words($nb = 3, $asText = false)
+    {
+        $words = array();
+        for ($i=0; $i < $nb; $i++) {
+            $words []= static::word();
+        }
+
+        return $asText ? join(' ', $words) : $words;
+    }
+
+    /**
+     * Generate a random sentence
+     *
+     * @example 'Lorem ipsum dolor sit amet.'
+     * @param integer $nbWords         around how many words the sentence should contain
+     * @param boolean $variableNbWords set to false if you want exactly $nbWords returned,
+     *                                  otherwise $nbWords may vary by +/-40% with a minimum of 1
+     * @return string
+     */
+    public static function sentence($nbWords = 6, $variableNbWords = true)
+    {
+        if ($nbWords <= 0) {
+            return '';
+        }
+        if ($variableNbWords) {
+            $nbWords = self::randomizeNbElements($nbWords);
+        }
+
+        $words = static::words($nbWords);
+        $words[0] = ucwords($words[0]);
+
+        return join($words, ' ') . '.';
+    }
+
+    /**
+     * Generate an array of sentences
+     *
+     * @example array('Lorem ipsum dolor sit amet.', 'Consectetur adipisicing eli.')
+     * @param  integer      $nb     how many sentences to return
+     * @param  bool         $asText if true the sentences are returned as one string
+     * @return array|string
+     */
+    public static function sentences($nb = 3, $asText = false)
+    {
+        $sentences = array();
+        for ($i=0; $i < $nb; $i++) {
+            $sentences []= static::sentence();
+        }
+
+        return $asText ? join(' ', $sentences) : $sentences;
+    }
+
+    /**
+     * Generate a single paragraph
+     *
+     * @example 'Sapiente sunt omnis. Ut pariatur ad autem ducimus et. Voluptas rem voluptas sint modi dolorem amet.'
+     * @param integer $nbSentences         around how many sentences the paragraph should contain
+     * @param boolean $variableNbSentences set to false if you want exactly $nbSentences returned,
+     *                                      otherwise $nbSentences may vary by +/-40% with a minimum of 1
+     * @return string
+     */
+    public static function paragraph($nbSentences = 3, $variableNbSentences = true)
+    {
+        if ($nbSentences <= 0) {
+            return '';
+        }
+        if ($variableNbSentences) {
+            $nbSentences = self::randomizeNbElements($nbSentences);
+        }
+
+        return join(static::sentences($nbSentences), ' ');
+    }
+
+    /**
+     * Generate an array of paragraphs
+     *
+     * @example array($paragraph1, $paragraph2, $paragraph3)
+     * @param  integer      $nb     how many paragraphs to return
+     * @param  bool         $asText if true the paragraphs are returned as one string, separated by two newlines
+     * @return array|string
+     */
+    public static function paragraphs($nb = 3, $asText = false)
+    {
+        $paragraphs = array();
+        for ($i=0; $i < $nb; $i++) {
+            $paragraphs []= static::paragraph();
+        }
+
+        return $asText ? join("\n\n", $paragraphs) : $paragraphs;
+    }
+
+    /**
+     * Generate a text string.
+     * Depending on the $maxNbChars, returns a string made of words, sentences, or paragraphs.
+     *
+     * @example 'Sapiente sunt omnis. Ut pariatur ad autem ducimus et. Voluptas rem voluptas sint modi dolorem amet.'
+     * @param  integer $maxNbChars Maximum number of characters the text should contain (minimum 5)
+     * @return string
+     */
+    public static function text($maxNbChars = 200)
+    {
+        $text = array();
+        if ($maxNbChars < 5) {
+            throw new \InvalidArgumentException('text() can only generate text of at least 5 characters');
+        } elseif ($maxNbChars < 25) {
+            // join words
+            while (empty($text)) {
+                $size = 0;
+                // determine how many words are needed to reach the $maxNbChars once;
+                while ($size < $maxNbChars) {
+                    $word = ($size ? ' ' : '') . static::word();
+                    $text []= $word;
+                    $size += strlen($word);
+                }
+                array_pop($text);
+            }
+            $text[0][0] = static::toUpper($text[0][0]);
+            $text[count($text) - 1] .= '.';
+        } elseif ($maxNbChars < 100) {
+            // join sentences
+            while (empty($text)) {
+                $size = 0;
+                // determine how many sentences are needed to reach the $maxNbChars once;
+                while ($size < $maxNbChars) {
+                    $sentence = ($size ? ' ' : '') . static::sentence();
+                    $text []= $sentence;
+                    $size += strlen($sentence);
+                }
+                array_pop($text);
+            }
+        } else {
+            // join paragraphs
+            while (empty($text)) {
+                $size = 0;
+                // determine how many paragraphs are needed to reach the $maxNbChars once;
+                while ($size < $maxNbChars) {
+                    $paragraph = ($size ? "\n" : '') . static::paragraph();
+                    $text []= $paragraph;
+                    $size += strlen($paragraph);
+                }
+                array_pop($text);
+            }
+        }
+
+        return join($text, '');
+    }
+
+    protected static function randomizeNbElements($nbElements)
+    {
+        return (int) ($nbElements * mt_rand(60, 140) / 100) + 1;
+    }
+}
