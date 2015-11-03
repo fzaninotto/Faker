2e89a6f5443727d43736a2891cc6cee113f37186
newman101 <jremes@outlook.com>
2015-11-03 19:06:27 +0000

    da_DK Provider unit test added

---
+++ test/Faker/Provider/da_DK/InternetTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Faker\Test\Provider\da_DK;
+
+use Faker\Generator;
+use Faker\Provider\da_DK\Person;
+use Faker\Provider\da_DK\Internet;
+use Faker\Provider\da_DK\Company;
+
+class InternetTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var Generator
+     */
+    private $faker;
+
+    public function setUp()
+    {
+        $faker = new Generator();
+        $faker->addProvider(new Person($faker));
+        $faker->addProvider(new Internet($faker));
+        $faker->addProvider(new Company($faker));
+        $this->faker = $faker;
+    }
+
+    public function testEmailIsValid()
+    {
+        $email = $this->faker->email();
+        $this->assertNotFalse(filter_var($email, FILTER_VALIDATE_EMAIL));
+    }
+}
