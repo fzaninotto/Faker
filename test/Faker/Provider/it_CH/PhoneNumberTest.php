88ced0656bb39f71c212853e7f362709996c496b
z38 <z38@z38.invalid>
2015-11-22 00:27:19 +0100

    Add Swiss mobile number formats

--- test/Faker/Provider/it_CH/PhoneNumberTest.php
+++ test/Faker/Provider/it_CH/PhoneNumberTest.php
@@ -24,4 +24,9 @@ class PhoneNumberTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertRegExp('/^0\d{2} ?\d{3} ?\d{2} ?\d{2}|\+41 ?(\(0\))?\d{2} ?\d{3} ?\d{2} ?\d{2}$/', $this->faker->phoneNumber());
     }
+
+    public function testMobileNumber()
+    {
+        $this->assertRegExp('/^07[56789] ?\d{3} ?\d{2} ?\d{2}$/', $this->faker->mobileNumber());
+    }
 }
