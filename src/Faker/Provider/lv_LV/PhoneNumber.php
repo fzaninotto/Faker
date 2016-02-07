2ac49ace3a6a595050618201a11dc6cd2d04e20e
veisis <veisis@tvnet.lv>
2016-02-07 19:35:35 +0200

    Update PhoneNumber.php
    Phone numbers length for Latvia is 8 digits plus country code. And sometimes divided with spaces.

--- src/Faker/Provider/lv_LV/PhoneNumber.php
+++ src/Faker/Provider/lv_LV/PhoneNumber.php
@@ -5,9 +5,8 @@ namespace Faker\Provider\lv_LV;
 class PhoneNumber extends \Faker\Provider\PhoneNumber
 {
     protected static $formats = array(
-        '##-###-###',
-        '##-######',
         '########',
-        '+371 #######',
+        '## ### ###',
+        '+371 ########',
     );
 }
