63a8fffaf4cbf70d406b76d60722dc42a281f76a
veisis <veisis@tvnet.lv>
2016-02-10 08:49:50 +0200

    Add source link

--- src/Faker/Provider/lv_LV/PhoneNumber.php
+++ src/Faker/Provider/lv_LV/PhoneNumber.php
@@ -4,6 +4,9 @@ namespace Faker\Provider\lv_LV;
 
 class PhoneNumber extends \Faker\Provider\PhoneNumber
 {
+    /**
+     * {@link} https://en.wikipedia.org/wiki/Telephone_numbers_in_Latvia
+     **/
     protected static $formats = array(
         '########',
         '## ### ###',
