3179e10353645db072cc38acb416aacbd210804c
Francois Zaninotto <fzaninotto@gmail.com>
2016-02-23 15:37:55 +0100

    Fix bad tld syntax in en_ZA Internet provider

--- src/Faker/Provider/en_ZA/Internet.php
+++ src/Faker/Provider/en_ZA/Internet.php
@@ -10,9 +10,9 @@ class Internet extends \Faker\Provider\Internet
      * @see https://en.wikipedia.org/wiki/.za
      */
     protected static $tld = array(
-        'co.za', 'co.za', 'co.za', 'co.za', 'com', 'com', 'net', 'gov.za', '.ac.za', '.edu.za', 'law.za', 'mil.za',
-        'net.za', 'nom.za','org.za', 'school.za', 'ecape.school.za', 'fs.school.za', 'gp.school.za','kzn.school.za',
-        '.mpm.za', 'ncape.school.za', 'lp.school.za', 'nw.school.za', 'wcape.school.za', 'web.za', 'agric.za', 'nis.za',
+        'co.za', 'co.za', 'co.za', 'co.za', 'com', 'com', 'net', 'gov.za', 'ac.za', 'edu.za', 'law.za', 'mil.za',
+        'net.za', 'nom.za', 'org.za', 'school.za', 'ecape.school.za', 'fs.school.za', 'gp.school.za', 'kzn.school.za',
+        'mpm.za', 'ncape.school.za', 'lp.school.za', 'nw.school.za', 'wcape.school.za', 'web.za', 'agric.za', 'nis.za',
         'grondar.za',
     );
 }
