3100ec66e284f7bf2d3054d16dbf52b086131d34
newman101 <jremes@outlook.com>
2016-04-23 13:11:50 +0100

    uk_UA & ru_RU: Removed @license from Text.php

--- src/Faker/Provider/ru_RU/Text.php
+++ src/Faker/Provider/ru_RU/Text.php
@@ -21,7 +21,6 @@ class Text extends \Faker\Provider\Text
      *
      * Language: Russian
      *
-     * @licence Creative Commons Attribution-ShareAlike https://creativecommons.org/licenses/by-sa/3.0/deed.ru
      * @see     https://wikimediafoundation.org/wiki/Terms_of_Use/
      * @link    http://ru.wikisource.org/wiki/%D0%9C%D1%91%D1%80%D1%82%D0%B2%D1%8B%D0%B5_%D0%B4%D1%83%D1%88%D0%B8_(%D0%93%D0%BE%D0%B3%D0%BE%D0%BB%D1%8C)/%D0%A2%D0%BE%D0%BC_I/%D0%93%D0%BB%D0%B0%D0%B2%D0%B0_I
      * @var string
