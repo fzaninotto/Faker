0df59596eee6483142eab395ed72e4465bd09c6d
Ariful Haque <arifulhb@gmail.com>
2016-07-03 16:16:13 +0800

    Added Malaysian PhoneNumber Provider

---
+++ test/Faker/Provider/ms_MY/PhoneNumberTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Faker\Test\Provider\ms_MY;
+
+use Faker\Generator;
+use Faker\Provider\ms_MY\PhoneNumber;
+
+class PhoneNumberTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var Generator
+     */
+    private $faker;
+
+    public function setUp()
+    {
+        $faker = new Generator();
+        $faker->addProvider(new PhoneNumber($faker));
+        $this->faker = $faker;
+    }
+
+    /**
+     * @group ms_MY
+     */
+    public function testPhoneNumberFormat()
+    {
+        $number = $this->faker->phoneNumber;
+        $this->assertNotEmpty($number);
+    }
+
+    /**
+     * @group ms_MY
+     */
+    public function testMobileNumberFormat()
+    {
+        $number = $this->faker->mobileNumber;
+        $this->assertNotEmpty($number);
+    }
+
+}
