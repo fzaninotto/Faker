88bf9eedbb178639230fd1e1b29c05f20a922d43
the-noob <bogdan@the-noob.me>
2016-07-07 16:25:24 +0100

    adding landline for en_GB

--- src/Faker/Provider/en_GB/PhoneNumber.php
+++ src/Faker/Provider/en_GB/PhoneNumber.php
@@ -26,11 +26,24 @@ class PhoneNumber extends \Faker\Provider\PhoneNumber
      * @var array
      */
     protected static $mobileFormats = array(
-      // Local
-      '07#########',
-      '07### ######',
-      '07### ### ###'
+        // Local
+        '07#########',
+        '07### ######',
+        '07### ### ###'
     );
+
+    /**
+     * An array of en_GB landline phone number formats
+     *
+     * @var array
+     */
+    protected static $landlineFormats = array(
+        '0333 #### ###',
+        '02### ### ###',
+        '01### ######',
+    );
+
+
 
     /**
      * Return a en_GB mobile phone number
@@ -39,5 +52,16 @@ class PhoneNumber extends \Faker\Provider\PhoneNumber
     public static function mobileNumber()
     {
         return static::numerify(static::randomElement(static::$mobileFormats));
+    }
+
+
+    /**
+     * Return a en_GB landline phone number
+     *
+     * @return string
+     */
+    public static function landlineNumber()
+    {
+        return static::numerify(static::randomElement(static::$landlineFormats));
     }
 }
