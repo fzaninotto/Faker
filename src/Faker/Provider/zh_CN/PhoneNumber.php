8b0c3ee7e17f3622edd3d2e220db397fcb6d8bbd
zhwei <zhwei.yes@gmail.com>
2016-08-01 15:02:30 +0800

    remove 149

--- src/Faker/Provider/zh_CN/PhoneNumber.php
+++ src/Faker/Provider/zh_CN/PhoneNumber.php
@@ -5,11 +5,10 @@ namespace Faker\Provider\zh_CN;
 class PhoneNumber extends \Faker\Provider\PhoneNumber
 {
     protected static $operators = array(
-        1340, 1341, 1342, 1343, 1344, 1345, 1346, 1347, 1348, 135, 136, 137, 138, 139, 147, 150, 151, 152, 157, 158, 159, 1705, 178, 182, 183, 184, 187, 188, // China Mobile
+        134, 135, 136, 137, 138, 139, 147, 150, 151, 152, 157, 158, 159, 1705, 178, 182, 183, 184, 187, 188, // China Mobile
         130, 131, 132, 145, 155, 156, 1707, 1708, 1709, 1718, 1719, 176, 185, 186, // China Unicom
-        133, 149, 153, 1700, 1701, 177, 180, 181, 189, // China Telecom
+        133, 153, 1700, 1701, 177, 180, 181, 189, // China Telecom
         170, 171, // virtual operators
-        1349, // ChinaSat
     );
 
     protected static $formats = array('###########');
