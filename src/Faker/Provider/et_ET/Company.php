95fbb0ee8140c5cd654d774c71ceee70badb0ce6
Mailis <maiilu@hotmail.com>
2016-08-15 01:38:33 +0300

    added Estonian Provider

---
+++ src/Faker/Provider/et_ET/Company.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Faker\Provider\et_ET;
+
+class Company extends \Faker\Provider\Company
+{
+    protected static $formats = array(
+        'FIE {{firstName}} {{lastName}}',
+        '{{companyPrefix}} {{lastName}}',
+        '{{companyPrefix}} "{{lastName}} ja {{lastName}}"',
+        '{{companyPrefix}} "{{lastName}} {{companySuffix}}"',
+        '{{companyPrefix}} {{lastName}} {{companySuffix}}',
+    );
+
+    protected static $companyPrefix = array('MTÜ', 'TÜ', 'OÜ', 'AS', 'FIE', 'UÜ');
+    protected static $companySuffix = array('Talu', 'Kodu', 'Ehitus', 'Keskus', 'Elekter', 'Teenused', 'Tootmine');
+
+    /**
+     * @example 'Ltd'
+     */
+    public static function companyPrefix()
+    {
+        return static::randomElement(static::$companyPrefix);
+    }
+}
---
+++ src/Faker/Provider/et_ET/Internet.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Faker\Provider\et_ET;
+
+class Internet extends \Faker\Provider\Internet
+{
+    protected static $userNameFormats = array(
+        '{{lastName}}.{{firstNameMale}}',
+        '{{lastName}}.{{firstNameFemale}}',
+        '{{firstNameMale}}##',
+        '{{firstNameFemale}}##',
+        '?{{lastName}}',
+    );
+
+    protected static $freeEmailDomain = array('gmail.com', 'yahoo.com', 'hotmail.com');
+    protected static $tld = array('com', 'com', 'net', 'org', 'eu', 'eu', 'eu', 'ee', 'ee', 'ee', 'ee', 'ee', 'ee', 'ee');
+}
