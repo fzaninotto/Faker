419b6136c13c1c9ddbbb3a11fd94f0274dae8aea
Evan Lovely <evan@evanlovely.com>
2016-09-02 12:13:07 -0700

    Adding Lorem Formatter to de_DE Provider

---
+++ src/Faker/Provider/de_DE/Lorem.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Faker\Provider\de_DE;
+
+class Lorem extends \Faker\Provider\Lorem
+{
+  protected static $wordList = array(
+    'Ich', 'weiß', 'nicht', 'ob', 'ich', 'Zeit', 'dazu', 'habe', 'Ich', 'muss', 'schlafen', 'gehen', 'Herzlichen',
+    'Glückwunsch', 'zum', 'Geburtstag', 'Muiriel!', 'Das', 'wird', 'nichts', 'ändern', 'Das', 'wird', 'nie', 'enden',
+    'Manchmal', 'kann', 'er', 'ein', 'seltsamer', 'Junge', 'sein', 'Ich', 'war', 'in', 'den', 'Bergen', 'Das', 'liegt',
+    'daran', 'dass', 'du', 'nicht', 'allein', 'sein', 'willst', 'Ich', 'mochte', 'Biologie', 'noch', 'nie', 'Heute',
+    'ist', 'der', '18', 'Juni', 'und', 'das', 'ist', 'der', 'Geburtstag', 'von', 'Muiriel!', 'Ich', 'mochte',
+    'mysteriöse', 'Charaktere', 'schon', 'immer', 'mehr', 'Du', 'bist', 'so', 'ungeduldig', 'mit', 'mir', 'Mach', 'dir',
+    'keine', 'Sorgen', 'Das', 'wird', '30', '€', 'kosten', 'Letztendlich', 'muss', 'jeder', 'selbst', 'lernen', 'Ich',
+    'kann', 'so', 'ein', 'Leben', 'nicht', 'leben', 'Ich', 'vermisse', 'dich', 'Muiriel', 'ist', 'jetzt', '20', 'Das',
+    'Passwort', 'ist', 'Muiriel', 'Du', 'solltest', 'schlafen', 'Ich', 'werde', 'bald', 'zurück', 'sein', 'Ich',
+    'sagte', 'ihnen', 'sie', 'sollen', 'mir', 'ein', 'neues', 'Ticket', 'schicken', 'Das', 'wird', 'nicht', 'passieren',
+    'Ich', 'kann', 'mich', 'nur', 'fragen', 'ob', 'es', 'für', 'alle', 'anderen', 'dasselbe', 'ist', 'Was', 'ist',
+    'das?', 'Es', 'kann', 'sein', 'dass', 'ich', 'bald', 'aufgebe', 'und', 'stattdessen', 'ein', 'Nickerchen', 'mache'
+  );
+}
