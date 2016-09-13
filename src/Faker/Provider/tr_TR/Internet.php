9e5a2ae38cc9082164aabffdc727b451e23822a4
Mert Can ESEN <mertcanesen@gmail.com>
2016-09-13 16:08:19 +0300

    Improving tr_TR Providers(freeEmailDomain & tld) & CS fix

--- src/Faker/Provider/tr_TR/Internet.php
+++ src/Faker/Provider/tr_TR/Internet.php
@@ -4,6 +4,17 @@ namespace Faker\Provider\tr_TR;
 
 class Internet extends \Faker\Provider\Internet
 {
-    protected static $freeEmailDomain = array('gmail.com', 'hotmail.com', 'yahoo.com', 'yandex.com.tr', 'mynet.com', 'turk.net', 'superposta.com');
-    protected static $tld = array('com', 'com', 'com', 'com', 'com.tr', 'com.tr', 'info', 'net', 'org', 'org.tr','edu', 'edu.tr', 'edu.tr');
+    protected static $freeEmailDomain = array(
+        'gmail.com','hotmail.com','mail.com.tr','mynet.com','outlook.com.tr','superposta.com',
+        'turk.net','yahoo.com','yandex.com.tr',
+    );
+
+    /**
+     * @link https://www.nic.tr
+     */
+    protected static $tld = array(
+        'av.tr','bbs.tr','biz.tr','com','com','com','com','com.tr','com.tr','dr.tr','edu',
+        'edu.tr','edu.tr','info','info.tr','k12.tr','name.tr','net','net.tr','org','org.tr',
+        'tel.tr','tv.tr','web.tr',
+    );
 }
