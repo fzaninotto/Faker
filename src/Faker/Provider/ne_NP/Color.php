38da73d89f23d136c5d4b4a42dc9bd4da23f754f
Swornim Srestaa <srestaswrnm@gmail.com>
2016-10-11 19:08:31 +0545

    Solves #1055

---
+++ src/Faker/Provider/ne_NP/Color.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Faker\Provider\ne_NP;
+
+class Color extends \Faker\Provider\Color
+{
+
+  protected static $allColorNames = array(
+    अमला, आकाशी, ईट्‌टा, कलेजी, कालो, खरानी, खैरो, गुलाफी, नीलो, पहेँलो, प्याज, वदामी, रातो. रानी, सिम्रीक, सुगा, सुनौलो, सुन्तला, सेतो, हरियो	
+  );
+}
+
+ ?>
