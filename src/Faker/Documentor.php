10906ff414dbeaa6d6e66aa4bca5c2d5a15debbb
jirehstudios <jirehstudios@users.noreply.github.com>
2016-10-26 01:22:42 -0400

    Updated spacing and line length
    Reduced some lines to a length of less than 80 characters per PSR-2 standards and added spacing for readability between some lines.

--- src/Faker/Documentor.php
+++ src/Faker/Documentor.php
@@ -22,41 +22,56 @@ class Documentor
         $formatters = array();
         $providers = array_reverse($this->generator->getProviders());
         $providers[]= new Provider\Base($this->generator);
+        
         foreach ($providers as $provider) {
             $providerClass = get_class($provider);
             $formatters[$providerClass] = array();
             $refl = new \ReflectionObject($provider);
+            
             foreach ($refl->getMethods(\ReflectionMethod::IS_PUBLIC) as $reflmethod) {
-                if ($reflmethod->getDeclaringClass()->getName() == 'Faker\Provider\Base' && $providerClass != 'Faker\Provider\Base') {
+                if ($reflmethod->getDeclaringClass()->getName() == 'Faker\Provider\Base'
+                    && $providerClass != 'Faker\Provider\Base') {
                     continue;
                 }
+                
                 $methodName = $reflmethod->name;
+                
                 if ($reflmethod->isConstructor()) {
                     continue;
                 }
+                
                 $parameters = array();
+                
                 foreach ($reflmethod->getParameters() as $reflparameter) {
                     $parameter = '$'. $reflparameter->getName();
+                    
                     if ($reflparameter->isDefaultValueAvailable()) {
-                        $parameter .= ' = ' . var_export($reflparameter->getDefaultValue(), true);
+                        $parameter .= ' = ' . var_export(
+                            $reflparameter->getDefaultValue(), true);
                     }
+                    
                     $parameters []= $parameter;
                 }
+                
                 $parameters = $parameters ? '('. join(', ', $parameters) . ')' : '';
+                
                 try {
                     $example = $this->generator->format($methodName);
                 } catch (\InvalidArgumentException $e) {
                     $example = '';
                 }
+                
                 if (is_array($example)) {
                     $example = "array('". join("', '", $example) . "')";
                 } elseif ($example instanceof \DateTime) {
                     $example = "DateTime('" . $example->format('Y-m-d H:i:s') . "')";
-                } elseif ($example instanceof Generator || $example instanceof UniqueGenerator) { // modifier
+                } elseif ($example instanceof Generator
+                          || $example instanceof UniqueGenerator) { // modifier
                     $example = '';
                 } else {
                     $example = var_export($example, true);
                 }
+                
                 $formatters[$providerClass][$methodName . $parameters] = $example;
             }
         }
