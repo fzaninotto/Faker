1faca5e886d4fb4ec1c981a166b368ecbf4fa975
Nurlan Alekberov <nurlan.alekberoff@gmail.com>
2016-10-26 18:51:49 +0400

    testEmailIsValid was failed

--- src/Faker/Provider/az_AZ/Internet.php
+++ src/Faker/Provider/az_AZ/Internet.php
@@ -4,6 +4,5 @@ namespace Faker\Provider\az_AZ;
 
 class Internet extends \Faker\Provider\Internet
 {
-    protected static $freeEmailDomain = array('mail.az', 'box.az', '', 'gmail.com', 'hotmail.com');
-    protected static $tld = array('com', 'com', 'net', 'org', 'az', 'com.az', 'az', 'az');
-}
+    protected static $freeEmailDomain = array('yandex.ru', 'mail.az', 'box.az', 'gmail.com', 'mail.ru', 'list.ru', 'bk.ru', 'inbox.ru', 'rambler.ru', 'hotmail.com');
+    protected static $tld = array('com', 'com', 'net', 'org', 'ru', 'ru', 'ru', 'ru');
