f3f0f52745f65d2151f77f7416c8f40726d37448
jirehstudios <jirehstudios@users.noreply.github.com>
2016-10-26 01:31:04 -0400

    Spacing and line length updates
    More spacing (for readability) and line length updates per PSR-2 recommendations.

--- src/Faker/Factory.php
+++ src/Faker/Factory.php
@@ -6,7 +6,10 @@ class Factory
 {
     const DEFAULT_LOCALE = 'en_US';
 
-    protected static $defaultProviders = array('Address', 'Barcode', 'Biased', 'Color', 'Company', 'DateTime', 'File', 'HtmlLorem', 'Image', 'Internet', 'Lorem', 'Miscellaneous', 'Payment', 'Person', 'PhoneNumber', 'Text', 'UserAgent', 'Uuid');
+    protected static $defaultProviders = array(
+        'Address', 'Barcode', 'Biased', 'Color', 'Company', 'DateTime', 'File',
+        'HtmlLorem', 'Image', 'Internet', 'Lorem', 'Miscellaneous', 'Payment',
+        'Person', 'PhoneNumber', 'Text', 'UserAgent', 'Uuid');
 
     /**
      * Create a new generator
@@ -17,6 +20,7 @@ class Factory
     public static function create($locale = self::DEFAULT_LOCALE)
     {
         $generator = new Generator();
+        
         foreach (static::$defaultProviders as $provider) {
             $providerClassName = self::getProviderClassname($provider, $locale);
             $generator->addProvider(new $providerClassName($generator));
@@ -35,15 +39,21 @@ class Factory
         if ($providerClass = self::findProviderClassname($provider, $locale)) {
             return $providerClass;
         }
+        
         // fallback to default locale
         if ($providerClass = self::findProviderClassname($provider, static::DEFAULT_LOCALE)) {
             return $providerClass;
         }
+        
         // fallback to no locale
         if ($providerClass = self::findProviderClassname($provider)) {
             return $providerClass;
         }
-        throw new \InvalidArgumentException(sprintf('Unable to find provider "%s" with locale "%s"', $provider, $locale));
+        
+        throw new \InvalidArgumentException(
+            sprintf('Unable to find provider "%s" with locale "%s"',
+                    $provider, $locale)
+        );
     }
 
     /**
@@ -53,7 +63,11 @@ class Factory
      */
     protected static function findProviderClassname($provider, $locale = '')
     {
-        $providerClass = 'Faker\\' . ($locale ? sprintf('Provider\%s\%s', $locale, $provider) : sprintf('Provider\%s', $provider));
+        $providerClass = 'Faker\\' . (
+            $locale ? sprintf('Provider\%s\%s', $locale, $provider)
+            : sprintf('Provider\%s', $provider)
+        );
+        
         if (class_exists($providerClass, true)) {
             return $providerClass;
         }
