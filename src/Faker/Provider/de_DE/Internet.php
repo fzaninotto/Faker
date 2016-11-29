95cb74e0c9c44a276d41c3e82e38efbb305209b7
HUB <github@interlook.de>
2016-11-29 02:48:57 +0100

    fixed german localizations

--- src/Faker/Provider/de_DE/Internet.php
+++ src/Faker/Provider/de_DE/Internet.php
@@ -4,6 +4,6 @@ namespace Faker\Provider\de_DE;
 
 class Internet extends \Faker\Provider\Internet
 {
-    protected static $freeEmailDomain = array('web.de', 'gmail.com', 'hotmail.de', 'yahoo.de', 'googlemail.com', 'aol.de', 'gmx.de');
+    protected static $freeEmailDomain = array('web.de', 'gmail.com', 'hotmail.de', 'yahoo.de', 'googlemail.com', 'aol.de', 'gmx.de', 'freenet.de');
     protected static $tld = array('com', 'com', 'com', 'net', 'org', 'de', 'de', 'de');
 }
