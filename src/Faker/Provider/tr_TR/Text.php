54e6b5d232633eeba21bf03320ec630a891db480
RaSiM ÖZDİNÇ <rsmxtr@gmail.com>
2016-12-12 11:42:33 +0300

    Create Text.php for tr_TR

---
+++ src/Faker/Provider/tr_TR/Text.php
@@ -0,0 +1,121 @@
+<?php
+namespace Faker\Provider\tr_TR;
+class Text extends \Faker\Provider\Text
+{
+    /**
+     *
+     *
+     * Title: Alp Er Tunga Destanı
+     *
+     *
+     *
+     * Language: Turkish
+     *
+     *
+     *
+     * @see https://tr.wikisource.org/wiki/Alp_Er_Tunga_Destan%C4%B1
+     * @var string
+     */
+    protected static $baseText = <<<'EOT'
+İran padişahı "Minûçehr"in ölümünü haber alan Turan padişahı Peşeng, İran aleyhine savaş açmak için Türk ulularını topladı: "İranlılar'ın bize yaptıklarını biliyorsunuz. Türkün öç alma zamanı gelmiştir" dedi. Oğlu "Alp er tunga"nın içinde öç duygularıyla kaynadı. Babasına: "Ben arslanlarla çarpışabilecek kişiyim. İran'dan öç almalıyım" dedi. Boyu servi gibi, göğsü ve kolları arslan gibi idi. Fil kadar güçlü idi. Dili yırtıcı kılıç gibi idi.
+
+
+Savaş hazırlıkları yapılırken Türk padişahının öteki oğlu "Alp Arız" saraya gelip babasına: "Baba! Sen Türkler'in en büyüğüsün. Minûçer öldü ama İran ordusunun büyük kahramanları var.İsyan etmeyelim. Edersek ülkemiz yıkılıp gider" dedi. Peşeng, oğluna şöyle cevap verdi: "Alp er tunga avda arslan, savaşta savaş filidir. Bahadır bir timsahtır. Atalarının öcünü almalıdır. Sen onunla birlik ol. Ovalarda otlar yeşerince ordunuzu "Amul"a yürütün. İran'ı atlarınıza çiğnetin. Suları kana boyayın."
+
+
+Baharda Türk ordusu alp er tunga'nın buyruğunda İran üzerine yürüdü.Dehistan'a geldi.İki ordu karşılaştı. Türk kahramanlarından Barman İranlılar'a doğru ilerleyip er diledi. İran kumandanı ordusuna baktı.Gençlerden kimse kıyışamadı. Yalnız kumandanın kardeşi Kubâd atıldı. Fakat yaşlıydı. Kardeşi ona dedi ki: "Barman genç,arslan yürekli bir atlıdır.
+
+
+Boyu güneşe kadar uzanmıştır.Sen yaşlısın. Kan, ak saçlarını kızartırsa yiğitlerimiz ürker".Fakat Kubâd dinlemedi: "İnsan av, ölüm onun avcısıdır" diyerek savaşa çıktı. Barman ona: "Başını bana veriyorsun. Biraz daha bekleseydin daha iyiydi.Çünkü zaten senin hayatına kasdetmiştir" dedi. Kubâd: "Ben zâten dünyadan payımı almış bulunuyorum" diye karşılık vererek atını saldırdı.Sabahtan akşama kadar uğraştılar. Sonunda Barman kargı ile Kubâd'ı devirerek zaferle Alp er tunga'nın yanına döndü. Bunu görünce İran ordusu ilerledi. İki ordu birbirine girdi. Cihanın görmediği bir savaş oldu. Alp er tunga üstün geldi. İranlılar dikiş tutturamayıp dağıldılar. İran padişahı iki oğlunu memlekete göndererek kadınları Zâve dağına yollattı.
+
+
+Türk ve İran orduları iki gün dinlendikten sonra üçüncü gün Alp er tunga yeniden saldırdı.İran büyükleri ölü ve yaralı olarak savaş alanını doldurdular. Geceleyin İranlılar bozuldu.Bunu görünce İran padişahı ve başkumandanı Dehistan kalesine sığındılar. Alp er tunga kaleyi kuşattı. İran padişahı kaleyi bırakıp giderken ardına düşen Alp er tunga onu tutsak etti.
+
+
+İran'a tâbi Kâbil ülkesinin pâdişahı olan kahraman Zâl İranlıların yardımına geldi. Büyük savaşlar yaparak Türk ordularını bozdu. Bundan öfkelenen Alp er tunga, tutsak bulunan İran pâdişahını kılıçla öldürdü. Öteki tutsakları da öldürecekti. Fakat kardeşi Alp Arız onu vazgeçirdi. Tutsakları 'Sarı'ya göndererek hapsettirdi. Kendisi de Dehistan'da 'Rey'e gelerek İran tacını giydi. İran ülkesinde padişah oldu. Fakat Sarı'daki tutsakların kaçmasına sebep olduğu için kardeşi Alp Arız'ı öldürdü.
+
+
+İran tahtına Zev geçtiği zaman iki ordu yine karşı karşıya gelip beş ay vuruştular. Ortalıkta kıtlık oldu. Sonunda insanlık bitmesin diye barış yaptılar. İran'ın şimal ülkeleri Turan'ın oldu.
+
+
+Fakat Zev ölünce Alp er tunga yine İran'a saldırdı. Kardeşi Alp Arız'ı öldürdüğü için babası kendisine dargındı. Fakat yeni İran padişahı da ölüp İran tahtı yine boş kalınca Turan padişahı Peşeng, oğlu Alp er tunga'ya yine haber yolladı.
+
+
+Ceyhun'u geçerek İran tahtına oturmasını bildirdi.İranlılar Türk ordusunun geleceğini duyunca korkup Zâl'e başvurdular Zâl artık kocadığını söyleyerek oğlu Rüstem'i yolladı. İki ordunun öncüleri arasındaki çarpışmada Rüstem Türkler'i yenerek Keykubâd'ı İran tahtına çıkardı.
+
+
+Asıl orduların çarpışmasında ise Rüstem,Alp er tunga ile karşı karşıya geldi. Alp Er Tunga'yı yenecekken Türk bahadırları onu kurtardılar. Rüstem bir hamlede 1160 Türk kahramanı öldürdüğü için Türkler yenildiler. Ceyhun'u geçtiler. Alp er tunga babasının yanına döndü. Babasını barışa kandırdılar. Barış yaptılar.
+
+
+İran tahtına Keykâvus geçtikten sonra Araplar isyân ettiler.Fakat galip gelen Keykâvus bir ziyafette sarhoş edilerek bağlandı. Bu haber İran'ı karmakarışık etti.Alp er tunga büyük bir orduyla Araplar'ın üzerine atılarak onları yendi. Türk ordusu İran'a yayılarak herkesi tutsak etmeye başladı. İranlılar yine Zâl'den yardım istediler. Zâl,Araplarda tutsak olan Keykâvus'u kurtarıp onların ordularını da kendi ordusuna kattıktan sonra Türkler'e yöneldi. Kanlı bir savaşta Turanlıların yarısı öldü. Alp er tunga yenilerek kaçtı.
+
+
+Bir gün İran'ın yedi ünlü pehlivanı Rüstem'e Turan'a giderek Alp er tunga'nın avlağında avlanmayı teklif ettiler. Sirahs civarındaki bu avlağa gidip yedi gün kaldılar. Alp er tunga bunu duyunca ordusuyla geldi. Teke tek dövüşlerde Türk pehlivanları İranlılar'dan üstün geldilerse de işe Rüstem karışınca yedi pehlivan ile birlikte Türk ordusunu dağıttı. Hatta az kalsın Alp er tunga da tutsak oluyordu.
+
+
+Keykâvus İran'da eğlenceler,aşk oyunları ile uğraşırken Alp er tunga Türk atlılarıyla ilerledi. Bu haber Keykâvus'a geldi. Oğlu Siyâvuş ile Rüstem'i Türkler'e karşı yolladı. Türk öncülerini yenerek Belh kalesini aldılar. Bu sırada kötü bir rüya görüp bunu tabir ettiren Alp er tunga, beğlerin fikrini de alarak İranlılar'la barış yaptı. Onlara rehineler verdi.
+
+
+Buhara,Semerkand ve Çaç şehirlerini bırakıp "Gang" şehrine çekildi.Fakat bu barışı istemeyen Keykavus, Rüstem'e ve Siyâvuş'a kızıp kötü muamele ettiğinden Rüstem kendi ülkesine çekildi. Siyâvuş da Alp er tunga'ya sığındı. Türkler'in payıtahtı olan Gang şehrine kadar büyük saygı görerek geldi. Kendini çok sevdirdi. Hatta Türk kahramanlarından 'Piran'ın kızı ile ve biraz sonra da Alp er tunga'nın büyük kızı olan güzel 'Ferengis' ile evlendi. Pîran'ın kızından bir oğlu oldu. Adını Keyhusrev koydular.
+
+
+Bir müddet sonra,Siyâvuş'u çekemeyenler Alp er tunga'ya aleyhinde sözler söylenerek aralarını açtılar.Siyâvuş öldürüldü. Bunun üzerine Rüstem yine ortaya çıktı. İlk çarpışmada Alp er tunga'nın oğlu 'Sarka'yı öldürdüler.Alp er tunga bunun öcünü almak için bizzat yürüdü. Fakat savaşı İranlılar kazanarak onu Çin denizine kadar kaçırdılar. Rüstem Turanlıları nerde bulduysa öldürüp altı yıl Turan'da kaldıktan sonra çekilip yurduna geldi.
+
+Alp er tunga Turan'ın yakıldığını, Türkler'in öldürüldüğünü görünce kan ağladı. Öç almaya and içti. Ordu toplayarak İran'a girdi. Ekinleri yaktı.İran'a hakim oldu. Kıtlık çıkararak İranlılar yedi yıl açlıktan kırıldılar. Bunun önüne geçip İran'ı kurtarmak için Keyhusrev'e tahtı bıraktı. Keyhusrev, Alp er tunga'dan öç almak için ordusunu hazırladı.
+
+
+Fakat bu ordu daha Alp er tunga ile karşılaşmadan bozuldu.Keyhusrev yine ordu yolladı.Türkler'den Bazur adında birisi büyü yaparak dağlara kar yağdırdı. İranlılar'ın elleri tutmaz oldu. Böylelikle İran ordusunu doğradılar. İranlılar yine Rüstem'i yolladılar. Harikulade savaşlardan sonra Rüstem Türk ordusunu bozup Türk ordusunda bulunan Çin hakanını da tutsak etti.
+
+
+Alp er tunga bu haberi alınca pek üzüldü. Uluları toplayıp danıştı. Bunlar: "Ne yapalım! Çin, Saklap orduları bozulduysa, Turan ordusuna bir şey olmadı. Anamız bizi ölmek için doğurdu" dediler. Alp er tunga hazırlığa başladı. Oğlu 'Şide' onun maneviyatını yükseltti. Bu savaşa Turan ordusu tarafından, Çin dağlarında oturan "Püladvend" adında bir Çinli de ordusuyla iştirak etti. İran pehlivanlarını yendiyse de sonunda Rüstem'e yenildi. Bunun üzerine Turan ve İran orduları çarpıştı.
+
+
+İranlılar kazandı. Alp er tunga kaçtı.Bundan sonra Keyhusrev dünyanın üçte ikisine hakim oldu. Bir gün sarayında şarap içerken Turan, sınırından İranlılar gelip Turanlılar'ın kendilerine zarar verdiğini söylediler. Keyhusrev bu işi halletmek için İran kahramanlarından 'Bijen' i gönderdi.Bijen sınırda ve Turan tarafındaki bir ormanda, yanındaki güzel kızlarla eğlenen 'Menîje'yi gördü. Menîje, Alp er tunga'nın kızıydı. Birbirlerini sevdiler. Menîje onu Turan'a, sarayına götürdü. Alp Er Tunga bunu duyunca çok öfkelendi. Bijen'i kuyuya hapsetti.Kızını da kovdu. İran padişahı genç kumandanının gelmediğini görünce yine Rüstem'i yolladı.
+
+
+Rüstem tüccar kılığında Türk pâyitahtına kadar gitti.Bijen'i kurtardığı gibi Alp Er Tunga'nın da sarayını basarak onu kaçırdı, Menîje'yi İran'a gönderdi. Alp Er Tunga ise yeniden ordu yığarak yürüdü.İran ordusunun arkasında 'Bîsütun' dağı vardı.Yine Rüstem'in sayesinde İranlılar bu savaşı kazandılar.Alp er tunga, Karluk'a kadar kaçtı. Beğlerine dedi ki: "Ben dünyaya buyruğumu geçiriyordum.Minûçehr zamanında bile İran Turan'a denk olamamıştı.
+
+
+Fakat bugün İranlılar hayatımı sarayımda bile tehdit ediyorlar. İyi bir öç almayı düşünüyorum.Bin kere bin bir Türk ve Çin ordusuyla yürüyelim" Toplanmaya başladılar. Fakat bizzat Alp er tunga'nın iştirak etmediği ilk savaşı İranlılar kazandılar. İran padişahı Asıl Alp er tunga'yı yok etmek istiyordu. Yeniden her yandan ordular toplayarak ilerledi.
+
+
+Alp Er Tunga bin kere bin ordusunun üçte ikisini toplamıştı. 'Beykend' şehrinde oturuyordu.Karargâhında pars derisinden çadırlar vardı. Kendisi altınlı ve mücevherli bir taht üzerinde idi. Karargâhın önünde birçok kahramanların bayrakları dikili idi.
+
+
+İleriye gönderdiği ordunun bozulduğunu duyunca başı döndü. Öç almadan dönmemeye and içti.Oğlu 'Kara Han' a ordusunun yarısını vererek Buhâra'ya gönderdi. Oğullarından Şide (ki asıl adı Peşeng idi), Cehen, Afrâsiyab, Girdegîr ve oğlu İlâ'nın oğlu Güheylâ bu orduda idiler. Çigil, Taraz, Oğuz, Karluk ve Türkmen ler çerisini teşkil ediyordu. İki ordu karşılaşınca ilk önce İran padişahı Keyhusrev'le Alp er tunga'nın oğlu Şide teke tek dövüştüler. Şide öldü. Alp er tunga duyunca saçlarını yoldu. Ertesi gün iki ordu akşama kadar savaşıp ayrıldılar.
+
+
+Daha ertesi gün yine çarpışıldı. AlprEr Tunga kükremiş gibi saldırıyordu.İran'ın büyük pehlivanlarından birkaçını öldürdü. Keyhusrev'le Alp er tunga karşı karşıya geldiler. Fakat Turan pehlivanları onun İran padişahıyla dövüşmesini istemeyerek atının dizgininden tutup geri götürdüler. O gece Alp er tunga ordusunu alıp Ceyhun'un ötesine geçti.
+
+
+Kara Han'ın ordusuyla birleşip Buhara'ya geldi.Biraz dinlendiler. Sonra pâyıtahtı olan Gang'a geldi. Bu şehir cennet gibiydi. Toprağı mis,tuğlaları altındı. Her yerden ordular çağırdı. Bu sırada casusları Keyhusrev Ceyhun'u geçti diye bildirdiler. Keyhusrev ilk önce Suğd'a geldi. Bir ay kalıp itaate aldı. Yine ilerledi. Türkler İranlılar'a su vermiyorlar,ordunun arkasında yalnız kalmış İranlı bulurlarsa öldürüyorlardı. Keyhusrev de önüne çıkan saray, kale, erkek, kadın en bulursa yok ediyordu.
+
+İki ordu 'Gülzâriyun' ırmağı kıyısında karşılaştılar. Birbirine girdiler. Alp er Tunga'nın ordusundan Keyhüsrev'e korku gelmişti. Ordunun arkasına çekilip Tanrıya yalvardı.Derhal fırtına kopup tozları Turan ordusuna doğru atmaya başladı. Türkler bozuldular. Fakat Alp Er Tunga kaçmak isteyenleri öldürerek ordusunu durdurdu.Dönüp iyen savaştılar. Gece çökünce iki ordu ayrıldı.
+
+Alp er tunga ertesi günü yine çarpışacaktı. Fakat kendisine gelen haberci oğlu Kara Han'ın ordusundan yalnız Kara Han'ın sağ kaldığını bildirdi.Bunun üzerine ağırlıklarını bile toplamadan hızla ordusu ile çöle atıldı.
+
+Rüstem'i vurmak istiyordu.Keyhusrev bunu Rüstem'e bildirdiği gibi kendisi de onun ardına düştü.Alp er tunga, Gang'a gelip Rüstem'e baskın yapmak istediyse de onun tetikte olduğunu görerek vazgeçti. Şehre girdi.
+
+Bu kalabalık şehrin kalesi o kadar yüksekti ki üstünden kartal bile uçamazdı. İçinde yiyecek boldu.Her köşesinde kaynaklar, havuzlar vardı.Havuzlar bir ok atımı boyunda ve eninde idi.Güzel bahçeleri, saraylarıyla bir cennetti. Alp er tunga ordusuyla Gang'a kapandı.Çin padişahına da mektup yazıp yardım diledi.Keyhusrev de ordusuyla gelerek Rüstem'le birleşti.
+
+Kalenin çevresine hendekler kazdırdı.Odunlar yığıp katranla ateş verdiler.Duvarlar yıkıldı. Şehire hücumla girdiler.Herkesi öldürdüler.Alp er tunga sarayının altındaki gizli yoldan 200 beği ile kaçarak kurtuldu.Çin padişahının yanına gitti. Çin hakanı büyük bir ordu hazırlamıştı. Bunu duyan Türkler her taraftan Alp er tunga'nın yanına gidiyorlardı.
+
+Keyhusrev Gang'a,bir kumandan bırakıp Alp er tunga'nın üzerine yürüdü.Karşılaştılar.Alp er tunga ona bir mektup yazarak insanlardan uzak ve kendisinin beğeneceği bir yerde teketek dövüşmeği teklif etti.
+
+Keyhusrev kabul etmedi.O gün iki ordu akşama kadar çarpıştı. Gece olunca Keyhusrev ordusunun önüne hendekler kazdırdı. Bir kısım kuvvetlerini Türk ordusunun gerisine gönderdi.
+
+Türkler gece baskını yapıp hendeğe düştüler.Arkalarındaki kuvvetler de pusudan çıktı. Türk ordusunu yendiler.Alp er tunga kalan çerisiyle çöle çekildi. Keyhusrev Gang'a döndü. Çin padişahı da Keyhusrev'den korkarak ona elçi gönderdi.
+
+Keyhusrev, Alp er tunga'yı bir daha yanına almamak şartı ile onunla barıştı.Alp er tunga bunu işitince perişan bir halde çöle çekildi. Zere denizine geldi.Bu, ucu bucağı olmayan bir denizdi. Orada bir gemici vardı:"Ey padişah! Bu derin denizi geçemezsin. 78 yaşındayım. Bunu, bir geminin geçtiğini görmedim" dedi.
+
+Alp Er Tunga, "Tutsak olmaktansa ölmek yeğdir" diye cevap verdi. Bir gemi yüzdürttü.Binip yelken açtılar. "Gangıdız" şehrine vardılar. Alp er tunga orada "geçmişi düşünmeyelim. Talih yine buna döner" diyerek yatıp uyudu.
+
+Keyhusrev, Alp er tunga'nın suyu geçtiğini haber aldı.Hazırlıklar yaparak birtakım ülkeleri aldıktan sonra Zere denizinin kıyısına geldi. Yedi ayda denizi geçtiler. Gangidiz'i aldı. Bulduklarını kestilerse de Alp er tunga gizlice kaçtı.
+
+Keyhusrev buradan Turan'ın payıtahtı oldu. Gang'a geldi. Alp er tunga'yı soruşturdu. Kimse bilmiyordu. Halbuki bu sıralarda o yiyeceksiz, içeceksiz dolaşıyordu. Kayalık bir dağın tepesindeki bir mağarayı kendine ev yapmıştı.
+
+Bu mağarada insanlardan uzak yaşayan 'Hûm' adında biri vardı. Bir gün mağarada bir ses işitti. Alp er tunga kendi kendine tâliine yanıyordu. Bu sözlerin Türkçe olmasından yabancının kim olduğunu anlayan Hûm ona hücum ederek tutsak etti. Fakat o yine kaçarak suya atıldı. Hile ile Alp er Tunga'yı sudan çıkararak öldürdüler.
+
+EOT;
+  
+}
