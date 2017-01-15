8769a81663135ce73e8935d2d63d32faa5c6387a
DESKTOP-ATRT1VG\Dago <riderfish@gmail.com>
2017-01-15 17:42:41 +0800

    update test functions as camelCase naming rule

--- test/Faker/Provider/zh_TW/TextTest.php
+++ test/Faker/Provider/zh_TW/TextTest.php
@@ -20,7 +20,7 @@ class TextTest extends \PHPUnit_Framework_TestCase
     }
 
     /** @test */
-    function it_should_explode_the_string_to_array()
+    function testItShouldExplodeTheStringToArray()
     {
         $this->assertSame(
             array('中', '文', '測', '試', '真', '有', '趣'),
@@ -34,7 +34,7 @@ class TextTest extends \PHPUnit_Framework_TestCase
     }
 
     /** @test */
-    function it_should_return_the_string_length()
+    function testItShouldReturnTheStringLength()
     {
         $this->assertContains(
             $this->getMethod('strlen')->invokeArgs(null, array('中文測試真有趣')),
@@ -43,7 +43,7 @@ class TextTest extends \PHPUnit_Framework_TestCase
     }
 
     /** @test */
-    function it_should_return_the_character_is_valid_start_or_not()
+    function testItShouldReturnTheCharacterIsValidStartOrNot()
     {
         $this->assertTrue($this->getMethod('validStart')->invokeArgs(null, array('中')));
 
@@ -57,7 +57,7 @@ class TextTest extends \PHPUnit_Framework_TestCase
     }
 
     /** @test */
-    function it_should_append_end_punct_to_the_end_of_string()
+    function testItShouldAppendEndPunctToTheEndOfString()
     {
         $this->assertSame(
             '中文測試真有趣。',
