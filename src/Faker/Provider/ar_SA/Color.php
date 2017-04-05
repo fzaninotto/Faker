df1ce03ceab7f5ee9dad72ceeffe169e5b5841b5
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-04-05 00:34:41 -0700

    Add ar_SA Color Provider

---
+++ src/Faker/Provider/ar_SA/Color.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Faker\Provider\ar_SA;
+
+class Color extends \Faker\Provider\Color
+{
+    protected static $safeColorNames = array(
+        'أسود', 'قرمزي', 'أخضر', 'أزرق داكن', 'زيتوني',
+        'أرجواني', 'أحمر', 'ليمي', 'أزرق', 'فضي',
+        'رمادي', 'أصفر', 'برتقالي', 'بني', 'أبيض'
+    );
+        
+}
