e18096e36b90c8ae592e9c1b585bd89d7802decb
Michael Tsang <michael@itwakes.me>
2017-04-24 10:30:30 +0800

    reformat code

--- src/Faker/Provider/en_HK/Internet.php
+++ src/Faker/Provider/en_HK/Internet.php
@@ -5,27 +5,10 @@ namespace Faker\Provider\en_HK;
 class Internet extends \Faker\Provider\Internet
 {
     protected static $freeEmailDomain = array(
-        'gmail.com',
-        'yahoo.com',
-        'hotmail.com',
-        'yahoo.com.hk',
-        'hotmail.com.hk',
+        'gmail.com', 'yahoo.com', 'hotmail.com', 'yahoo.com.hk', 'hotmail.com.hk'
     );
     protected static $tld = array(
-        'com',
-        'com',
-        'com',
-        'com.hk',
-        'com.hk',
-        'com',
-        'biz',
-        'info',
-        'net',
-        'org',
-        'com.hk',
-        'edu.hk',
-        'org.hk',
-        'idv.hk',
+        'com', 'com', 'com', 'com.hk', 'com.hk', 'com', 'biz', 'info', 'net', 'org',
+        'com.hk', 'edu.hk', 'org.hk', 'idv.hk'
     );
-
 }
