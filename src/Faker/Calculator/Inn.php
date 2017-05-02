34aabd19e59171009680a35fd16ef48079496bc3
Aleksandr Novikov <pwsdotru@gmail.com>
2017-05-02 23:07:51 +0300

    Add support INN and KPP - identification info for company in Russia

---
+++ src/Faker/Calculator/Inn.php
@@ -0,0 +1,34 @@
+<?php
+namespace Faker\Calculator;
+
+class Inn
+{
+    /**
+     * Generates INN Checksum
+     *
+     * https://ru.wikipedia.org/wiki/%D0%98%D0%B4%D0%B5%D0%BD%D1%82%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D1%8B%D0%B9_%D0%BD%D0%BE%D0%BC%D0%B5%D1%80_%D0%BD%D0%B0%D0%BB%D0%BE%D0%B3%D0%BE%D0%BF%D0%BB%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D1%89%D0%B8%D0%BA%D0%B0
+     *
+     * @param string $inn
+     * @return string Checksum (one digit)
+     */
+    public static function checksum($inn)
+    {
+        $multipliers = array(1 => 2, 2 => 4, 3 => 10, 4 => 3, 5 => 5, 6 => 9, 7 => 4, 8 => 6, 9 => 8);
+        $sum = 0;
+        for ($i = 1; $i <= 9; $i++) {
+            $sum += intval(substr($inn, $i-1, 1)) * $multipliers[$i];
+        }
+        return strval(($sum % 11) % 10);
+    }
+
+    /**
+     * Checks whether an INN has a valid checksum
+     *
+     * @param string $inn
+     * @return boolean
+     */
+    public static function isValid($inn)
+    {
+        return self::checksum(substr($inn, 0, -1)) === substr($inn, -1, 1);
+    }
+}
--- src/Faker/Provider/ru_RU/Company.php
+++ src/Faker/Provider/ru_RU/Company.php
@@ -49,4 +49,25 @@ class Company extends \Faker\Provider\Company
     {
         return static::randomElement(static::$companyNameSuffixes);
     }
+
+    public static function inn($area_code = "")
+    {
+        if ($area_code === "" || intval($area_code) == 0) {
+            //Simple generation code for areas in Russian without check for valid
+            $area_code = static::numberBetween(1, 91);
+        } else {
+            $area_code = intval($area_code);
+        }
+        $area_code = str_pad($area_code, 2, '0', STR_PAD_LEFT);
+        $inn_base =  $area_code . static::numerify('#######');
+        return $inn_base . \Faker\Calculator\Inn::checksum($inn_base);
+    }
+
+    public static function kpp($inn = "")
+    {
+        if ($inn == "" || strlen($inn) < 4) {
+            $inn = static::inn();
+        }
+        return substr($inn, 0, 4) . "01001";
+    }
 }
