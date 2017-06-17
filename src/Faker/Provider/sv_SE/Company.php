25b82b92e993fbe5204005038f831276da3c03ed
Vincent Klaiber <vinkla@users.noreply.github.com>
2017-06-17 18:21:57 +0200

    Update Company.php

--- src/Faker/Provider/sv_SE/Company.php
+++ src/Faker/Provider/sv_SE/Company.php
@@ -17,7 +17,7 @@ class Company extends \Faker\Provider\Company
 
     protected static $companySuffix = array('AB', 'HB');
     
-    protected static $jobTitles = array('Automationsingenjör', 'Bagare', 'Digital Designer', 'Ekonom', 'Ekonomichef', 'Elektronikingenjör', 'Försäljare', 'Försäljningschef', 'Innovationsdirektör', 'Investeringsdirektör', 'Journalist', 'Kock', 'Kulturstrateg', 'Läkare', 'Lokförare', 'Mäklare', 'Programmerare', 'Projektledare', 'Sjuksköterska', 'Utvecklare' 'Utvecklare', 'UX Designer', 'Webbutvecklare');
+    protected static $jobTitles = array('Automationsingenjör', 'Bagare', 'Digital Designer', 'Ekonom', 'Ekonomichef', 'Elektronikingenjör', 'Försäljare', 'Försäljningschef', 'Innovationsdirektör', 'Investeringsdirektör', 'Journalist', 'Kock', 'Kulturstrateg', 'Läkare', 'Lokförare', 'Mäklare', 'Programmerare', 'Projektledare', 'Sjuksköterska', 'Utvecklare', 'UX Designer', 'Webbutvecklare');
     
     public function jobTitle()
     {
