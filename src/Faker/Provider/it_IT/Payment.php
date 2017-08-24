a53ed286038d2840f0ae529ea82ac817c1f269f4
Giuseppe Sanfrancesco <giuseppe.sanfrancesco@axelero.it>
2017-08-24 10:16:40 +0200

    Added some italian banks

--- src/Faker/Provider/it_IT/Payment.php
+++ src/Faker/Provider/it_IT/Payment.php
@@ -5,6 +5,20 @@ namespace Faker\Provider\it_IT;
 class Payment extends \Faker\Provider\Payment
 {
     /**
+     * @see list of Italian banks (2017-11-08), source: https://en.wikipedia.org/wiki/Category:Banks_of_Italy
+     */
+    protected static $banks = array(
+        'Banca Antonveneta', 'Banca Carige', 'Banca Intesa', 'Banca Mediolanum', 'Banca Montepaschi di Siena',
+        'Banca Nazionale del Lavoro', 'Banca Sella', 'Banco BPM', 'Banco di Brescia', 'Banco di Desio e della Brianza',
+        'Banco di Napoli', 'Credito Valtellinese', 'FinecoBank', 'Intesa Sanpaolo', 'Mediobanca', 'UniCredit'
+    );
+    
+    public static function bank()
+    {
+        return static::randomElement(static::$banks);
+    }
+    
+    /**
      * International Bank Account Number (IBAN)
      * @link http://en.wikipedia.org/wiki/International_Bank_Account_Number
      * @param  string $prefix for generating bank account number of a specific bank
