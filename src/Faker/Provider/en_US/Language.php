4055904f5dff590d0d6c5e6943ad931d3873d2e5
Espen Nilsen <espen.k.nilsen@gmail.com>
2017-09-12 17:16:30 +0200

    Added localized versions of language (source: wikipedia).

---
+++ src/Faker/Provider/en_US/Language.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Faker\Provider\en_US;
+
+class Language extends \Faker\Provider\Language
+{
+    /**
+     * {@link} https://en.wikipedia.org/wiki/List_of_official_languages
+     **/
+    protected static $language = array(
+      //A
+      'Afar', 'Afrikaans', 'Aja-Gbe', 'Akan (Akuapem Twi, Ashante Twi, Fante)',
+      'Albanian', 'Amazigh', 'Amharic', 'Anii', 'Arabic', 'Armenian', 'Assamese',
+      'Aymara', 'Azerbaijani',
+      //B
+      'Balanta', 'Bambara', 'Bariba', 'Bassari', 'Bedik', 'Belarusian', 'Bengali',
+      'Berber', 'Biali', 'Bislama', 'Boko', 'Bomu', 'Bosnian', 'Bozo', 'Buduma',
+      'Bulgarian', 'Burmese',
+      //C
+      'Cantonese', 'Catalan', 'Chinese, Mandarin', 'Chichewa', 'Chirbawe (Sena)',
+      'Comorian', 'Croatian', 'Czech',
+      //D
+      'Dagaare', 'Dagbani', 'Dangme', 'Danish', 'Dari', 'Dendi', 'Dhivehi',
+      'Dioula', 'Dogon', 'Dutch', 'Dzongkha',
+      //E
+      'English', 'Estonian', 'Ewe-Gbe',
+      //F
+      'Fijian', 'Filipino', 'Finnish', 'Fon-Gbe', 'Foodo', 'French', 'Fula',
+      //G
+      'Ga', 'Gàidhlig', 'Gbe', 'Gen-Gbe', 'Georgian', 'German', 'Gonja',
+      'Gourmanché', 'Greek', 'Guaraní', 'Gujarati',
+      //H
+      'Haitian Creole', 'Hassaniya', 'Hausa', 'Hebrew', 'Hindi', 'Hiri Motu',
+      'Hungarian',
+      //I
+      'Igbo', 'Icelandic', 'Indonesian', 'Irish', 'Italian',
+      //J
+      'Japanese', 'Jola',
+      //K
+      'Kabye', 'Kalanga', 'Kanuri', 'Kasem', 'Kazakh', 'Khmer', 'Kinyarwanda',
+      'Kirundi', 'Kissi', 'Khoisan', 'Korean', 'Kpelle', 'Kurdish', 'Kyrgyz',
+      //L
+      'Lao', 'Latin', 'Latvian', 'Lithuanian', 'Lukpa', 'Luxembourgish',
+      //M
+      'Macedonian', 'Malagasy', 'Malay', 'Malinke', 'Maltese', 'Mamara',
+      'Manding (Mandinka, Malinke)', 'Mandinka', 'Mandjak', 'Mankanya',
+      'Manx Gaelic', 'Māori', 'Marshallese', 'Mbelime', 'Moldovan', 'Mongolian',
+      'Montenegrin', 'Mossi',
+      //N
+      'Nambya', 'Nateni', 'Nauruan', 'Ndau', 'Ndebele (Northern)',
+      'Ndebele (Southern)', 'Nepali', /* 'New Zealand Sign Language', */
+      'Noon', 'North Korean', 'Northern Sotho', 'Norwegian', 'Nzema',
+      //O
+      'Oniyan', 'Ossetian',
+      //P
+      'Palauan', 'Papiamento', 'Pashto', 'Persian', 'Polish', 'Portuguese',
+      //Q
+      'Quechua',
+      //R
+      'Romanian', 'Romansh', 'Russian',
+      //S
+      'Safen', 'Samoa', 'Sango', 'Sena', 'Serbian', 'Serer', 'Seychellois Creole',
+      'Shona', 'Sinhala', 'Slovak', 'Slovene', 'Somali', 'Songhay-Zarma',
+      'Soninke', 'Sotho', 'Spanish', 'Susu', 'Swahili', 'Swati', 'Swedish',
+      'Syenara',
+      //T
+      'Tajik', 'Tagalog', 'Tamasheq', 'Tamil', 'Tammari', 'Tasawaq', 'Tebu',
+      'Telugu', 'Tetum', 'Thai', 'Tigrinya', 'Tok Pisin', 'Toma', 'Tonga',
+      'Tongan', 'Tsonga', 'Tswana', 'Turkish', 'Turkmen', 'Tuvaluan',
+      //U
+      'Ukrainian', 'Urdu', 'Uzbek',
+      //V
+      'Venda', 'Vietnamese',
+      //W
+      'Waama', 'Waci-Gbe', 'Wamey', 'Welsh', 'Wolof',
+      //X
+      'Xhosa', 'Xwela-Gbe',
+      //Y
+      'Yobe', 'Yom', 'Yoruba',
+      //Z
+      /* 'Zimbabwean sign language', */ 'Zulu',
+    );
+}
