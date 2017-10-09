a38e34c8114066adf40964a54a3ac2b649584869
Juha Remes <jremes@outlook.com>
2017-10-09 11:23:19 +0100

    Add InternetTest.php

--- src/Faker/Provider/he_IL/Internet.php
+++ src/Faker/Provider/he_IL/Internet.php
@@ -5,6 +5,7 @@ namespace Faker\Provider\he_IL;
 class Internet extends \Faker\Provider\Internet
 {
   protected static $freeEmailDomain = array('nana.co.il', 'gmail.com', 'hotmail.co.il', 'walla.co.il');
+  protected static $tld = array('com', 'net', 'org', 'co.il');
 }
 
 ?>
---
+++ test/Faker/Provider/he_IL/InternetTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Faker\Test\Provider\he_IL;
+
+use Faker\Generator;
+use Faker\Provider\he_IL\Person;
+use Faker\Provider\he_IL\Internet;
+use Faker\Provider\he_IL\Company;
+
+class InternetTest extends \PHPUnit_Framework_TestCase
+{
+
+  /**
+   *  @var Faker\Generator
+   */
+
+   public function setUp()
+   {
+     $faker = new Generator();
+     $faker->addProvider(new Person($faker));
+     $faker->addProvider(new Internet($faker));
+     $faker->addProvider(new Company($faker));
+     $this->faker = $faker;
+   }
+
+}
+
+?>
