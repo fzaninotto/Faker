7c19e14de6e1b4da12f67c994efacac35febef8e
Juha Remes <jremes@outlook.com>
2017-10-09 11:01:22 +0100

    Add Internet.php to he_IL locale

---
+++ src/Faker/Provider/he_IL/Internet.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Faker\Provider\he_IL;
+
+class Internet extends \Faker\Provider\Internet
+{
+  protected static $freeEmailDomain = array('nana.co.il', 'gmail.com', 'hotmail.co.il', 'walla.co.il');
+}
+
+?>
