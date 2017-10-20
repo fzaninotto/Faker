3ed1dafc09712afe2456b24139490dba2a27bb10
Sasawat Iammatavee <i.sasawat@gmail.com>
2017-10-20 16:04:18 +0700

    Changed credit card networks and masks to support payment gateways in Thailand

--- src/Faker/Provider/th_TH/Payment.php
+++ src/Faker/Provider/th_TH/Payment.php
@@ -5,6 +5,45 @@ namespace Faker\Provider\th_TH;
 class Payment extends \Faker\Provider\Payment
 {
     /**
+     * @var array List of card networks generally accept by payment gateway in Thailand
+     */
+    protected static $cardVendors = array(
+        'Visa', 'MasterCard',
+    );
+    
+    /**
+     * @var array List of card brand masks for generating valid credit card numbers; Most Thai payment gateways accept only 16 digits cards
+     * @see https://en.wikipedia.org/wiki/Payment_card_number Reference for existing prefixes
+     * @see https://www.mastercard.us/en-us/issuers/get-support/2-series-bin-expansion.html MasterCard 2017 2-Series BIN Expansion
+     */
+    protected static $cardParams = array(
+        'Visa' => array(
+            "4539###########",
+            "4556###########",
+            "4916###########",
+            "4532###########",
+            "4929###########",
+            "40240071#######",
+            "4485###########",
+            "4716###########",
+            "4##############"
+        ),
+        'MasterCard' => array(
+            "2221###########",
+            "23#############",
+            "24#############",
+            "25#############",
+            "26#############",
+            "2720###########",
+            "51#############",
+            "52#############",
+            "53#############",
+            "54#############",
+            "55#############"
+        ),
+    );
+    
+    /**
      * @var array Thai bank names
      * @link https://th.wikipedia.org/wiki/%E0%B8%A3%E0%B8%B2%E0%B8%A2%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%98%E0%B8%99%E0%B8%B2%E0%B8%84%E0%B8%B2%E0%B8%A3%E0%B9%83%E0%B8%99%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B9%80%E0%B8%97%E0%B8%A8%E0%B9%84%E0%B8%97%E0%B8%A2
      */
