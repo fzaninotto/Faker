14069a8985736febcdb1c1c883b3455d0200dc7d
dknacht <dknacht@dknacht>
2017-11-26 23:50:25 -0500

    Adding most commonly used free email domains and tdls

---
+++ src/Faker/Provider/es_CO/Internet.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Faker\Provider\es_ES;
+
+class Internet extends \Faker\Provider\Internet
+{
+    protected static $freeEmailDomain = array('gmail.com', 'hotmail.com', 'hotmail.es', 'yahoo.com', 'yahoo.es');
+    protected static $tld = array('com', 'co', 'com.co', 'net', 'net.co');
+}
