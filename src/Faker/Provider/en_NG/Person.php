698536947bfd4b3f15cde944bf77bf8842867952
wtoalabi <wtoalabi@gmail.com>
2017-11-30 16:22:55 +0100

    Update Nigerian Names

--- src/Faker/Provider/en_NG/Person.php
+++ src/Faker/Provider/en_NG/Person.php
@@ -21,7 +21,7 @@ class Person extends \Faker\Provider\Person
      * @link https://andela-celisha-wigwe.github.io/names.html
      */
     protected static $firstNameMale = array(
-        'Abimbola','Abisola','Abisoye','Adeboye','Adedayo','Adegoke', 'Adewale,' 'Akande', 'Akanni', 'Alade', 'Ayinde', 'Azubuike',
+        'Abimbola','Abisola','Abisoye','Adeboye','Adedayo','Adegoke', 'Adewale', 'Akande', 'Akanni', 'Alade', 'Ayinde', 'Azubuike',
         'Banji', 'Bankole', 'Buchi', 'Bukola',
         'Chinedu', 'Chisom', 'Chukwu',
         'Damilare', 'Damilola', 'Danjuma',
@@ -35,9 +35,9 @@ class Person extends \Faker\Provider\Person
         'Lolade',
         'Makinwa', 'Mohammed', 'Musa', 'Muyiwa',
         'Nnamdi',
-        'Olaide', 'Olawale', 'Olufunmi', 'Olufunso','Olumide', 'Oluwunmi', 'Onoriode',
+        'Olaide', 'Olawale', 'Olufunmi', 'Olufunso','Olumide', 'Oluwunmi', 'Omosalewa','Onoriode',
         'Remilekun', 'Rotimi',
-        'Shade', 'Shalewa', 'Sname',
+        'Shade', 'Shalewa',
         'Tari', 'Temitope', 'Titilope', 'Tobiloba', 'Toke', 'Tomiloba', 'Tope',
         'Uzodimma',
         'Yakubu', 'Yusuf', 'Yusuf',
@@ -65,7 +65,7 @@ class Person extends \Faker\Provider\Person
      * @link https://andela-celisha-wigwe.github.io/names.html
      */
     protected static $lastName = array(
-        'Abiodun', 'Abiola', 'Abodunrin', 'Abosede', 'Adaobi', 'Adebayo', 'Adegboye', 'Adegoke', 'Ademayowa', 'Ademola', 'Adeniyan', 'Adeoluwa', 'Aderinsola', 'Aderonke', 'Adesina', 'Adewale', 'Adewale', 'Adewale', 'Adewunmi', 'Adewura', 'Adeyemo', 'Afolabi', 'Afunku', 'Agboola', 'Agboola', 'Agnes', 'Aigbiniode', 'Ajakaiye', 'Ajose-adeogun', 'Akeem-omosanya', 'Akerele', 'Akintade', 'Aligbe', 'Amaechi', 'Aminat', 'Aremu', 'Atanda', 'Ayisat', 'Ayobami', 'Ayomide', 'Ayomide',
+        'Abiodun', 'Abiola', 'Abodunrin', 'Abosede', 'Adaobi', 'Adebayo', 'Adegboye', 'Adegoke', 'Ademayowa', 'Ademola', 'Adeniyan', 'Adeoluwa', 'Aderinsola', 'Aderinto', 'Aderonke', 'Adesina', 'Adewale', 'Adewale', 'Adewale', 'Adewunmi', 'Adewura', 'Adeyemo', 'Afolabi', 'Afunku', 'Agboola', 'Agboola', 'Agnes', 'Aigbiniode', 'Ajakaiye', 'Ajose-adeogun', 'Akeem-omosanya', 'Akerele', 'Akintade', 'Aligbe', 'Amaechi', 'Aminat', 'Aremu', 'Atanda', 'Ayisat', 'Ayobami', 'Ayomide', 'Ayomide',
         'Babalola', 'Babatunde', 'Balogun', 'Bamisebi', 'Bello', 'Busari',
         'Chibike', 'Chibuike', 'Chidinma', 'Chidozie', 'Christian', 'Clare',
         'David', 'David',
