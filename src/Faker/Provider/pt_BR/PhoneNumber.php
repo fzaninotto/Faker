6aad0a13c1729df1595a39b7186178c8a4e471b9
Jack Miras <jackmiras@gmail.com>
2018-01-11 17:08:06 -0200

    Fixing the implementation of the function that generates Brazilian area codes

--- src/Faker/Provider/pt_BR/PhoneNumber.php
+++ src/Faker/Provider/pt_BR/PhoneNumber.php
@@ -14,12 +14,21 @@ class PhoneNumber extends \Faker\Provider\PhoneNumber
     protected static $cellphoneFormats = array('9####-####');
 
     /**
-     * Generates a 2-digit area code not composed by zeroes.
+     * Pick one random entry out of an array of area codes.
      * @return string
      */
     public static function areaCode()
     {
-        return static::randomDigitNotNull().static::randomDigitNotNull();
+        $areaCodes = array(
+            '11', '12', '13', '14', '15', '16', '17', '18', '19', '21', '22', '24',
+            '27', '28', '31', '32', '33', '34', '35', '37', '38', '41', '42', '43',
+            '44', '45', '46', '47', '48', '49', '51', '53', '54', '55', '61', '62',
+            '63', '64', '65', '66', '67', '68', '69', '71', '73', '74', '75', '77',
+            '79', '81', '82', '83', '84', '85', '86', '87', '88', '89', '91', '92',
+            '93', '94', '95', '96', '97', '98', '99'
+        );
+
+        return array_rand($areaCodes, 1);
     }
 
     /**
