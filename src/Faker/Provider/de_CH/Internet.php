d94d6a44388a8c11b946db0bb5022175167f817e
stoffo <stefan.lange@freenet.ag>
2018-01-18 12:26:03 +0100

    Add datasources for change

--- src/Faker/Provider/de_CH/Internet.php
+++ src/Faker/Provider/de_CH/Internet.php
@@ -4,6 +4,18 @@ namespace Faker\Provider\de_CH;
 
 class Internet extends \Faker\Provider\Internet
 {
-    protected static $freeEmailDomain = array('gmail.com', 'hotmail.com', 'yahoo.com', 'googlemail.com', 'gmx.ch', 'bluewin.ch', 'swissonline.ch');
+    /**
+     * @link https://www.statista.com/statistics/446418/most-popular-e-mail-providers-germany/
+     * @link http://blog.shuttlecloud.com/the-10-most-popular-email-providers-in-germany
+     */
+    protected static $freeEmailDomain = array(
+        'gmail.com',
+        'hotmail.com',
+        'yahoo.com',
+        'googlemail.com',
+        'gmx.ch',
+        'bluewin.ch',
+        'swissonline.ch'
+    );
     protected static $tld = array('com', 'com', 'com', 'net', 'org', 'li', 'ch', 'ch');
 }
