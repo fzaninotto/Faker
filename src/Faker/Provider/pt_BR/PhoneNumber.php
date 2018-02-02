6f3742153946c27cf6e11f67572a5724f98d5fdd
Jack Miras <jackmiras@gmail.com>
2018-02-02 11:36:34 -0200

    Implementation of the function that generates Brazilian area codes fixed. (#1401)
    * Fixing the implementation of the function that generates Brazilian area codes
    
    * Fixing a problem with random element picking in the function areaCode at pt_BR/PhoneNumber.php class
    
    * Improving the areaCode() function documentation and changing the way of how the area code is generated

--- src/Faker/Provider/pt_BR/PhoneNumber.php
+++ src/Faker/Provider/pt_BR/PhoneNumber.php
@@ -4,7 +4,6 @@ namespace Faker\Provider\pt_BR;
 
 class PhoneNumber extends \Faker\Provider\PhoneNumber
 {
-
     protected static $landlineFormats = array('2###-####', '3###-####', '4###-####');
 
     /**
@@ -15,11 +14,21 @@ class PhoneNumber extends \Faker\Provider\PhoneNumber
 
     /**
      * Generates a 2-digit area code not composed by zeroes.
+     * @link http://www.anatel.gov.br/legislacao/resolucoes/16-2001/383-resolucao-263.
      * @return string
      */
     public static function areaCode()
     {
-        return static::randomDigitNotNull().static::randomDigitNotNull();
+        $areaCodes = array(
+            '11', '12', '13', '14', '15', '16', '17', '18', '19', '21', '22', '24',
+            '27', '28', '31', '32', '33', '34', '35', '37', '38', '41', '42', '43',
+            '44', '45', '46', '47', '48', '49', '51', '53', '54', '55', '61', '62',
+            '63', '64', '65', '66', '67', '68', '69', '71', '73', '74', '75', '77',
+            '79', '81', '82', '83', '84', '85', '86', '87', '88', '89', '91', '92',
+            '93', '94', '95', '96', '97', '98', '99'
+        );
+
+        return self::randomElement($areaCodes);
     }
 
     /**
