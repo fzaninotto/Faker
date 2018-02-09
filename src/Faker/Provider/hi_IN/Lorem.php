5a71e947bbfa172ec9b17779783c88ca931045f9
Art Design <artndes9@gmail.com>
2018-02-09 15:05:23 +0530

    Add hi_IN Provider
    Added Lorem, text, .. , provider

---
+++ src/Faker/Provider/hi_IN/Lorem.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Faker\Provider\hi_IN;
+
+class Lorem extends \Faker\Provider\Lorem
+{
+    /**
+     * @var array $wordList Hindi
+     */
+    protected static $wordList = array(
+        'तरीके', 'दुनिया', 'जानकारी', 'सुचनाचलचित्र', 'एछित', 'उनके', 'बातसमय', 'संपुर्ण', 'ध्येय', 'विशेष', 'कोहम', 'अमितकुमार', 'प्रौध्योगिकी', 'पुस्तक', 'सुविधा', 'अनुवादक', 'बारे', 'ढांचा', 'अंतर्गत', 'अधिकांश', 'मार्गदर्शन', 'होगा', 'होसके', 'उनके', 'चिदंश', 'अधिकार', 'अविरोधता', 'प्रति', 'स्थापित', 'पहोचाना', 'कर्य', 'विश्व', 'खयालात', 'सभिसमज', 'पहोचाना', 'रखति', 'दस्तावेज', 'विकेन्द्रित', '७हल', 'चुनने', 'तरहथा', 'वातावरण', 'लेकिन', 'आपके', 'उद्योग', 'पहोचाना', 'अर्थपुर्ण', 'तरीके', 'उनके', 'होसके', 'सारांश', 'मुश्किल', 'भेदनक्षमता', 'देते', 'ढांचा', 'मुश्किल', 'वार्तालाप', 'भाषए', 'वास्तव', 'लाभान्वित', 'प्रोत्साहित', 'लक्षण', 'कीने मजबुत', 'लिये', 'आधुनिक', 'निरपेक्ष', 'सेऔर', 'आवश्यक', 'द्वारा', 'है।अभी', 'पुर्व', 'सहयोग', 'भाति', 'स्थापित', 'निर्माता', 'संस्था', 'परिभाषित', 'तरीके', 'मुखय', 'आवश्यक', 'विज्ञान', 'कराना', 'उनको', 'दौरान', 'अपनि', 'वर्तमान', 'विचरविमर्श', 'आंतरकार्यक्षमता', 'कुशलता', 'सहायता', 'उसीएक्', 'मेमत', 'होसके', 'शीघ्र', 'भाषा', 'दर्शाता', 'वास्तविक', 'मुक्त', 'भारतीय', 'रहारुप', 'असरकारक', 'एछित', 'तरहथा', 'हुआआदी', 'मुखय', 'लेकिन', 'जिम्मे', 'क्षमता', 'विचारशिलता', 'कलइस', 'बाजार अत्यंत', 'व्याख्यान', 'अमितकुमार', 'थातक', 'दौरान', 'डाले', 'बिन्दुओ', 'मुश्किल', 'परिभाषित', 'होने', 'ढांचा', 'कुशलता', 'वैश्विक', 'व्यवहार', 'जाने', 'साधन', 'संपुर्ण', 'जानकारी', 'विवरन', '२४भि', 'विवरन', 'एसेएवं', 'विचारशिलता', 'कलइस', 'ध्येय', 'जिम्मे', 'सोफ़तवेर', 'नवंबर', 'निर्माण', 'अनुवादक', 'मुश्किले', 'जैसी', 'सभीकुछ', 'पुर्णता', 'प्रतिबध', 'जिवन', 'ढांचा', 'शीघ्र', 'अधिक', 'वर्णित', 'उपलब्धता', 'विचरविमर्श', 'बनाकर', 'जिवन', 'गयेगया', 'मार्गदर्शन', 'पुस्तक', 'तकरीबन', 'सक्षम', 'कोहम', 'प्राण', 'मुख्यतह', 'कार्यलय', 'स्वतंत्र', 'कर्य जानकारी', 'अधिकांश', 'संभव', 'सक्षम', 'खयालात', 'समस्याए', 'लिये', 'बढाता', 'कलइस', 'मुख्य', 'परिवहन', 'प्राथमिक', 'संदेश', 'अपने', 'मेंभटृ', 'अंग्रेजी', 'औषधिक', 'ब्रौशर', 'विनिमय', 'जैसी', 'दौरान', 'उसके', 'वर्तमान', 'सुचनाचलचित्र', 'ढांचामात्रुभाषा', 'सुस्पश्ट', 'विकासक्षमता', 'जानते', 'रचना', 'चिदंश', 'शुरुआत', 'जागरुक', 'हार्डवेर', 'काम', 'पहेला', 'सुनत', 'समस्याए', 'अन्तरराष्ट्रीयकरन', 'आवश्यक', 'द्वारा', 'असक्षम', 'जिवन', 'बाटते', 'भाति', 'निरपेक्ष', 'अन्तरराष्ट्रीयकरन', 'बाजार', 'तरहथा', 'वैश्विक', 'प्राण', 'लिए', 'बारे', 'पुर्णता', 'प्राथमिक', 'मुश्किले', 'प्रति कुशलता', 'लाभान्वित', 'प्रति', 'उनको', 'गुजरना', 'यायेका', 'जाता', 'मुख्य', 'सुनत', 'निरपेक्ष', 'लाभान्वित', 'प्राण', 'गटको', 'वर्णन', 'सुविधा', 'आवश्यकत', 'विश्वव्यापि', 'वर्णन', 'उद्योग', 'गटकउसि', 'वर्तमान', 'काम', 'होने', 'बाटते', 'है।अभी', 'एसेएवं', 'सदस्य', 'कैसे', 'आधुनिक', 'असक्षम', 'विश्वव्यापि', 'जिवन', 'होसके', 'प्रमान', 'हार्डवेर लक्षण', 'कलइस', 'अनुकूल', 'निर्देश', 'भोगोलिक', 'अन्य', 'प्रमान', 'प्राथमिक', 'प्राण', 'वर्णित', 'असक्षम', 'संपुर्ण', 'विभाग', 'विषय', 'जानते', 'लेने', 'संस्था', 'सभिसमज', 'समस्याए', 'कोहम', 'प्रदान', 'दुनिया', 'आंतरजाल', 'सदस्य', 'ज्यादा', 'मेंभटृ', 'अपनि', 'बाटते', 'सिद्धांत', 'माहितीवानीज्य', 'सकती', 'सहयोग', 'होने', 'वैश्विक', 'मुश्किले', 'ढांचामात्रुभाषा', 'विवरण', 'बलवान', 'अधिकांश', 'सिद्धांत', 'सार्वजनिक', 'भारत कर्य', 'मुक्त', 'करेसाथ', 'वास्तव', 'आवश्यकत', 'औषधिक', 'एसलिये', 'आंतरकार्यक्षमता', 'समाज', 'चुनने', 'सेऔर', 'जानकारी', 'आंतरकार्यक्षमता', 'अन्य', 'बनाकर', 'प्रमान', 'अमितकुमार', 'विचरविमर्श', 'तरहथा', 'वास्तविक', 'विचरविमर्श', 'बारे', 'करता', 'जनित', 'आधुनिक', 'बातसमय', 'प्राण', 'रखते', 'प्रतिबध', 'व्याख्या', 'पुर्व', 'पुर्व', 'हैं', 'तरहथा', 'अनुवाद', 'व्यवहार', 'बनाना', 'आजपर', 'प्रमान', 'बिन्दुओमे', 'संस्क्रुति', 'करके', 'व्रुद्धि', 'हार्डवेर', 'करके(विशेष', 'संदेश', 'जानते', 'आजपर', 'असक्षम', 'यायेका देखने', 'प्राधिकरन', 'अर्थपुर्ण', 'करने', 'देते', 'मुक्त', 'पासपाई', 'अंतर्गत', 'उपेक्ष', 'वर्णित', 'भाषाओ', 'जैसे', 'विशेष', 'पढाए', 'तरहथा', 'हमेहो', 'विज्ञान', 'विभाग', 'अन्य', 'उद्योग', 'वास्तविक', 'मार्गदर्शन', 'तरहथा', 'रहारुप', 'विकसित', 'बढाता', 'लिये', 'और्४५०', 'आवश्यकत', 'अधिक', 'बाटते', 'देखने', 'चाहे', 'अविरोधता', 'माहितीवानीज्य', 'गएआप', 'गटकउसि', 'बातसमय', 'कराना', 'मर्यादित', 'विभाजनक्षमता', 'लेने', 'औषधिक', 'समाजो', 'काम', 'गयेगया', 'क्षमता', 'उपलब्ध', 'प्राप्त', 'लिए', 'प्राण', 'क्षमता', 'प्रतिबध', 'निरपेक्ष', 'ध्वनि', 'बनाए', 'कलइस', 'दुनिया', 'कार्यसिधान्तो', 'बनाकर', 'उपलब्ध', 'सुचनाचलचित्र', 'मुखय', 'चुनने', 'बाटते', 'रचना', 'परिभाषित', 'अर्थपुर्ण', 'विभाजनक्षमता', 'परस्पर', 'कम्प्युटर', 'सहित', 'विश्व', 'भीयह', 'सुचना', 'प्रमान', 'अनुवादक', 'गोपनीयता', 'केवल', 'तरहथा', 'विश्वास', 'औषधिक', 'उशकी', 'ज्यादा', 'मुख्यतह', 'सीमित', 'बनाए', 'हुआआदी', 'यायेका', 'शीघ्र', 'जिवन', 'सम्पर्क', 'वार्तालाप', 'सहयोग', 'देखने', 'सभिसमज', 'सम्पर्क', 'रिती', 'कार्य', 'सुनत', 'प्राथमिक', 'ढांचामात्रुभाषा', 'कराना', 'सकता', 'अनुवाद', 'सम्पर्क', 'मर्यादित', 'भारतीय', 'जागरुक', 'व्रुद्धि', 'वर्णन', 'मुखय', 'हिंदी', 'आपको', 'स्थिति', 'उदेशीत', 'रखते', 'आवश्यकत', 'सुस्पश्ट', 'संसाध', 'सारांश', 'हुएआदि', 'रखति', 'प्रति', 'उपलब्ध', 'शारिरिक', 'निर्माता', 'लोगो', 'सीमित', 'सीमित', 'पुस्तक', 'हमेहो', 'आपको', 'विभाग', 'मर्यादित', 'स्वतंत्रता', 'बहतर', 'जिसकी', 'सकती', 'प्रदान', 'लचकनहि', 'क्षमता', 'माहितीवानीज्य', 'आपको', 'शीघ्र', 'आपको', 'दौरान', 'अनुकूल', 'व्रुद्धि',
+    );
+
+	protected static $encoding = 'UTF-8';
+
+	/**
+     * @example 'बहतर'
+     * @return string
+     */
+    public static function word()
+    {
+        return static::randomElement(static::$wordList);
+    }
+
+    /**
+     * Generate an array of random words
+     *
+     * @example array('आवश्यकत', 'प्राथमिक', 'लचकनहि')
+     * @param  integer      $nb     how many words to return
+     * @param  bool         $asText if true the sentences are returned as one string
+     * @return array|string
+     */
+    public static function words($nb = 3, $asText = false)
+    {
+        $words = array();
+        for ($i=0; $i < $nb; $i++) {
+            $words []= static::word();
+        }
+
+        return $asText ? implode(' ', $words) : $words;
+    }
+
+    /**
+     * Generate a random sentence
+     *
+     * @example 'भेदनक्षमता व्यवहार मेंभटृ हुएआदि वार्तालाप आधुनिक सीमित काम संभव।'
+     * @param integer $nbWords         around how many words the sentence should contain
+     * @param boolean $variableNbWords set to false if you want exactly $nbWords returned,
+     *                                  otherwise $nbWords may vary by +/-40% with a minimum of 1
+     * @return string
+     */
+    public static function sentence($nbWords = 6, $variableNbWords = true)
+    {
+        if ($nbWords <= 0) {
+            return '';
+        }
+        if ($variableNbWords) {
+            $nbWords = self::randomizeNbElements($nbWords);
+        }
+
+        $words = static::words($nbWords);
+		// Hindi has no Capitalization
+
+        return implode($words, ' ') . '।';
+    }
+
+    /**
+     * Generate an array of sentences
+     *
+     * @example array('मेंभटृ हुएआदि वार्तालाप आधुनिक सीमित संभव।', 'आपको स्थापित लिए प्रति कुशलता।')
+     * @param  integer      $nb     how many sentences to return
+     * @param  bool         $asText if true the sentences are returned as one string
+     * @return array|string
+     */
+    public static function sentences($nb = 3, $asText = false)
+    {
+        $sentences = array();
+        for ($i=0; $i < $nb; $i++) {
+            $sentences []= static::sentence();
+        }
+
+        return $asText ? implode(' ', $sentences) : $sentences;
+    }
+
+    /**
+     * Generate a single paragraph
+     *
+      * @example 'भारतीय अधिक स्वतंत्र आवश्यकत सीमित कुशलता काम। सुनत पुर्व रखति जाने कलइस क्षमता व्यवहार सहयोग। '
+     * @param integer $nbSentences         around how many sentences the paragraph should contain
+     * @param boolean $variableNbSentences set to false if you want exactly $nbSentences returned,
+     *                                      otherwise $nbSentences may vary by +/-40% with a minimum of 1
+     * @return string
+     */
+    public static function paragraph($nbSentences = 3, $variableNbSentences = true)
+    {
+        if ($nbSentences <= 0) {
+            return '';
+        }
+        if ($variableNbSentences) {
+            $nbSentences = self::randomizeNbElements($nbSentences);
+        }
+
+        return implode(static::sentences($nbSentences), ' ');
+    }
+
+    /**
+     * Generate an array of paragraphs
+     *
+     * @example array($paragraph1, $paragraph2, $paragraph3)
+     * @param  integer      $nb     how many paragraphs to return
+     * @param  bool         $asText if true the paragraphs are returned as one string, separated by two newlines
+     * @return array|string
+     */
+    public static function paragraphs($nb = 3, $asText = false)
+    {
+        $paragraphs = array();
+        for ($i=0; $i < $nb; $i++) {
+            $paragraphs []= static::paragraph();
+        }
+
+        return $asText ? implode("\n\n", $paragraphs) : $paragraphs;
+    }
+
+    /**
+     * Generate a text string.
+     * Depending on the $maxNbChars, returns a string made of words, sentences, or paragraphs.
+     *
+     * @example 'आवश्यकत विशेष है।अभी विचरविमर्श हार्डवेर प्राण अनुवाद माहितीवानीज्य समस्याए असक्षम।  वातावरण वर्तमान * 			उनको सुचनाचलचित्र जिवन अधिकांश भाति भेदनक्षमता सिद्धांत मुख्य सभीकुछ कोहम हुआआदी उद्योग उनके 	
+     * 			 अनुवाद गटको।
+     *
+     * @param  integer $maxNbChars Maximum number of characters the text should contain (minimum 5)
+     *
+     * @return string
+     */
+    public static function text($maxNbChars = 200)
+    {
+        if ($maxNbChars < 5) {
+            throw new \InvalidArgumentException('text() can only generate text of at least 5 characters');
+        }
+
+        $type = ($maxNbChars < 25) ? 'word' : (($maxNbChars < 100) ? 'sentence' : 'paragraph');
+
+        $text = array();
+        while (empty($text)) {
+            $size = 0;
+
+            // until $maxNbChars is reached
+            while ($size < $maxNbChars) {
+                $word   = ($size ? ' ' : '') . static::$type();
+                $text[] = $word;
+
+                $size += strlen($word);
+            }
+
+            array_pop($text);
+        }
+
+        if ($type === 'word') {
+        	// Hindi has no Capitalization
+
+
+            // end sentence with full stop
+            $text[count($text) - 1] .= '।';
+        }
+
+        return implode($text, '');
+    }
+
+    protected static function randomizeNbElements($nbElements)
+    {
+        return (int) ($nbElements * mt_rand(60, 140) / 100) + 1;
+    }
+}
