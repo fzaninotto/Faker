a498a55cded0b82328cbd3d8a2194124e79cc752
Art Design <artndes9@gmail.com>
2018-02-09 15:45:19 +0530

    Removed internet
    Redundant since en_IN already present in library

--- src/Faker/Provider/hi_IN/Internet.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Faker\Provider\hi_IN;
-
-class Internet extends \Faker\Provider\Internet
-{
-    protected static $freeEmailDomain = array('gmail.com', 'yahoo.com', 'hotmail.com', 'yahoo.co.in', 'rediffmail.com');
-    protected static $tld = array('भारत', 'भारत', 'भारत', 'भारत', 'भारत', 'भारत', 'com', 'in', 'in', 'in', 'ac.in', 'net', 'org', 'co.in');
-}
