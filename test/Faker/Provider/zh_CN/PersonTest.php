d4c888f487d5c88d52f0175747b8f73cd7cb3052
Jialei <cuiijalei@megvii.com>
2018-02-14 18:26:37 +0800

    add id number provider for zh_CN

---
+++ test/Faker/Provider/zh_CN/PersonTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Faker\Test\Provider\zh_CN;
+
+use Faker\Generator;
+use Faker\Provider\zh_CN\Person;
+use PHPUnit\Framework\TestCase;
+
+class PersonTest extends TestCase
+{
+    /**
+     * @var Generator
+     */
+    private $faker;
+
+    public function setUp()
+    {
+        $faker = new Generator();
+        $faker->addProvider(new Person($faker));
+        $this->faker = $faker;
+    }
+
+    public function testIdNumber()
+    {
+        $id = $this->faker->idNumber;
+        $this->assertRegExp('/(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}$)/', $id);
+    }
+}
