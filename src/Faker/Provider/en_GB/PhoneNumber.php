ee1bf707f3dbbff8916deacf3a5a48944230b9e3
Dan Walker <dwalker@wonde.com>
2018-02-22 08:40:55 +0000

    Adds en_GB formatters for safe phone and mobile phone numbers

--- src/Faker/Provider/en_GB/PhoneNumber.php
+++ src/Faker/Provider/en_GB/PhoneNumber.php
@@ -22,6 +22,27 @@ class PhoneNumber extends \Faker\Provider\PhoneNumber
     );
 
     /**
+     * An array of en_GB safe (reserved for dramatic use) phone number formats
+     * @var array
+     */
+    protected static $safeFormats = array(
+        '+44(0)1632######',
+        '+44(0)1632 ######',
+        '+44(0)1632#####',
+        '+44(0)1632 #####',
+        '01632######',
+        '01632#####',
+        '01632 ######',
+        '01632 #####',
+        '0163 2## ####',
+        '0163 2######',
+        '(01632) ######',
+        '(01632) #####',
+        '(0163) 2## ####',
+        '(0163) 2######',
+    );
+
+    /**
      * An array of en_GB mobile (cell) phone number formats
      * @var array
      */
@@ -33,11 +54,39 @@ class PhoneNumber extends \Faker\Provider\PhoneNumber
     );
 
     /**
+     * An array of en_GB safe (reserved for dramatic use) mobile (cell) phone number formats
+     * @var array
+     */
+    protected static $safeMobileFormats = array(
+        '07700900###',
+        '07700 900###',
+        '07700 900 ###'
+    );
+
+    /**
+     * Return a en_GB safe phone number
+     * @return string
+     */
+    public static function safePhoneNumber()
+    {
+        return static::numerify(static::randomElement(static::$safeFormats));
+    }
+
+    /**
      * Return a en_GB mobile phone number
      * @return string
      */
     public static function mobileNumber()
     {
         return static::numerify(static::randomElement(static::$mobileFormats));
+    }
+
+    /**
+     * Return a en_GB safe mobile phone number
+     * @return string
+     */
+    public static function safeMobileNumber()
+    {
+        return static::numerify(static::randomElement(static::$safeMobileFormats));
     }
 }
