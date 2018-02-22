60d7331a559f91ee40e1eb414aa3e8ec423353ea
z38 <z38@users.noreply.github.com>
2018-02-22 17:38:57 +0100

    Add German job titles

--- src/Faker/Provider/de_CH/Company.php
+++ src/Faker/Provider/de_CH/Company.php
@@ -2,7 +2,7 @@
 
 namespace Faker\Provider\de_CH;
 
-class Company extends \Faker\Provider\Company
+class Company extends \Faker\Provider\de_DE\Company
 {
     protected static $formats = array(
         '{{lastName}} {{companySuffix}}',
