21cf8802c53433858d99015a004c1f16f91c94c5
David <code@imdavid.xyz>
2018-04-09 11:04:36 +0300

    Estonian names

---
+++ src/Faker/Provider/et_EE/Person.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Faker\Provider\et_EE;
+
+/**
+ * Class Person
+ * @package Faker\Provider\et_EE
+ * @author David Gegelija <code@imdavid.xyz>
+ */
+class Person extends \Faker\Provider\Person
+{
+    /**
+     * @link https://news.err.ee/114745/most-popular-baby-names-of-2014
+     */
+    protected static $firstNameMale = array(
+        'Rasmus', 'Artjom', 'Robin', 'Martin', 'Oliver', 'Romet', 'Sebastian', 'Robert',
+        'Artur', 'Maksim', 'Markus', 'Marten', 'Karl', 'Kristofer', 'Oskar', 'Daniel',
+        'Hugo', 'Henri', 'Mark', 'Nikita', 'Kirill', 'Sander', 'Kevin', 'Aleksandr', 'Daniil'
+    );
+
+    /**
+     * @link https://news.err.ee/114745/most-popular-baby-names-of-2014
+     */
+    protected static $firstNameFemale = array(
+        'Sofia', 'Eliise', 'Maria', 'Mia', 'Lisandra', 'Mirtel', 'Sandra', 'Emma', 'Laura',
+        'Darja', 'Arina', 'Milana', 'Alisa', 'Anastasia', 'Lenna', 'Liisa', 'Anna', 'Viktoria',
+        'Elisabeth', 'Polina', 'Marta', 'Aleksandra', 'Marleen', 'Hanna', 'Nora'
+    );
+
+    /**
+     * @link https://en.wikipedia.org/wiki/Category:Estonian-language_surnames
+     */
+    protected static $lastName = array(
+        'Aare', 'Aarma', 'Aas', 'Aasmäe', 'Aav', 'Aavik', 'Allik', 'Alver', 'Eenpalu', 'Eskola',
+        'Härma', 'Ilves', 'Jakobson', 'Jänes', 'Järve', 'Järvis', 'Jõgi', 'Kaaleste', 'Kaasik',
+        'Käbin', 'Kalda', 'Kaljulaid', 'Kaljurand', 'Kallas', 'Kallaste', 'Kangro', 'Kangur',
+        'Kapp', 'Kärner', 'Kask', 'Käsper', 'Kass', 'Keres', 'Keskküla', 'Kesküla', 'Kikkas',
+        'Kingsepp', 'Kirs', 'Kirsipuu', 'Kivi', 'Klavan', 'Kõiv', 'Kokk', 'Kontaveit', 'Koppel',
+        'Korjus', 'Kotkas', 'Kreek', 'Kross', 'Kruus', 'Kukk', 'Kütt', 'Kuusik', 'Kuusk', 'Laar',
+        'Laht', 'Lass', 'Laur', 'Laurits', 'Lemsalu', 'Lepik', 'Lepmets', 'Lepp', 'Levandi',
+        'Liiv', 'Lill', 'Lindmaa', 'Linna', 'Lipp', 'Lippmaa', 'Lõhmus', 'Loo', 'Lõoke', 'Luik',
+        'Luts', 'Mägi', 'Mälk', 'Mänd', 'Männik', 'Margiste', 'Mark', 'Masing', 'Mets', 'Must',
+        'Nõmmik', 'Nurme', 'Nurmsalu', 'Oja', 'Olesk', 'Öpik', 'Orav', 'Pääsuke', 'Padar', 'Pärn',
+        'Peebo', 'Peetre', 'Pihlak', 'Piho', 'Piip', 'Põder', 'Põld', 'Poska', 'Puhvel', 'Pütsep',
+        'Puusepp', 'Raag', 'Rand', 'Raud', 'Raudsepp', 'Rebane', 'Reek', 'Reinsalu', 'Rooba',
+        'Roolaid', 'Rootare', 'Rummo', 'Rüütel', 'Rüütli', 'Saar', 'Salumäe', 'Sepp', 'Sibul',
+        'Siimar', 'Simm', 'Sirel', 'Sisask', 'Sokk', 'Soosaar', 'Susi', 'Talts', 'Tamme',
+        'Tarvas', 'Teder', 'Toom', 'Toome', 'Toots', 'Vaher', 'Vahtra', 'Vaino', 'Vainola',
+        'Välbe', 'Valdma', 'Väljas', 'Valk', 'Vesik', 'Veski', 'Viiding', 'Vitsut', 'Võigemast',
+        'Võsu',
+    );
+}
