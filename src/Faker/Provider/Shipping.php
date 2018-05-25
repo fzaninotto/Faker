b9821392c9906cace2fdb3dd8966d25095402516
Gabor Tjong A Hung <g.v.tjongahung@iqreate.com>
2018-05-25 13:08:26 -0300

    Corrected the format of the UPS code

--- src/Faker/Provider/Shipping.php
+++ src/Faker/Provider/Shipping.php
@@ -14,8 +14,35 @@ class Shipping extends Base
      */
     public function ups()
     {
-        $formats = array('1Z################','############','T##########','#########');
+        $formats = array('1Z******##########','############','T##########','#########');
+        return static::bothify($this->generator->parse(static::randomElement($formats)));
+    }
+
+    public function fedex()
+    {
+        $formats = array('############','##############','#################');
         return static::numerify($this->generator->parse(static::randomElement($formats)));
+    }
+
+    public function usps()
+    {
+        $formats = array('####################','??#########US');
+        return static::bothify($this->generator->parse(static::randomElement($formats)));
+    }
+
+    public function dhl()
+    {
+        $formats = array('##########');
+        return static::numerify($this->generator->parse(static::randomElement($formats)));
+    }
+
+    public function tracking_number(){
+        return $this->ups();
+    }
+    public function amazon_tracking_number(){
+        //call_user_func();
+        $shipping_companies = ["UPS(".$this->ups().")", "FedEx(".$this->fedex().")", "DHL(".$this->dhl().")"];
+        return $shipping_companies[mt_rand(0, count($shipping_companies) - 1)];
     }
 
 }
