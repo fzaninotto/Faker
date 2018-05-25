f59a7c5226960220706a00b703e8d16358da3216
Gabor Tjong A Hung <g.v.tjongahung@iqreate.com>
2018-05-24 21:06:11 -0300

    Added shipping codes for UPS

---
+++ src/Faker/Provider/Shipping.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Faker\Provider;
+
+
+class Shipping extends Base
+{
+    /**
+     * @example
+     * 1Z9999999999999999
+     * 999999999999
+     * T9999999999
+     * 999999999
+     */
+    public function ups()
+    {
+        $formats = array('1Z################','############','T##########','#########');
+        return static::numerify($this->generator->parse(static::randomElement($formats)));
+    }
+    
+}
