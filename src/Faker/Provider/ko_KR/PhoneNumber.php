06810ecf3a53199fd80c7037d260b30024d4779e
Enosh Yu <yjh6079@gmail.com>
2018-06-14 10:07:21 +0900

    add phone numer format

--- src/Faker/Provider/ko_KR/PhoneNumber.php
+++ src/Faker/Provider/ko_KR/PhoneNumber.php
@@ -14,4 +14,32 @@ class PhoneNumber extends \Faker\Provider\PhoneNumber
         '06#-####-####',
         '1588-####',
     );
+    
+    protected static $cellPhoneFormats = array(
+        '010-####-####',
+    );
+    
+    public function cellPhoneNumber()
+    {
+        $format = self::randomElement(static::$cellPhoneFormats);
+
+        return self::numerify($this->generator->parse($format));
+    }
+    
+    protected static $commonPhoneFormats = array(
+        // Standard formats
+        '070-####-####',
+        '02-####-####',
+        '03#-####-####',
+        '04#-####-####',
+        '05#-####-####',
+        '06#-####-####',
+    );
+    
+    public function commonPhoneNumber()
+    {
+        $format = self::randomElement(static::$commonPhoneFormats);
+
+        return self::numerify($this->generator->parse($format));
+    }
 }
