7031e7bf685a659838595dcc2596c94138faee17
Abdujabbor <abdujabbor1987@gmail.com>
2018-08-03 14:14:13 +0500

    added mobile phone numbers generators in uz_UZ

---
+++ src/Faker/Provider/uz_UZ/PhoneNumber.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abdujabbor
+ * Date: 8/3/18
+ * Time: 1:51 PM
+ */
+
+namespace Faker\Provider\uz_UZ;
+
+
+class PhoneNumber extends \Faker\Provider\PhoneNumber
+{
+    protected static $formats = array(
+        '+998 (90) #######',
+        '+998 (91) #######',
+        '+998 (93) #######',
+        '+998 (94) #######',
+        '+998 (97) #######',
+        '+998 (98) #######',
+        '+998 (99) #######',
+    );
+}
