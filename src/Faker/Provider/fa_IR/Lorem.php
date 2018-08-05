b7d57613be21aef65440f922933973e5eda3a523
Mahdi Abedi <mahdiabedi220@yahoo.com>
2018-08-05 10:02:19 +0430

    persian(farsi) lorem
    Persian language was added to lorem

---
+++ src/Faker/Provider/fa_IR/Lorem.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace Faker\Provider\fa_IR;
+
+class Lorem extends \Faker\Provider\Lorem
+{
+    /**
+     * generates text string in arabic
+     *
+     * @author 'مهدی عابدی کردلر'
+     * @param  integer                   $maxNbChars
+     * @param  integer                   $indexSize
+     * @return string
+     * @email 'mahdiabedi220@yahoo.com'
+     */
+    protected static $wordList = array(
+        'سلام', 'بر ابراهیم', 'دعوت', 'پروژه', 'لیست', 'کامپیوتر',
+        'متن', 'استاندارد', 'دعوت', 'تعاون','شهید', 'رئیس', 'آزمایشگاه',
+        'کلمه', 'شرکت', 'کارمند', 'حقوق', 'اعتبار', 'بیمه',
+        'دسترسی', 'مترو', 'ماشین', 'اتوبوس', 'دوچرخه', 'کفش', 'میز',
+        'صندلی', 'مدرسه', 'متاهل', 'مجرد', 'خانم', 'آقا', 'وزیر',
+        'پلیس', 'وکیل', 'قاضی', 'چاپ', 'رژیم', 'غذا', 'صبحانه',
+        'ناهار', 'شام', 'عصرانه', 'تلویزیون', 'فیلم', 'سریال', 'املت',
+        'دکتر', 'مهندس', 'معلم', 'منشی', 'برنامه نویس', 'سایت', 'معمار',
+        'اسنپ', 'تپسی', 'کرانه', 'دریا', 'ساحل', 'ویلا', 'خانه', 'آپارتمان',
+        'کانکس', 'پراید', 'پژو', 'بنز', 'کامیون', 'اتوبوس', 'مینی بوس',
+        'فناوری اطلاعات', 'راه و ساختمان', 'توسعه معادن', 'استخراج و اکتشاف',
+        'سرمایه گذاری', 'نساجی', 'کاریابی', 'تجهیزات اداری', 'تولیدی', 'فولاد',
+        'رسمی', 'پیمانی', 'تمام وقت', 'پاره وقت', 'پروژه ای', 'ساعتی',
+        'ساعت', 'دیر', 'زود', 'رمان', 'کتاب', 'کتابخانه', 'مدرسه',
+        'درمانگاه', 'بیمارستان', 'دیوانه خانه', 'ایلیاوب', 'خانه رویایی', 'مهدی عابدی', 'کریم تو مسلمان نیستی',
+        'حافظ', 'سعدی', 'کباب', 'کبابپز', 'استخر', 'ورزشگاه',
+        'جام جهانی', 'استخدام', 'دندانپزشک', 'آنتیک', 'اتیقه', 'تانک',
+        'تفنگ', 'گلوله', 'دعا', 'قرآن', 'خدا', 'نماز',
+        'شب یلدا', 'شب قدر', 'دعای کمیل', 'مسئول', 'دروغ', 'دروغگو',
+        'دولت', 'گرانی', 'کرمان', 'روحانی',  "آذربایجان شرقی", "آذربایجان غربی", "اردبیل",
+         "اصفهان", "البرز", "ایلام", "بوشهر",
+        "تهران", "خراسان جنوبی", "خراسان رضوی", "خراسان شمالی", "خوزستان", "زنجان", "سمنان",
+        "سیستان و بلوچستان", "فارس", "قزوین", "قم", "لرستان", "مازندران", "مرکزی", "هرمزگان",
+        "همدان", "چهارمحال و بختیاری", "کردستان", "کرمان", "کرمانشاه", "کهگیلویه و بویراحمد",
+        "گلستان", "گیلان", "یزد",
+        'راسخ', 'راوندی', 'رجایی', 'رحماندوست', 'رحمانیان', 'رستمی', 'رسولی', 'رفیعی', 'رنجبر', 'رنگرز', 'رهنما', 'روحانی', 'روزبه', 'روستا',
+        'زارع', 'زالی', 'زرشناس', 'زمردیان', 'زنجانی', 'زنوزی', 'زهرایی', 'زین‌الدین',
+        'سادات', 'ساعی', 'سبحانی', 'سبزواری', 'ستاری', 'سحاب', 'سحابی', 'سراج', 'سرشار', 'سرمد', 'سروستانی', 'سروش', 'سلامت', 'سپه‌وند', 'سیف', 'سیف‌زاده',
+        'شادمهر', 'شاملو', 'شاه‌حسینی', 'شبستری', 'شجاعی', 'شرع‌پسند', 'شرف', 'شریعتمداری', 'شریعتی', 'شریف', 'شریفیان', 'شعبانی', 'شفا', 'شهیدی', 'شیخ‌الاسلامی', 'شیدا', 'شیرازی', 'شیرمحمدی', 'شیروانی',
+        'کاشی', 'کاملی', 'کامکار', 'کاویانی', 'کاکاوند', 'کدیور', 'کرمانی', 'کریمی', 'کلباسی', 'کمالی', 'کهنمویی', 'کوشکی', 'کیان', 'کیانی', 'کیمیایی',
+        'گل', 'گلپایگانی', 'گنجی',
+        'صانعی', 'صباغ', 'صدر', 'صدیق', 'صدیقی', 'صغیری', 'صفوی',
+        'ضابطی', 'ضرغامی',
+    );
+
+    /**
+     * @example 'Lorem'
+     * @return string
+     */
+    public static function word()
+    {
+        return static::randomElement(static::$wordList);
+    }
+
+    /**
+     * Generate an array of random words
+     *
+     * @example array('Lorem', 'ipsum', 'dolor')
+     * @param  integer      $nb     how many words to return
+     * @param  bool         $asText if true the sentences are returned as one string
+     * @return array|string
+     */
+    public static function words($nb = 3, $asText = false)
+    {
+        $words = array();
+        for ($i=0; $i < $nb; $i++) {
+            $words []= static::word();
+        }
+
+        return $asText ? implode(' ', $words) : $words;
+    }
+
+    /**
+     * Generate a random sentence
+     *
+     * @example 'Lorem ipsum dolor sit amet.'
+     * @param integer $nbWords         around how many words the sentence should contain
+     * @param boolean $variableNbWords set to false if you want exactly $nbWords returned,
+     *                                  otherwise $nbWords may vary by +/-40% with a minimum of 1
+     * @return string
+     */
+    public static function sentence($nbWords = 6, $variableNbWords = true)
+    {
+        if ($nbWords <= 0) {
+            return '';
+        }
+        if ($variableNbWords) {
+            $nbWords = self::randomizeNbElements($nbWords);
+        }
+
+        $words = static::words($nbWords);
+        $words[0] = ucwords($words[0]);
+
+        return implode($words, ' ') . '.';
+    }
+
+    /**
+     * Generate an array of sentences
+     *
+     * @example array('Lorem ipsum dolor sit amet.', 'Consectetur adipisicing eli.')
+     * @param  integer      $nb     how many sentences to return
+     * @param  bool         $asText if true the sentences are returned as one string
+     * @return array|string
+     */
+    public static function sentences($nb = 3, $asText = false)
+    {
+        $sentences = array();
+        for ($i=0; $i < $nb; $i++) {
+            $sentences []= static::sentence();
+        }
+
+        return $asText ? implode(' ', $sentences) : $sentences;
+    }
+
+    /**
+     * Generate a single paragraph
+     *
+      * @example 'Sapiente sunt omnis. Ut pariatur ad autem ducimus et. Voluptas rem voluptas sint modi dolorem amet.'
+     * @param integer $nbSentences         around how many sentences the paragraph should contain
+     * @param boolean $variableNbSentences set to false if you want exactly $nbSentences returned,
+     *                                      otherwise $nbSentences may vary by +/-40% with a minimum of 1
+     * @return string
+     */
+    public static function paragraph($nbSentences = 3, $variableNbSentences = true)
+    {
+        if ($nbSentences <= 0) {
+            return '';
+        }
+        if ($variableNbSentences) {
+            $nbSentences = self::randomizeNbElements($nbSentences);
+        }
+
+        return implode(static::sentences($nbSentences), ' ');
+    }
+
+    /**
+     * Generate an array of paragraphs
+     *
+     * @example array($paragraph1, $paragraph2, $paragraph3)
+     * @param  integer      $nb     how many paragraphs to return
+     * @param  bool         $asText if true the paragraphs are returned as one string, separated by two newlines
+     * @return array|string
+     */
+    public static function paragraphs($nb = 3, $asText = false)
+    {
+        $paragraphs = array();
+        for ($i=0; $i < $nb; $i++) {
+            $paragraphs []= static::paragraph();
+        }
+
+        return $asText ? implode("\n\n", $paragraphs) : $paragraphs;
+    }
+
+    /**
+     * Generate a text string.
+     * Depending on the $maxNbChars, returns a string made of words, sentences, or paragraphs.
+     *
+     * @example 'Sapiente sunt omnis. Ut pariatur ad autem ducimus et. Voluptas rem voluptas sint modi dolorem amet.'
+     *
+     * @param  integer $maxNbChars Maximum number of characters the text should contain (minimum 5)
+     *
+     * @return string
+     */
+    public static function text($maxNbChars = 200)
+    {
+        if ($maxNbChars < 5) {
+            throw new \InvalidArgumentException('text() can only generate text of at least 5 characters');
+        }
+
+        $type = ($maxNbChars < 25) ? 'word' : (($maxNbChars < 100) ? 'sentence' : 'paragraph');
+
+        $text = array();
+        while (empty($text)) {
+            $size = 0;
+
+            // until $maxNbChars is reached
+            while ($size < $maxNbChars) {
+                $word   = ($size ? ' ' : '') . static::$type();
+                $text[] = $word;
+
+                $size += strlen($word);
+            }
+
+            array_pop($text);
+        }
+
+        if ($type === 'word') {
+            // capitalize first letter
+            $text[0] = ucwords($text[0]);
+
+            // end sentence with full stop
+            $text[count($text) - 1] .= '.';
+        }
+
+        return implode($text, '');
+    }
+
+    protected static function randomizeNbElements($nbElements)
+    {
+        return (int) ($nbElements * mt_rand(60, 140) / 100) + 1;
+    }
+}
