c11a63b75158b70f030e79917da82ecbaf6cd3dd
Juanan Ruiz <juanan@us.es>
2018-08-18 18:43:55 +0200

    Spanish Web Safe Color Names

---
+++ src/Faker/Provider/es_ES/Color.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Faker\Provider\es_ES;
+
+class Color extends \Faker\Provider\Color
+{
+    protected static $safeColorNames = array(
+        'negro', 'marrón', 'verde', 'azul marino', 'oliva',
+        'morado', 'turquesa', 'lima', 'azul', 'plata',
+        'gris', 'amarillo', 'magenta', 'celeste', 'blanco'
+    );
+
+}
