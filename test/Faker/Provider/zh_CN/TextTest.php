1717a38bed6e8414138c2bf63d619f216510a79a
Ganlv <ganlvtech@qq.com>
2018-10-04 22:30:28 +0800

    Rebuild zh_CN Text provider. Use new baseText. Add punctuation pair support.

--- test/Faker/Provider/zh_CN/TextTest.php
+++ test/Faker/Provider/zh_CN/TextTest.php
@@ -3,18 +3,42 @@
 namespace Faker\Test\Provider\zh_CN;
 
 use Faker\Factory;
+use Faker\Provider\zh_CN\Text;
 use PHPUnit\Framework\TestCase;
 
 class TextTest extends TestCase
 {
+    protected static $class = 'Faker\Provider\zh_CN\Text';
+
     protected static function getEncoding()
     {
-        return static::readAttribute('Faker\Provider\zh_CN\Text', 'encoding');
+        return static::readAttribute(static::$class, 'encoding');
+    }
+
+    protected static function getBaseText()
+    {
+        return static::readAttribute(static::$class, 'baseText');
     }
 
     public static function getGenerator()
     {
         return Factory::create('zh_CN');
+    }
+
+    protected static function mb_substr($str, $start, $length = null)
+    {
+        return mb_substr($str, $start, $length, static::getEncoding());
+    }
+
+    protected static function mb_strlen($str)
+    {
+        return mb_strlen($str, static::getEncoding());
+    }
+
+    public function testUtf8Len()
+    {
+        $baseText = static::getBaseText();
+        $this->assertEquals(static::mb_strlen($baseText), TextProxy::utf8_strlen_public($baseText));
     }
 
     public function testRealTextNoSpacing()
@@ -25,19 +49,41 @@ class TextTest extends TestCase
 
     public function testRealTextEndPunctuation()
     {
-        $notEndPunct = $this->readAttribute('Faker\Provider\zh_CN\Text', 'notEndPunct');
-        $endPunct = $this->readAttribute('Faker\Provider\zh_CN\Text', 'endPunct');
         $faker = static::getGenerator();
 
-        $this->assertTrue(in_array(mb_substr($faker->realText(), -1, 1, static::getEncoding()), $endPunct));
-        $this->assertFalse(in_array(mb_substr($faker->realText(), -1, 1, static::getEncoding()), $notEndPunct));
+        $notEndPunctuations = static::readAttribute(static::$class, 'notEndPunctuations');
+        $this->assertFalse(in_array(static::mb_substr($faker->realText(), -1, 1), $notEndPunctuations));
+
+        $punctuations = static::readAttribute(static::$class, 'punctuations');
+        $this->assertTrue(in_array(static::mb_substr($faker->realText(), -1, 1), $punctuations));
     }
 
     public function testRealTextBeginPunctuation()
     {
-        $notBeginPunct = $this->readAttribute('Faker\Provider\zh_CN\Text', 'notBeginPunct');
         $faker = static::getGenerator();
 
-        $this->assertFalse(in_array(mb_substr($faker->realText(), 0, 1, static::getEncoding()), $notBeginPunct));
+        $notBeginPunctuations = static::readAttribute(static::$class, 'notBeginPunctuations');
+        $this->assertFalse(in_array(static::mb_substr($faker->realText(), 0, 1), $notBeginPunctuations));
+    }
+
+    public function testRealTextPunctuationPair()
+    {
+        $faker = static::getGenerator();
+
+        $text = $faker->realText();
+        $punctuationPairs = static::readAttribute(static::$class, 'punctuationPairs');
+        foreach ($punctuationPairs as $left => $right) {
+            $leftCount = substr_count($text, $left);
+            $rightCount = substr_count($text, $right);
+            $this->assertEquals($leftCount, $rightCount);
+        }
+    }
+}
+
+class TextProxy extends Text
+{
+    public static function utf8_strlen_public($text)
+    {
+        return static::utf8_strlen($text);
     }
 }
