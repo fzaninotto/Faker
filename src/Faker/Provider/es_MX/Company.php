695e122a8191b72f0bcb9942755dfbc5bf7fac4e
Marco Rivadeneyra <marcorivm@gmail.com>
2018-11-20 18:25:10 -0600

    No need to redefine catchPhrase or bs methods if nothing is changed

--- src/Faker/Provider/es_MX/Company.php
+++ src/Faker/Provider/es_MX/Company.php
@@ -4,39 +4,5 @@ namespace Faker\Provider\es_MX;
 
 class Company extends \Faker\Provider\es_ES\Company
 {
-    protected static $companySuffix = array('y Asoc.', 'SA', 'SA. de CV.');
-
-    /**
-     * @example 'Grupo'
-     */
-    public static function companyPrefix()
-    {
-        return static::randomElement(static::$companyPrefix);
-    }
-
-    /**
-     * @example 'Robust full-range hub'
-     */
-    public function catchPhrase()
-    {
-        $result = array();
-        foreach (static::$catchPhraseWords as &$word) {
-            $result[] = static::randomElement($word);
-        }
-
-        return join($result, ' ');
-    }
-
-    /**
-     * @example 'integrate extensible convergence'
-     */
-    public function bs()
-    {
-        $result = array();
-        foreach (static::$bsWords as &$word) {
-            $result[] = static::randomElement($word);
-        }
-
-        return join($result, ' ');
-    }
+    protected static $companySuffix = array('y Asoc.', 'SA', 'SA. de CV.', 'SRL', 'SAPI');
 }
