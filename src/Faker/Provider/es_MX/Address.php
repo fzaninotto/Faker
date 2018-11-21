12a5ee64a1e9af6a7d8270c49b9cea64d9ebe978
Marco Rivadeneyra <marcorivm@gmail.com>
2018-11-20 18:22:43 -0600

    Split states list by first letter

--- src/Faker/Provider/es_MX/Address.php
+++ src/Faker/Provider/es_MX/Address.php
@@ -12,7 +12,25 @@ class Address extends \Faker\Provider\es_ES\Address
     );
     protected static $postcode = array('#####');
     protected static $state = array(
-        'Aguascalientes','Baja California','Baja California Sur','Campeche','Chiapas','Chihuahua','Coahuila','Colima','Distrito Federal','Durango','Estado de México','Guanajuato','Guerrero','Hidalgo','Jalisco','Michoacán','Morelos','Nayarit','Nuevo León','Oaxaca','Puebla','Querétaro','Quintana Roo','San Luis Potosí','Sinaloa','Sonora','Tabasco','Tamaulipas','Tlaxcala','Veracruz','Yucatán','Zacatecas'
+        'Aguascalientes',
+        'Baja California','Baja California Sur',
+        'Campeche','Chiapas','Chihuahua','Coahuila','Colima',
+        'Distrito Federal','Durango',
+        'Estado de México',
+        'Guanajuato','Guerrero',
+        'Hidalgo',
+        'Jalisco',
+        'Michoacán','Morelos',
+        'Nayarit','Nuevo León',
+        'Oaxaca',
+        'Puebla',
+        'Querétaro','Quintana Roo',
+        'San Luis Potosí',
+        'Sinaloa','Sonora',
+        'Tabasco','Tamaulipas','Tlaxcala',
+        'Veracruz',
+        'Yucatán',
+        'Zacatecas',
     );
 
     protected static $cityFormats = array(
