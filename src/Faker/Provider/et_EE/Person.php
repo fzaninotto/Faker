3768448092d8131816a75393e436b4de2595b066
David <david@Verslauk-MacPro-No-2.local>
2018-12-03 15:40:45 +0200

    Add more estonian names/surnames

--- src/Faker/Provider/et_EE/Person.php
+++ src/Faker/Provider/et_EE/Person.php
@@ -11,42 +11,74 @@ class Person extends \Faker\Provider\Person
 {
     /**
      * @link https://news.err.ee/114745/most-popular-baby-names-of-2014
+     * @link https://www.stat.ee/public/apps/nimed/TOP
      */
-    protected static $firstNameMale = array(
-        'Rasmus', 'Artjom', 'Robin', 'Martin', 'Oliver', 'Romet', 'Sebastian', 'Robert',
-        'Artur', 'Maksim', 'Markus', 'Marten', 'Karl', 'Kristofer', 'Oskar', 'Daniel',
-        'Hugo', 'Henri', 'Mark', 'Nikita', 'Kirill', 'Sander', 'Kevin', 'Aleksandr', 'Daniil'
-    );
+    protected static $firstNameMale = [
+        "Aleksandr", "Artjom", "Artur",
+        "Daniel", "Daniil",
+        "Fjodorov",
+        "Henri", "Hugo",
+        "Ilves", "Ivanov",
+        "Jõgi",
+        "Kaasik", "Kallas", "Kangur", "Karl", "Karu", "Kask", "Kevin", "Kirill", "Kivi", "Kõiv", "Koppel", "Kristofer", "Kukk", "Kütt", "Kuusik", "Kuusk", "Kuznetsov",
+        "Laur", "Lepik", "Lepp", "Leppik", "Liiv", "Lõhmus", "Luik",
+        "Mägi", "Maksim", "Mark", "Markus", "Marten", "Martin", "Mets", "Mihhailov", "Mölder",
+        "Nikita",
+        "Oja", "Oliver", "Oskar", "Ots",
+        "Pärn", "Pavlov", "Peterson", "Petrov", "Põder", "Popov",
+        "Rasmus", "Raudsepp", "Rebane", "Robert", "Robin", "Romet",
+        "Saar", "Sander", "Sebastian", "Semjonov", "Sepp", "Smirnov", "Sokolov", "Stepanov",
+        "Tamm", "Teder", "Toom",
+        "Vaher", "Vassiljev", "Volkov",
+    ];
 
     /**
      * @link https://news.err.ee/114745/most-popular-baby-names-of-2014
+     * @link https://www.stat.ee/public/apps/nimed/TOP
      */
-    protected static $firstNameFemale = array(
-        'Sofia', 'Eliise', 'Maria', 'Mia', 'Lisandra', 'Mirtel', 'Sandra', 'Emma', 'Laura',
-        'Darja', 'Arina', 'Milana', 'Alisa', 'Anastasia', 'Lenna', 'Liisa', 'Anna', 'Viktoria',
-        'Elisabeth', 'Polina', 'Marta', 'Aleksandra', 'Marleen', 'Hanna', 'Nora'
-    );
+    protected static $firstNameFemale = [
+        "Aino", "Aleksandra", "Alisa", "Anastasia", "Anna", "Anne", "Anneli", "Anu", "Arina",
+        "Darja", "Diana",
+        "Elena", "Eliise", "Elisabeth", "Emma", "Ene", "Eve",
+        "Galina",
+        "Hanna",
+        "Irina",
+        "Jekaterina", "Jelena", "Julia",
+        "Kadri", "Katrin", "Kristi", "Kristiina", "Kristina",
+        "Laura", "Lenna", "Liisa", "Linda", "Lisandra", "Ljubov", "Ljudmila",
+        "Maie", "Malle", "Mare", "Maria", "Marina", "Marleen", "Marta", "Merike", "Mia", "Milana", "Mirtel",
+        "Natalia", "Niina", "Nora",
+        "Olga",
+        "Piret", "Polina",
+        "Reet", "Riina",
+        "Sandra", "Sirje", "Sofia", "Svetlana",
+        "Tamara", "Tatiana", "Tiina", "Tiiu", "Triin",
+        "Ülle",
+        "Valentina", "Viktoria",
+    ];
 
     /**
      * @link https://en.wikipedia.org/wiki/Category:Estonian-language_surnames
+     * @link https://www.stat.ee/public/apps/nimed/pere/TOP
      */
-    protected static $lastName = array(
-        'Aare', 'Aarma', 'Aas', 'Aasmäe', 'Aav', 'Aavik', 'Allik', 'Alver', 'Eenpalu', 'Eskola',
-        'Härma', 'Ilves', 'Jakobson', 'Jänes', 'Järve', 'Järvis', 'Jõgi', 'Kaaleste', 'Kaasik',
-        'Käbin', 'Kalda', 'Kaljulaid', 'Kaljurand', 'Kallas', 'Kallaste', 'Kangro', 'Kangur',
-        'Kapp', 'Kärner', 'Kask', 'Käsper', 'Kass', 'Keres', 'Keskküla', 'Kesküla', 'Kikkas',
-        'Kingsepp', 'Kirs', 'Kirsipuu', 'Kivi', 'Klavan', 'Kõiv', 'Kokk', 'Kontaveit', 'Koppel',
-        'Korjus', 'Kotkas', 'Kreek', 'Kross', 'Kruus', 'Kukk', 'Kütt', 'Kuusik', 'Kuusk', 'Laar',
-        'Laht', 'Lass', 'Laur', 'Laurits', 'Lemsalu', 'Lepik', 'Lepmets', 'Lepp', 'Levandi',
-        'Liiv', 'Lill', 'Lindmaa', 'Linna', 'Lipp', 'Lippmaa', 'Lõhmus', 'Loo', 'Lõoke', 'Luik',
-        'Luts', 'Mägi', 'Mälk', 'Mänd', 'Männik', 'Margiste', 'Mark', 'Masing', 'Mets', 'Must',
-        'Nõmmik', 'Nurme', 'Nurmsalu', 'Oja', 'Olesk', 'Öpik', 'Orav', 'Pääsuke', 'Padar', 'Pärn',
-        'Peebo', 'Peetre', 'Pihlak', 'Piho', 'Piip', 'Põder', 'Põld', 'Poska', 'Puhvel', 'Pütsep',
-        'Puusepp', 'Raag', 'Rand', 'Raud', 'Raudsepp', 'Rebane', 'Reek', 'Reinsalu', 'Rooba',
-        'Roolaid', 'Rootare', 'Rummo', 'Rüütel', 'Rüütli', 'Saar', 'Salumäe', 'Sepp', 'Sibul',
-        'Siimar', 'Simm', 'Sirel', 'Sisask', 'Sokk', 'Soosaar', 'Susi', 'Talts', 'Tamme',
-        'Tarvas', 'Teder', 'Toom', 'Toome', 'Toots', 'Vaher', 'Vahtra', 'Vaino', 'Vainola',
-        'Välbe', 'Valdma', 'Väljas', 'Valk', 'Vesik', 'Veski', 'Viiding', 'Vitsut', 'Võigemast',
-        'Võsu',
-    );
+    protected static $lastName = [
+        "Aare", "Aarma", "Aas", "Aasmäe", "Aav", "Aavik", "Allik", "Alver",
+        "Eenpalu", "Eskola",
+        "Fjodorov", "Fjodorova",
+        "Härma",
+        "Ilves", "Ivanov", "Ivanova",
+        "Jakobson", "Jänes", "Järve", "Järvis",
+        "Jõgi",
+        "Kaaleste",
+        "Kaasik", "Käbin", "Kalda", "Kaljulaid", "Kaljurand", "Kallas", "Kallaste", "Kangro", "Kangur", "Kapp", "Kärner", "Karu", "Kask", "Käsper", "Kass", "Keres", "Keskküla", "Kesküla", "Kikkas", "Kingsepp", "Kirs", "Kirsipuu", "Kivi", "Klavan", "Kõiv", "Kokk", "Kontaveit", "Koppel", "Korjus", "Kotkas", "Kreek", "Kross", "Kruus", "Kukk", "Kull", "Kütt", "Kuusik", "Kuusk", "Kuznetsov", "Kuznetsova",
+        "Laar", "Laht", "Lass", "Laur", "Laurits", "Lemsalu", "Lepik", "Lepmets", "Lepp", "Leppik", "Levandi", "Liiv", "Lill", "Lindmaa", "Linna", "Lipp", "Lippmaa", "Lõhmus", "Loo", "Lõoke", "Luik", "Luts",
+        "Mägi", "Mälk", "Mänd", "Männik", "Margiste", "Mark", "Masing", "Mets", "Mihhailov", "Mihhailova",
+        "Mölder", "Must",
+        "Nõmmik", "Nurme", "Nurmsalu",
+        "Oja", "Olesk", "Öpik", "Orav", "Ots",
+        "Pääsuke", "Padar", "Pärn", "Pavlov", "Pavlova", "Peebo", "Peetre", "Peterson", "Petrov", "Petrova", "Pihlak", "Piho", "Piip", "Põder", "Põld", "Popov", "Popova", "Poska", "Puhvel", "Pütsep", "Puusepp", 
+        "Raag", "Raud", "Raudsepp", "Rebane", "Reek", "Reinsalu", "Rooba", "Roolaid", "Rootare", "Rummo", "Rüütel", "Rüütli",
+        "Saar", "Salumäe", "Semjonov", "Sepp", "Sibul", "Siimar", "Simm", "Sirel", "Sisask", "Smirnov", "Smirnova", "Sokk", "Sokolov", "Soosaar", "Stepanov", "Stepanova", "Susi",
+        "Talts", "Tamm", "Tamme", "Tarvas", "Teder", "Toom", "Toome", "Toots", "Vaher", "Vahtra", "Vaino", "Vainola", "Välbe", "Valdma", "Väljas", "Valk", "Vassiljev", "Vassiljeva", "Vesik", "Veski", "Viiding", "Vitsut", "Võigemast", "Volkov", "Volkova", "Võsu",
+    ];
 }
