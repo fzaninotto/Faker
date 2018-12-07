9dec10d21b6fcff8a3d0ceef5ce2639011d4c970
Faton Sopa <faton.sopa@manaferra.com>
2018-12-07 08:50:33 +0100

    minor improvements
    - fail silently when copy fails
    - the silently return is returned on curl_exec

--- src/Faker/Provider/Image.php
+++ src/Faker/Provider/Image.php
@@ -61,45 +61,54 @@ class Image extends Base
      *
      * @example '/path/to/dir/13b73edae8443990be1aa8f1a483bc27.jpg'
      */
-    public static function image($dir = null, $width = 640, $height = 480, $category = null, $fullPath = true, $randomize = true, $word = null)
+        public static function image($dir = null, $width = 640, $height = 480, $category = null, $fullPath = true, $randomize = true, $word = null)
     {
         $dir = is_null($dir) ? sys_get_temp_dir() : $dir; // GNU/Linux / OS X / Windows compatible
+            
         // Validate directory path
         if (!is_dir($dir) || !is_writable($dir)) {
             throw new \InvalidArgumentException(sprintf('Cannot write to directory "%s"', $dir));
         }
+            
         // Generate a random filename. Use the server address so that a file
         // generated at the same time on a different server won't have a collision.
         $name = md5(uniqid(empty($_SERVER['SERVER_ADDR']) ? '' : $_SERVER['SERVER_ADDR'], true));
         $filename = $name .'.jpg';
         $filepath = $dir . DIRECTORY_SEPARATOR . $filename;
         $url = static::imageUrl($width, $height, $category, $randomize, $word);
-        
+
         // save file
         $success = false;
-        
+
         if (function_exists('curl_exec')) {
             // use cURL
             $fp = fopen($filepath, 'w');
             $ch = curl_init($url);
             curl_setopt($ch, CURLOPT_FILE, $fp);
             $success = curl_exec($ch) && curl_getinfo($ch, CURLINFO_HTTP_CODE) === 200;
-            
-            if (!$success || file_exists($filepath)) {
-                @unlink($filepath); // although the file may exists, it may be "Temporary unvailable" because cURL failed for some reason.
-            }
-            
             fclose($fp);
             curl_close($ch);
-        }
-        
-        // When cURL fails for some reason, try to get the image via allow_url_fopen method.
-        if (!$success && ini_get('allow_url_fopen')) {
+
+            if (!$success) {
+                if (file_exists($filepath)) {
+                    unlink($filepath); // although the file may exists, it may be "Temporary unvailable" because cURL failed for some reason.
+                }
+
+                // could not contact the distant URL or HTTP error - fail silently.
+                return false;
+            }
+
+        } elseif (ini_get('allow_url_fopen')) {
             // use remote fopen() via copy()
-            $success = copy($url, $filepath);
+            if (!@copy($url, $filepath)) {
+                // could not contact the distant URL or HTTP error - fail silently.
+                return false;
+            }
         } else {
             return new \RuntimeException('The image formatter downloads an image from a remote HTTP server. Therefore, it requires that PHP can request remote hosts, either via cURL or fopen()');
         }
+
+
         return $fullPath ? $filepath : $filename;
     }
 }
