902683e75395aeaf3721993852f2a037fc1f03ea
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2019-01-03 17:59:38 +0300

    Support populating Doctrine's immutable date types

--- src/Faker/ORM/Doctrine/ColumnTypeGuesser.php
+++ src/Faker/ORM/Doctrine/ColumnTypeGuesser.php
@@ -67,6 +67,12 @@ class ColumnTypeGuesser
                 return function () use ($generator) {
                     return $generator->datetime;
                 };
+            case 'datetime_immutable':
+            case 'date_immutable':
+            case 'time_immutable':
+                return function () use ($generator) {
+                    return \DateTimeImmutable::createFromMutable($generator->datetime);
+                };
             default:
                 // no smart way to guess what the user expects here
                 return null;
