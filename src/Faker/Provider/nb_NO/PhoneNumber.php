74beb786fa04d5eb1a175f26e6152a532f3ef0d0
Rolf Haug <rolf@rah.no>
2019-03-30 15:21:26 +0100

    feature: mobile number for nb_NO
    Splitted land line and cell numbers and added feature to fake mobile numbers.

--- src/Faker/Provider/nb_NO/PhoneNumber.php
+++ src/Faker/Provider/nb_NO/PhoneNumber.php
@@ -5,18 +5,45 @@ namespace Faker\Provider\nb_NO;
 class PhoneNumber extends \Faker\Provider\PhoneNumber
 {
     /**
-    * @var array Norwegian phone number formats
+    * @var array Norwegian land line phone number formats
     */
-    protected static $formats = array(
-        '+47#########',
-        '+47 ## ## ## ##',
-        '## ## ## ##',
-        '## ## ## ##',
-        '########',
-        '########',
+    protected static $formats =
+        array(
+        '+472#######',
+        '+473#######',
+        '+475#######',
+        '+476#######',
+        '+477#######',
+        '+47 2# ## ## ##',
+        '+47 3# ## ## ##',
+        '+47 5# ## ## ##',
+        '+47 6# ## ## ##',
+        '+47 7# ## ## ##',
+    );
+
+    /**
+     * An array of nb_NO mobile phone number formats
+     * @var array
+     */
+    protected static $mobileFormats = array(
+        // Local
         '9## ## ###',
         '4## ## ###',
         '9#######',
         '4#######',
+        // International
+        '+479#######',
+        '+47 9## ## ###',
+        '+474#######',
+        '+47 4## ## ###'
     );
+
+    /**
+     * Return a nb_NO mobile phone number
+     * @return string
+     */
+    public static function mobileNumber()
+    {
+        return static::numerify(static::randomElement(static::$mobileFormats));
+    }
 }
