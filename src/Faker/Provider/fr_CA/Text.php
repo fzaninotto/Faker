41b2e56dc53dbb7416effb288eef0f47948f2420
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2019-04-15 13:12:40 -0400

    Corrected spaces

--- src/Faker/Provider/fr_CA/Text.php
+++ src/Faker/Provider/fr_CA/Text.php
@@ -104,7 +104,7 @@ class Text extends \Faker\Provider\Text
      * @see http://www.gutenberg.org/cache/epub/16210/pg16210.txt
      * @var string
      */
-protected static $baseText = <<<'EOT'
+    protected static $baseText = <<<'EOT'
 LA CHASSE-GALERIE
 
 I
