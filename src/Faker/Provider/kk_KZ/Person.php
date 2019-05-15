16380b8234207ec8263ae2cad0903dead68625f5
Yerlen Zhubangaliyev <yerlen@yerlen.com>
2019-05-15 10:43:56 +0600

    fix century detection

--- src/Faker/Provider/kk_KZ/Person.php
+++ src/Faker/Provider/kk_KZ/Person.php
@@ -7,7 +7,7 @@ use \Faker\Provider\DateTime;
 class Person extends \Faker\Provider\Person
 {
 
-    const GENDER_MALE = 0;
+    const GENDER_MALE   = 0;
     const GENDER_FEMALE = 1;
 
     const CENTURY_19TH = 0;
@@ -177,13 +177,13 @@ class Person extends \Faker\Provider\Person
     );
 
     /**
-     * @param  integer $year
+     * @param integer $year
      *
      * @return integer|null
      */
     private static function getCenturyByYear($year)
     {
-        if ($year >= 2000 && $year <= DateTime::year()) {
+        if ($year >= 2000) {
             return self::CENTURY_21ST;
         } elseif ($year >= 1900) {
             return self::CENTURY_20TH;
@@ -198,8 +198,8 @@ class Person extends \Faker\Provider\Person
      * @link   http://egov.kz/wps/portal/Content?contentPath=%2Fegovcontent%2Fcitizen_migration%2Fpassport_id_card%2Farticle%2Fiin_info&lang=en
      * @link   https://ru.wikipedia.org/wiki/%D0%98%D0%BD%D0%B4%D0%B8%D0%B2%D0%B8%D0%B4%D1%83%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9_%D0%B8%D0%B4%D0%B5%D0%BD%D1%82%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D1%8B%D0%B9_%D0%BD%D0%BE%D0%BC%D0%B5%D1%80
      *
-     * @param  \DateTime $birthDate
-     * @param  integer   $gender
+     * @param \DateTime $birthDate
+     * @param integer   $gender
      *
      * @return string 12 digits, like 780322300455
      */
@@ -211,15 +211,14 @@ class Person extends \Faker\Provider\Person
 
         do {
             $population = mt_rand(1000, 2000);
-            $century = self::getCenturyByYear((int) $birthDate->format('Y'));
-
-            $iin = $birthDate->format('ymd');
-            $iin .= (string) self::$genderCenturyMap[$gender][$century];
-            $iin .= (string) $population;
-            $checksum = self::checkSum($iin);
+            $century    = self::getCenturyByYear((int)$birthDate->format('Y'));
+            $iin        = $birthDate->format('ymd');
+            $iin        .= (string)self::$genderCenturyMap[$gender][$century];
+            $iin        .= (string)$population;
+            $checksum   = self::checkSum($iin);
         } while ($checksum === 10);
 
-        return $iin . (string) $checksum;
+        return $iin . (string)$checksum;
     }
 
     /**
@@ -240,7 +239,7 @@ class Person extends \Faker\Provider\Person
 
     /**
      * @param string $iinValue
-     * @param array $sequence
+     * @param array  $sequence
      *
      * @return integer
      */
@@ -249,7 +248,7 @@ class Person extends \Faker\Provider\Person
         $sum = 0;
 
         for ($i = 0; $i <= 10; $i++) {
-            $sum += (int) $iinValue[$i] * $sequence[$i];
+            $sum += (int)$iinValue[$i] * $sequence[$i];
         }
 
         return $sum % 11;
