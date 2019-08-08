58f2b78a20695576b711e2177909e7231198a11f
Abdellah Chadidi <chadidi97@gmail.com>
2019-08-08 18:26:05 +0100

    Create ar_MA Provider with files copied from ar_JO

---
+++ src/Faker/Provider/ar_MA/Company.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Faker\Provider\ar_JO;
+
+class Company extends \Faker\Provider\Company
+{
+    protected static $formats = array(
+        '{{lastName}} {{companySuffix}}',
+        '{{companyPrefix}} {{lastName}} {{companySuffix}}',
+        '{{companyPrefix}} {{lastName}}',
+    );
+
+    protected static $bsWords = array(
+        array()
+    );
+
+    protected static $catchPhraseWords = array(
+        array('الخدمات','الحلول','الانظمة'),
+        array(
+            'الذهبية','الذكية','المتطورة','المتقدمة', 'الدولية', 'المتخصصه', 'السريعة',
+            'المثلى', 'الابداعية', 'المتكاملة', 'المتغيرة', 'المثالية'
+            ),
+    );
+
+    protected static $companyPrefix = array('شركة','مؤسسة','مجموعة','مكتب','أكاديمية','معرض');
+
+    protected static $companySuffix = array('وأولاده', 'للمساهمة المحدودة', ' ذ.م.م', 'مساهمة عامة', 'وشركائه');
+
+    /**
+     * @example 'مؤسسة'
+     * @return string
+     */
+    public function companyPrefix()
+    {
+        return static::randomElement(self::$companyPrefix);
+    }
+
+    /**
+     * @example 'Robust full-range hub'
+     */
+    public function catchPhrase()
+    {
+        $result = array();
+        foreach (static::$catchPhraseWords as &$word) {
+            $result[] = static::randomElement($word);
+        }
+
+        return join($result, ' ');
+    }
+
+    /**
+     * @example 'integrate extensible convergence'
+     */
+    public function bs()
+    {
+        $result = array();
+        foreach (static::$bsWords as &$word) {
+            $result[] = static::randomElement($word);
+        }
+
+        return join($result, ' ');
+    }
+}
