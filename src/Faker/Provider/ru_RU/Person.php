c01c36f5d22736623c38fb868781c08f8af566d3
Omniglies <abaz.nurfarhana@gmail.com>
2019-08-15 22:10:29 +0700

    Edit phpdoc on method lastName

--- src/Faker/Provider/ru_RU/Person.php
+++ src/Faker/Provider/ru_RU/Person.php
@@ -161,7 +161,7 @@ class Person extends \Faker\Provider\Person
      * @access public
      * @param string|null $gender A gender of the last name should be generated
      *     for. If the argument is skipped a random gender will be used.
-     * @return string Middle name
+     * @return string Last name
      */
     public function lastName($gender = null)
     {
