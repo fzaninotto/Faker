9cf6b44db0d31710975b511afc1b038799545795
cacing69 <cacingworm69@gmail.com>
2019-08-22 15:47:07 +0700

    add id_ID Color

---
+++ src/Faker/Provider/id_ID/Color.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cacing
+ * Date: 22/08/2019
+ * Time: 14:39
+ */
+
+namespace Faker\Provider\id_ID;
+
+
+class Color extends \Faker\Provider\Color
+{
+    protected static $safeColorNames = array(
+        'abu-abu', 'biru', 'biru dongker', 'biru laut', 'coklat', 'emas', 'hijau', 'hitam', 'kuning',
+        'magenta', 'mawar', 'merah', 'merah marun', 'merah jambu', 'nila', 'jingga', 'perak', 'putih', 'ungu', 'emas', 'cokelat',
+    );
+
+    protected static $allColorNames = array(
+        'Merah Indian', 'Koral Terang', 'Salmon', 'Salmon Gelap', 'Salmon Terang', 'Krimson', 'Merah',
+        'Merah Bata', 'Merah Tua', 'Merah Muda', 'Merah Muda Panas', 'Koral',
+        'Merah Oranye', 'Jingga Tua', 'Jingga', 'Emas', 'Kuning', 'Kuning Terang', 'Khaki', 'Dark Khaki',
+        'Kuning Lemon', 'Plum', 'Magenta', 'Ungu',  'Hijau Kekuningan', 'Hijau Lemon',
+        'Hijah Gelap', 'Biru', 'Hitam', 'Abu-Abu Tua', 'Abu-Abu Muda', 'Putih', 'Cokelat', 'Perak',
+        'Kuning Sawo', 'Ungu Lembayung', 'Kuning Aprikot', 'Biru Laut', 'Cokelat Kemerahan',
+        'Biru Langit', 'Abu-Abu Kecoklatan', 'Hijau Pudar', 'Jingga Muda', 'Arang', 'Hijau Muda Kekuningan',
+        'Cokelat Tua', 'Tembaga', 'Merah Kekunung-Kuningan', 'Krem', 'Merah Tua Terang', 'Biru Terang',
+        'Gelap', 'Ungu Terong', 'Hijau Zamrud', 'Hijau Hutan', 'Merah Muda Terang', 'Coklat Keemasan',
+        'Nila', 'Putih Gading', 'Hijau Lumut', 'Hitam Pekat', 'Kuning Pucat',
+        'Ungu Muda', 'Kuning Muda', 'Putih Terang', 'Hijau Cerah', 'Merah Muda Keunguan Pudar', 'Hijau Tua',
+        'Merah Keungu Unguan', 'Ungu Lembayung Muda', 'Kuning Kehijauan Pucat', 'Hijau Neon',
+        'Kuning Kecoklatan Tua', 'Paster', 'Peach', 'Biru Pucat', 'Sawo Matang', 'Jingga Labu',
+        'Pelangi', 'Mawar', 'Delima', 'Cokelat Kekuningan', 'Kuning Jingga', 'Biru Nilam',
+        'Merah Muda Kekuningan', 'Merah Tomat', 'Biru Keabu-abuan', 'Biru Baja',
+        'Biru Dongker', 'Hijau Kebiruan', 'Biru Kehijauan', 'Biru Laut Tua', 'Ungu Kebiruan', 'Coklat Gandum',
+        'Hijau Abu-Abu', 'Ungu Kecoklatan'
+    );
+}
