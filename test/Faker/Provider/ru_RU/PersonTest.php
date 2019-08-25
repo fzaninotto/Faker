c6d03aa993f07f318063b93d868d322eff41ce5b
Omniglies <abaz.nurfarhana@gmail.com>
2019-08-25 22:02:00 +0700

    Add ru_RU Person Test

---
+++ test/Faker/Provider/ru_RU/PersonTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Faker\Test\Provider\ru_RU;
+
+use Faker\Generator;
+use Faker\Provider\ru_RU\Person;
+use PHPUnit\Framework\TestCase;
+
+class PersonTest extends TestCase
+{
+    /**
+     * @var Generator
+     */
+    private $faker;
+
+    public function setUp()
+    {
+        $faker = new Generator();
+        $faker->addProvider(new Person($faker));
+        $this->faker = $faker;
+    }
+
+    public function testLastNameFemale()
+    {
+        $this->assertEquals("a", substr($this->faker->lastName('female'), -1));
+    }
+
+    public function testLastNameMale()
+    {
+        $this->assertNotEquals("a", substr($this->faker->lastName(), -1));
+    }
+}
