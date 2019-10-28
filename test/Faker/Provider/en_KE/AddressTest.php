fbfc8d5d12a9efbf0f5981fa71a8d265fe8cc26c
Uzziel <uzzielkk@gmail.com>
2019-10-29 00:17:51 +0300

    Minor fixes

--- test/Faker/Provider/en_KE/AddressTest.php
+++ test/Faker/Provider/en_KE/AddressTest.php
@@ -28,62 +28,6 @@ class AddressTest extends TestCase
     $this->assertInternalType('string', $city);
     $this->assertRegExp('/[A-Z][a-z]+/', $city);
   }
-
-  public function testCountry()
-  {
-    $country = $this->faker->country();
-    $this->assertNotEmpty($country);
-    $this->assertInternalType('string', $country);
-    $this->assertRegExp('/[A-Z][a-z]+/', $country);
-  }
-
-  public function testMunicipality()
-  {
-    $municipality = $this->faker->municipality();
-    $this->assertNotEmpty($municipality);
-    $this->assertInternalType('string', $municipality);
-    $this->assertRegExp('/[A-Z][a-z]+/',$municipality);
-  }
-
-  public function testCounty()
-  {
-    $county = $this->faker->county();
-    $this->assertNotEmpty($county);
-    $this->assertInternalType('string', $county);
-    $this->assertRegExp('/[A-Z][a-z]+/',$county);
-  }
-
-  public function testTown()
-  {
-    $town = $this->faker->town();
-    $this->assertNotEmpty($town);
-    $this->assertInternalType('string', $town);
-    $this->assertRegExp('/[A-Z][a-z]+/',$town);
-  }
-
-  public function testCenter()
-  {
-    $center = $this->faker->center();
-    $this->assertNotEmpty($center);
-    $this->assertInternalType('string', $center);
-    $this->assertRegExp('/[A-Z][a-z]+/',$center);
-  }
-
-  public function testStreetName()
-  {
-    $street = $this->faker->street();
-    $this->assertNotEmpty($street);
-    $this->assertInternalType('string', $street);
-    $this->assertRegExp('/[A-Z][a-z]+/',$street);
-  }
-
-  public function testPostCode()
-  {
-    $code = $this->faker->postcode();
-    $this->assertNotEmpty($code);
-    $this->assertInternalType('string', $code);
-    $this->assertRegExp('/[0-9]+/',$code);
-  }
 }
 
 ?>
--- test/Faker/Provider/en_KE/PhoneNumberTest.php
+++ test/Faker/Provider/en_KE/PhoneNumberTest.php
@@ -28,37 +28,4 @@ class PhoneNumberTest extends TestCase
     $this->assertInternalType('string', $phone);
     $this->assertRegexp('/^(?:254|\+254|0)?(7(?:(?:[0-9][0-9])|(?:5[0-6])|([0-9][5-9]))[0-9]{6})$/',$phone);
   }
-
-  public function testSafaricom()
-  {
-    $phone = $this->faker->safaricom();
-    $this->assertNotEmpty($phone);
-    $this->assertInternalType('string', $phone);
-    $this->assertRegexp('/^(?:254|\+254|0)?(7(?:(?:[12][0-9])|(?:0[0-9])|(9[0-9]))[0-9]{6})$/',$phone);
-  }
-
-  public function testTelkom()
-  {
-    $phone = $this->faker->telkom();
-    $this->assertNotEmpty($phone);
-    $this->assertInternalType('string', $phone);
-    $this->assertRegexp('/^(?:254|\+254|0)?(77[0-9][0-9]{6})$/',$phone);
-  }
-
-  public function testAirtel()
-  {
-    $phone = $this->faker->airtel();
-    $this->assertNotEmpty($phone);
-    $this->assertInternalType('string', $phone);
-    $this->assertRegexp('/^(?:254|\+254|0)?(7(?:(?:[3][0-9])|(?:5[0-6])|(8[5-9]))[0-9]{6})$/',$phone);
-  }
-
-  public function testFaiba()
-  {
-    $phone = $this->faker->faiba();
-    $this->assertNotEmpty($phone);
-    $this->assertInternalType('string', $phone);
-    $this->assertRegexp('/^(?:254|\+254|0)?(74[7][0-9]{6})$/',$phone);
-  }
-
 }
