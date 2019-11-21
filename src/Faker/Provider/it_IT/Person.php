32e479949834a1eb47811d1d0f974f38ab10052a
marcomidu <39595473+marcomidu@users.noreply.github.com>
2019-11-21 16:39:14 +0100

    Update Person.php

--- src/Faker/Provider/it_IT/Person.php
+++ src/Faker/Provider/it_IT/Person.php
@@ -99,7 +99,7 @@ class Person extends \Faker\Provider\Person
     public static function taxId($lastName = null, $firstName = null, $gender = 'm', \DateTime $birthdate = null, $belfiore = null)
     {
         //Birth Date
-        $months = array(1=>'A',2=>'B',3=>'C',4=>'D',5=>'E',6=>'H',7=>'L',8=>'M',9=>'P',10=>'R',11=>'S',12=>'T']);
+        $months = array(1=>'A',2=>'B',3=>'C',4=>'D',5=>'E',6=>'H',7=>'L',8=>'M',9=>'P',10=>'R',11=>'S',12=>'T');
         if(is_null($birthdate)){
             $birthdate = \Faker\Provider\DateTime::dateTimeThisCentury();
         }
