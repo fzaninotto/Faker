1480a4f944b4a908ab3fa1bf0aa595a2acc77987
Christoph Mayer <christoph.mayer@seatbelt.xyz>
2019-12-18 09:23:05 +0100

    Generate valid german phone numbers
    fix for #1876

--- src/Faker/Provider/de_DE/PhoneNumber.php
+++ src/Faker/Provider/de_DE/PhoneNumber.php
@@ -5,16 +5,21 @@ namespace Faker\Provider\de_DE;
 class PhoneNumber extends \Faker\Provider\PhoneNumber
 {
     protected static $formats = array(
-        '+49(0)##########',
-        '+49(0)#### ######',
-        '+49 (0) #### ######',
-        '+49(0) #########',
-        '+49(0)#### #####',
-        '0##########',
-        '0#########',
-        '0#### ######',
-        '0#### #####',
-        '(0####) ######',
-        '(0####) #####',
+        '+49(0){{areaStartCode}}#########',
+        '+49(0){{areaStartCode}}### ######',
+        '+49 (0) {{areaStartCode}}### ######',
+        '+49(0) {{areaStartCode}}########',
+        '+49(0){{areaStartCode}}### #####',
+        '0{{areaStartCode}}#########',
+        '0{{areaStartCode}}########',
+        '0{{areaStartCode}}### ######',
+        '0{{areaStartCode}}### #####',
+        '(0{{areaStartCode}}###) ######',
+        '(0{{areaStartCode}}###) #####',
     );
+
+    public static function areaStartCode()
+    {
+        return self::numberBetween(1, 9);
+    }
 }
