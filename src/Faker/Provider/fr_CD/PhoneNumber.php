75ff97ae165a3721a70d3d7f9936d12e317dd606
Dark Angel <jonathanyombo@gmail.com>
2020-03-31 11:03:09 +0200

    [NEW] add congolese phone number
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

---
+++ src/Faker/Provider/fr_CD/PhoneNumber.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Faker\Provider\fr_CD;
+
+class PhoneNumber extends \Faker\Provider\PhoneNumber
+{
+    protected static $mobileFormats = array(
+        '+243 (0)## ### ## ##',
+        '+243(0)#########',
+        '+243 ## ### ## ##',
+        '0#########',
+        '+243#########',
+        '0## ### ## ##',
+    );
+
+    protected static $prefixs = array('99', '97', '98', '81', '82', '85', '84', '89', '90', '91');
+
+    /**
+     * return a Congolese phone number
+     *
+     * @return string
+     */
+    public static function mobileNumber()
+    {
+        $prefix = static::randomElement(static::$prefixs);
+        $code = static::randomElement(array('+243, 0', '00243'));
+        $format = static::numerify(static::randomElement(array('#######', ' ## ### ##', ' ### ## ##', '## ## ###')));
+        return $prefix.$code.$format;
+    }
+}
