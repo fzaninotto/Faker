4b96097d1166fc65c27d76a12759a56500d4cef6
Christhofer Natalius <christhofernatalius@gmail.com>
2020-08-19 22:42:16 +0700

    added job title for id_ID provider

--- src/Faker/Provider/id_ID/Company.php
+++ src/Faker/Provider/id_ID/Company.php
@@ -22,6 +22,34 @@ class Company extends \Faker\Provider\Company
     protected static $companySuffix = array('(Persero) Tbk', 'Tbk');
 
     /**
+     * @link https://id.wikipedia.org/wiki/Daftar_pekerjaan
+     */
+    protected static $jobTitleFormat = array(
+        'Administrator', 'Agen Asuransi', 'Agen Perjalanan', 'Agen Rahasia', 'Ahli Cuaca', 'Ahli Gizi', 'Aktor / Aktris', 'Akuntan', 'Antariksawan', 'Apoteker', 'Arsitek', 'Arkeolog', 'Asisten Rumah Tangga', 'Atlet',
+        'Bartender', 'Biarawan / Biarawati', 'Bidan', 'Biksu', 'Blogger', 'Bupati', 'Buruh',
+        'Camat', 'Calo',
+        'Desainer', 'Detektif', 'Direktur', 'Dokter', 'Dosen', 'Dukun',
+        'Editor',
+        'Fotografer',
+        'Gubernur', 'Guru',
+        'Hakim',
+        'Ilmuwan', 'Ilustrator', 'Inspektur', 'Insinyur', 'Ibu Rumah Tangga', 'Instruktur Olahraga',
+        'Jaksa', 'Jurnalis',
+        'Karyawan', 'Kasir', 'Koki', 'Komikus', 'Komisaris', 'Kondektur', 'Konsultan', 'Kurir',
+        'Lurah',
+        'Makelar', 'Manajer', 'Marketing', 'Marinir', 'Masinis', 'Model', 'Montir', 'Musisi',
+        'Nahkoda', 'Nelayan', 'Novelis', 'Notaris',
+        'Operator',
+        'Pedagang', 'Pegawai Negri', 'Pelaut', 'Pelayan', 'Pelawak', 'Pelukis', 'Pemadam Kebakaran', 'Pemahat', 'Pemain Bulu Tangkis', 'Pemain Basket', 'Pemain Musik', 'Pemain Sepak Bola', 'Pemeran Pengganti', 'Penagih Hutang', 'Penari', 'Pendeta', 'Peneliti', 'Penerjemah', 'Pengacara', 'Pengasuh Anak', 'Pengawal', 'Pengisi Acara', 'Pengisi Suara', 'Pengrajin', 'Pengusaha', 'Penulis', 'Penyanyi', 'Perancang Busana', 'Perancang Grafis', 'Perawat', 'Perdana Menteri', 'Peretas', 'Perias Wajah', 'Petani', 'Peternak', 'Pesulap', 'Petugas Kebersihan', 'Petugas Forensik', 'Petinju', 'Polisi', 'Politikus', 'Pramugara / Pramugari', 'Psikiater', 'Psikolog', 'Pilot', 'Pramusaji', 'Presiden', 'Pustakawan',
+        'Raja', 'Ratu', 'Resepsionis',
+        'Satpam', 'Sekretaris', 'Seniman', 'Sipir', 'Supir', 'Supir Angkot', 'Supir Bus', 'Supir Taksi', 'Supir Truk', 'Sutradara',
+        'Tengkulak', 'Tentara', 'Tukang AC', 'Tukang Bangunan', 'Tukang Bakso', 'Tukang Batu', 'Tukang Cuci Mobil', 'Tukang Cukur', 'Tukang Kayu', 'Tukang Kebun', 'Tukang Las', 'Tukang Ledeng', 'Tukang Listrik', 'Tukang Parkir', 'Tukang Pijat', 'Tukang Sate', 'Tukang Sayur', 'Tukang Tambal Ban',
+        'Ulama',
+        'Walikota', 'Wartawan', 'Wasit', 'Wirausahawan',
+        'Youtuber'
+    );
+
+    /**
      * Get company prefix
      *
      * @return string company prefix
