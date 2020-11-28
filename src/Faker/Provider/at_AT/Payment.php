af4a5cecf2cb1b1d0fa50aa73177ec648e1ffd6a
Bram Ceulemans <bramceulemans@me.com>
2020-11-28 19:42:28 +0100

    Deprecate at_AT providers (#78)

--- src/Faker/Provider/at_AT/Payment.php
+++ src/Faker/Provider/at_AT/Payment.php
@@ -2,6 +2,11 @@
 
 namespace Faker\Provider\at_AT;
 
+/**
+ * @deprecated at_AT is not an existing locale, use {@link \Faker\Provider\de_AT\Payment}.
+ *
+ * @see \Faker\Provider\de_AT\Payment
+ */
 class Payment extends \Faker\Provider\de_AT\Payment
 {
 }
