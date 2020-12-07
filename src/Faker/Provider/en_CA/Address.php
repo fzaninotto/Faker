9f12db8c85c5d10ec1ca02be7097aabd5e1ee67c
Daniel Schmelz <daniel@schmelz.org>
2020-12-07 15:51:04 +0100

    Increase consistency of long arrays (#118)
    * increase array consistency
    
    * increase array consistency

--- src/Faker/Provider/en_CA/Address.php
+++ src/Faker/Provider/en_CA/Address.php
@@ -13,7 +13,15 @@ class Address extends \Faker\Provider\en_US\Address
     protected static $postcodeLetters = ['A', 'B', 'C', 'E', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'V', 'X', 'Y'];
 
     protected static $province = [
-        'Alberta', 'British Columbia', 'Manitoba', 'New Brunswick', 'Newfoundland and Labrador', 'Northwest Territories', 'Nova Scotia', 'Nunavut', 'Ontario', 'Prince Edward Island', 'Quebec', 'Saskatchewan', 'Yukon Territory',
+        'Alberta',
+        'British Columbia',
+        'Manitoba',
+        'New Brunswick', 'Newfoundland and Labrador', 'Northwest Territories', 'Nova Scotia', 'Nunavut',
+        'Ontario',
+        'Prince Edward Island',
+        'Quebec',
+        'Saskatchewan',
+        'Yukon Territory',
     ];
 
     protected static $provinceAbbr = [
