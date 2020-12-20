a9498d8495f7ca06454ead25395484480a985a22
Daniel Schmelz <daniel@schmelz.org>
2020-12-20 13:43:48 +0100

    Deprecate zh_TW methods that only call base methods (#122)
    * deprecate zh_TW methods that only call base methods
    
    * deprecate zh_TW methods that only call base methods
    
    * remove docblocks

--- src/Faker/Provider/zh_TW/Payment.php
+++ src/Faker/Provider/zh_TW/Payment.php
@@ -2,10 +2,19 @@
 
 namespace Faker\Provider\zh_TW;
 
+/**
+ * @deprecated Use {@link \Faker\Provider\Payment} instead
+ * @see \Faker\Provider\Payment
+ */
 class Payment extends \Faker\Provider\Payment
 {
+    /**
+     * @return array
+     * @deprecated Use {@link \Faker\Provider\Payment::creditCardDetails()} instead
+     * @see \Faker\Provider\Payment::creditCardDetails()
+     */
     public function creditCardDetails($valid = true)
     {
-        return \Faker\Factory::create('en_US')->creditCardDetails($valid);
+        return parent::creditCardDetails($valid);
     }
 }
