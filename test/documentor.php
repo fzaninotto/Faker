18ffb0ca81865d9190691e20645be49e873f54ce
Andreas Möller <am@localheinz.com>
2020-12-21 20:19:55 +0100

    Enhancement: Enable semicolon_after_instruction fixer (#211)
    * Enhancement: Enable semicolon_after_instruction fixer
    
    * Fix: Run 'make cs'

--- test/documentor.php
+++ test/documentor.php
@@ -7,10 +7,10 @@ $documentor = new Faker\Documentor($generator);
 ?>
 <?php foreach ($documentor->getFormatters() as $provider => $formatters): ?>
 
-### `<?php echo $provider ?>`
+### `<?php echo $provider; ?>`
 
 <?php foreach ($formatters as $formatter => $example): ?>
-    <?php echo str_pad($formatter, 23) ?><?php if ($example): ?> // <?php echo $example ?> <?php endif; ?>
+    <?php echo str_pad($formatter, 23); ?><?php if ($example): ?> // <?php echo $example; ?> <?php endif; ?>
 
 <?php endforeach; ?>
 <?php endforeach;
--- test/test.php
+++ test/test.php
@@ -7,29 +7,29 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 <contacts>
 <?php for ($i=0; $i < 10; ++$i): ?>
-  <contact firstName="<?php echo $faker->firstName ?>" lastName="<?php echo $faker->lastName ?>" email="<?php echo $faker->email ?>" >
-    <phone number="<?php echo $faker->phoneNumber ?>"/>
+  <contact firstName="<?php echo $faker->firstName; ?>" lastName="<?php echo $faker->lastName; ?>" email="<?php echo $faker->email; ?>" >
+    <phone number="<?php echo $faker->phoneNumber; ?>"/>
 <?php if ($faker->boolean(25)): ?>
-    <birth date="<?php echo $faker->dateTimeThisCentury->format('Y-m-d') ?>" place="<?php echo $faker->city ?>"/>
+    <birth date="<?php echo $faker->dateTimeThisCentury->format('Y-m-d'); ?>" place="<?php echo $faker->city; ?>"/>
 <?php endif; ?>
     <address>
-      <street><?php echo $faker->streetAddress ?></street>
-      <city><?php echo $faker->city ?></city>
-      <postcode><?php echo $faker->postcode ?></postcode>
-      <state><?php echo $faker->state ?></state>
+      <street><?php echo $faker->streetAddress; ?></street>
+      <city><?php echo $faker->city; ?></city>
+      <postcode><?php echo $faker->postcode; ?></postcode>
+      <state><?php echo $faker->state; ?></state>
     </address>
-    <company name="<?php echo $faker->company ?>" catchPhrase="<?php echo $faker->catchPhrase ?>">
+    <company name="<?php echo $faker->company; ?>" catchPhrase="<?php echo $faker->catchPhrase; ?>">
 <?php if ($faker->boolean(33)): ?>
-      <offer><?php echo $faker->bs ?></offer>
+      <offer><?php echo $faker->bs; ?></offer>
 <?php endif; ?>
 <?php if ($faker->boolean(33)): ?>
-      <director name="<?php echo $faker->name ?>" />
+      <director name="<?php echo $faker->name; ?>" />
 <?php endif; ?>
     </company>
 <?php if ($faker->boolean(15)): ?>
     <details>
 <![CDATA[
-<?php echo $faker->text(400) ?>
+<?php echo $faker->text(400); ?>
 ]]>
     </details>
 <?php endif; ?>
