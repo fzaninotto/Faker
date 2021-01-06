d4262953d04e5c07e7766bc78ba353dd2d82d3e3
Travis Weston <anubisthejackle@users.noreply.github.com>
2021-01-06 06:51:40 -0500

    [Core] Improve checks in blood tests (#245)
    * Improve checks in blood tests
    
    * run make cs
    
    * Update loop counts to bare minimum to make tests pass
    
    * Update blood tests to use UniqueGenerator
    
    * Fix: Run 'make cs'
    
    Co-authored-by: Andreas Möller <am@localheinz.com>

--- test/Faker/Core/BloodTest.php
+++ test/Faker/Core/BloodTest.php
@@ -5,21 +5,69 @@ declare(strict_types=1);
 namespace Faker\Test\Core;
 
 use Faker\Test\TestCase;
+use Faker\UniqueGenerator;
 
 final class BloodTest extends TestCase
 {
     public function testBloodType(): void
     {
-        self::assertContains($this->faker->bloodType(), ['A', 'AB', 'B', 'O']);
+        $expected = [
+            'A',
+            'AB',
+            'B',
+            'O',
+        ];
+
+        $faker = new UniqueGenerator($this->faker);
+
+        $generated = [];
+
+        for ($i = 0; $i < count($expected); ++$i) {
+            $generated[] = $faker->bloodType();
+        }
+
+        self::assertEqualsCanonicalizing($expected, $generated);
     }
 
     public function testBloodRh(): void
     {
-        self::assertContains($this->faker->bloodRh(), ['+', '-']);
+        $expected = [
+            '+',
+            '-',
+        ];
+
+        $faker = new UniqueGenerator($this->faker);
+
+        $generated = [];
+
+        for ($i = 0; $i < count($expected); ++$i) {
+            $generated[] = $faker->bloodRh();
+        }
+
+        self::assertEqualsCanonicalizing($expected, $generated);
     }
 
     public function testBloodGroup(): void
     {
-        self::assertContains($this->faker->bloodGroup(), ['A+', 'A-', 'AB+', 'AB-', 'B+', 'B-', 'O+', 'O-']);
+        $expected = [
+            'A+',
+            'A-',
+            'AB+',
+            'AB-',
+            'B+',
+            'B-',
+            'O+',
+            'O-',
+        ];
+
+        $faker = new UniqueGenerator($this->faker);
+
+        $generated = [];
+
+        for ($i = 0; $i < count($expected); ++$i) {
+            $generated[] = $faker->bloodGroup();
+        }
+
+        self::assertEqualsCanonicalizing($expected, $generated);
     }
 }
