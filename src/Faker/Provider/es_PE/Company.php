32e8cc497f9b6fd8ebd4e3f47679e45025b004ee
Christian QH <Oxicode@users.noreply.github.com>
2021-01-06 11:27:02 -0500

    [es_PE] Add RUC (#244)
    * add Quispe at lastName
    
    * add RUC at Perú
    
    * exec "make cs"
    
    * type as bool and English variable names.
    
    * return type string
    
    * Update src/Faker/Provider/es_PE/Person.php
    
    :/
    
    Co-authored-by: Bram Ceulemans <bramceulemans@me.com>
    
    * Update src/Faker/Provider/es_PE/Company.php
    
    Co-authored-by: Bram Ceulemans <bramceulemans@me.com>
    
    Co-authored-by: Oxicode <juan.quispe@upc.pe>
    Co-authored-by: Bram Ceulemans <bramceulemans@me.com>

--- src/Faker/Provider/es_PE/Company.php
+++ src/Faker/Provider/es_PE/Company.php
@@ -65,4 +65,24 @@ class Company extends \Faker\Provider\Company
 
         return implode(' ', $result);
     }
+
+    /**
+     * Generate a REG. UNICO DE CONTRIBUYENTES (RUC) number
+     *
+     * Doesn't include a checksum, as peruvians commonly use only the first
+     * 8 digits.
+     *
+     * @example '10164090581'
+     * @example '20105895410'
+     *
+     * @param bool $isNaturalPerson If set to false or empty, RUC starts with 20 and otherwise with 10
+     *
+     * @see http://www2.sunat.gob.pe/pdt/pdtModulos/independientes/p695/TipoDoc.htm
+     */
+    public static function ruc(bool $isNaturalPerson = false): string
+    {
+        $prefix = $isNaturalPerson ? '10' : '20';
+
+        return $prefix . static::numerify('#########');
+    }
 }
---
+++ test/Faker/Provider/es_PE/CompanyTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Faker\Test\Provider\es_PE;
+
+use Faker\Provider\es_PE\Company;
+use Faker\Test\TestCase;
+
+/**
+ * @group legacy
+ */
+final class CompanyTest extends TestCase
+{
+    public function testGenerateValidRuc()
+    {
+        $companyRUC = $this->faker->ruc();
+
+        self::assertEquals(11, strlen($companyRUC));
+        self::assertMatchesRegularExpression('/^([1|2])0\d{9}$/', $companyRUC);
+    }
+
+    public function testGenerateValidRucPersonaNatural()
+    {
+        $companyRUC = $this->faker->ruc(true);
+
+        self::assertMatchesRegularExpression('/^10\d{9}$/', $companyRUC);
+    }
+
+    public function testGenerateValidRucPersonaJuridica()
+    {
+        $companyRUC = $this->faker->ruc(false);
+
+        self::assertMatchesRegularExpression('/^20\d{9}$/', $companyRUC);
+    }
+
+    protected function getProviders(): iterable
+    {
+        yield new Company($this->faker);
+    }
+}
