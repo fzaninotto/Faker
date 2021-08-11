3647f54490175f210e6246d1ee0a19c186757cf7
Eduardo França <edox86@gmail.com>
2021-08-11 15:03:52 -0400

    Text provider for Brazilian Portuguese (pt_BR locale) (#355)

---
+++ test/Faker/Provider/pt_BR/TextTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Faker\Test\Provider\pt_BR;
+
+use Faker\Test\TestCase;
+
+/**
+ * @group legacy
+ */
+final class TextTest extends TestCase
+{
+    private $textClass;
+
+    protected function setUp(): void
+    {
+        $this->textClass = new \ReflectionClass('Faker\Provider\pt_BR\Text');
+    }
+
+    protected function getMethod($name)
+    {
+        $method = $this->textClass->getMethod($name);
+
+        $method->setAccessible(true);
+
+        return $method;
+    }
+
+    public function testItShouldAppendEndPunctToTheEndOfString()
+    {
+        self::assertSame(
+            'Uma noite destas.',
+            $this->getMethod('appendEnd')->invokeArgs(null, ['Uma noite destas, '])
+        );
+
+        self::assertSame(
+            'Uma noite destas.',
+            $this->getMethod('appendEnd')->invokeArgs(null, ['Uma noite destas—   '])
+        );
+
+        self::assertSame(
+            'Uma noite destas.',
+            $this->getMethod('appendEnd')->invokeArgs(null, ['Uma noite destas,'])
+        );
+
+        self::assertSame(
+            'Uma noite destas!.',
+            $this->getMethod('appendEnd')->invokeArgs(null, ['Uma noite destas! '])
+        );
+
+        self::assertSame(
+            'Uma noite destas.',
+            $this->getMethod('appendEnd')->invokeArgs(null, ['Uma noite destas: '])
+        );
+
+        self::assertSame(
+            'Uma noite destas.',
+            $this->getMethod('appendEnd')->invokeArgs(null, ['Uma noite destas; '])
+        );
+    }
+}
