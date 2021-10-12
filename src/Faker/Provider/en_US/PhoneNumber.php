5aa3bfe7e10c62c29e4a03daacf986b7bd9267c1
Michael Newton <miken32@gmail.com>
2021-10-12 00:19:54 -0600

    Proper format for E.164 numbers (#380)

--- src/Faker/Provider/en_US/PhoneNumber.php
+++ src/Faker/Provider/en_US/PhoneNumber.php
@@ -59,7 +59,7 @@ class PhoneNumber extends \Faker\Provider\PhoneNumber
     ];
 
     protected static $e164Formats = [
-        '+1{{areaCode}}#######',
+        '+1{{areaCode}}{{exchangeCode}}####',
     ];
 
     /**
