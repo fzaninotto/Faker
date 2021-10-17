d3c0752e756a387214c5b942cb7c5231d9bc8b64
Mike Rockétt <mike@rockett.pw>
2021-10-17 08:58:46 +0200

    updates to en_za/internet (#383)

--- src/Faker/Provider/en_ZA/Internet.php
+++ src/Faker/Provider/en_ZA/Internet.php
@@ -7,12 +7,17 @@ class Internet extends \Faker\Provider\Internet
     protected static $freeEmailDomain = ['gmail.com', 'yahoo.com', 'hotmail.com', 'webmail.co.za', 'vodamail.co.za'];
 
     /**
+     * An array of South African TLDs.
+     *
      * @see https://en.wikipedia.org/wiki/.za
+     * @see https://en.wikipedia.org/wiki/List_of_Internet_top-level_domains#Africa
+     *
+     * @var array
      */
     protected static $tld = [
-        'co.za', 'co.za', 'co.za', 'co.za', 'com', 'com', 'net', 'gov.za', 'ac.za', 'edu.za', 'law.za', 'mil.za',
-        'net.za', 'nom.za', 'org.za', 'school.za', 'ecape.school.za', 'fs.school.za', 'gp.school.za', 'kzn.school.za',
-        'mpm.za', 'ncape.school.za', 'lp.school.za', 'nw.school.za', 'wcape.school.za', 'web.za', 'agric.za', 'nis.za',
-        'grondar.za',
+        'ac.za', 'africa', 'agric.za', 'capetown', 'co.za', 'co.za', 'co.za', 'co.za', 'com', 'com',
+        'durban', 'ecape.school.za', 'edu.za', 'fs.school.za', 'gov.za', 'gp.school.za', 'grondar.za',
+        'joburg', 'kzn.school.za', 'law.za', 'lp.school.za', 'mil.za', 'mpm.za', 'ncape.school.za',
+        'net.za', 'net', 'nis.za', 'nom.za', 'nw.school.za', 'org.za', 'school.za', 'wcape.school.za', 'web.za',
     ];
 }
