4e651e1648483e2740af8e24c67c5f823476c748
Bram <bram@ceulemans.dev>
2022-03-08 23:29:28 +0100

    Set cache directory (#461)

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -13,7 +13,9 @@ $finder = PhpCsFixer\Finder::create()
 
 $config = new PhpCsFixer\Config('faker');
 
+mkdir('.build/php-cs-fixer', 0755, true);
 return $config
+    ->setCacheFile('.build/php-cs-fixer/cache')
     ->setFinder($finder)
     ->setRiskyAllowed(true)
     ->setRules([
