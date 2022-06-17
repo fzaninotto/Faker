d389c7e561e608830911b8a56c45b8e4899f8552
Francescu GAROBY <f.garoby@gmail.com>
2022-06-17 09:06:53 +0200

    run 'mkdir' only if the directory doesn't exist yet (#481)
    * run 'mkdir' only if the directory doesn't exist yet
    
    * codestyle
    
    * Use 'id_dir' instead of 'file_exists'
    
    Co-authored-by: Francescu Garoby <francescu.garoby@docndoc.fr>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -13,7 +13,10 @@ $finder = PhpCsFixer\Finder::create()
 
 $config = new PhpCsFixer\Config('faker');
 
-mkdir('.build/php-cs-fixer', 0755, true);
+if (!is_dir('.build/php-cs-fixer')) {
+    mkdir('.build/php-cs-fixer', 0755, true);
+}
+
 return $config
     ->setCacheFile('.build/php-cs-fixer/cache')
     ->setFinder($finder)
