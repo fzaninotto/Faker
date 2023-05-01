ddb039044866c9573e0180d56b077db507cea647
Andreas Möller <am@localheinz.com>
2023-05-01 11:52:40 +0200

    Fix: Clean up `randomElements()` and `randomElement()` (#639)
    * Fix: Use iterator_to_array()
    
    * Fix: Do not assign multiple values in one go
    
    * Fix: Whitespace
    
    * Fix: Rename variable
    
    * Fix: Rename variable
    
    * Fix: Count elements
    
    * Fix: Slide statement
    
    * Fix: Rename variable
    
    * Fix: Rename variable
    
    * Enhancement: Extract variable
    
    * Fix: Rename variable
    
    * Fix: Rename variable
    
    * Fix: Rename variable
    
    * Fix: Rename variable
    
    * Fix: Slide statement
    
    * Fix: DocBlock
    
    * Fix: Whitespace
    
    * Fix: Rename variable
    
    * Fix: Return early
    
    * Enhancement: Use iterator_to_array()
    
    * Fix: DocBlock
    
    * Fix: Do not reassign value to argument

--- src/Faker/Provider/Base.php
+++ src/Faker/Provider/Base.php
@@ -179,9 +179,9 @@ class Base
     /**
      * Returns randomly ordered subsequence of $count elements from a provided array
      *
-     * @param array $array           Array to take elements from. Defaults to a-c
-     * @param int   $count           Number of elements to take.
-     * @param bool  $allowDuplicates Allow elements to be picked several times. Defaults to false
+     * @param array|\Traversable $array           Array to take elements from. Defaults to a-c
+     * @param int                $count           Number of elements to take.
+     * @param bool               $allowDuplicates Allow elements to be picked several times. Defaults to false
      *
      * @throws \LengthException When requesting more elements than provided
      *
@@ -189,57 +189,70 @@ class Base
      */
     public static function randomElements($array = ['a', 'b', 'c'], $count = 1, $allowDuplicates = false)
     {
-        $traversables = [];
-        $arr = $array;
+        $elements = $array;
 
         if ($array instanceof \Traversable) {
-            foreach ($array as $element) {
-                $traversables[] = $element;
-            }
-            $arr = $traversables;
+            $elements = \iterator_to_array($array, false);
         }
 
-        $allKeys = array_keys($arr);
-        $numKeys = count($allKeys);
+        $numberOfElements = count($elements);
 
-        if (!$allowDuplicates && $numKeys < $count) {
-            throw new \LengthException(sprintf('Cannot get %d elements, only %d in array', $count, $numKeys));
+        if (!$allowDuplicates && $numberOfElements < $count) {
+            throw new \LengthException(sprintf(
+                'Cannot get %d elements, only %d in array',
+                $count,
+                $numberOfElements,
+            ));
         }
 
-        $highKey = $numKeys - 1;
-        $keys = $elements = [];
-        $numElements = 0;
+        $randomElements = [];
 
-        while ($numElements < $count) {
-            $num = mt_rand(0, $highKey);
+        $keys = array_keys($elements);
+        $maxIndex = $numberOfElements - 1;
+        $elementHasBeenSelectedAlready = [];
+        $numberOfRandomElements = 0;
+
+        while ($numberOfRandomElements < $count) {
+            $index = mt_rand(0, $maxIndex);
 
             if (!$allowDuplicates) {
-                if (isset($keys[$num])) {
+                if (isset($elementHasBeenSelectedAlready[$index])) {
                     continue;
                 }
-                $keys[$num] = true;
+
+                $elementHasBeenSelectedAlready[$index] = true;
             }
 
-            $elements[] = $arr[$allKeys[$num]];
-            ++$numElements;
+            $key = $keys[$index];
+
+            $randomElements[] = $elements[$key];
+
+            ++$numberOfRandomElements;
         }
 
-        return $elements;
+        return $randomElements;
     }
 
     /**
      * Returns a random element from a passed array
      *
-     * @param array $array
+     * @param array|\Traversable $array
      */
     public static function randomElement($array = ['a', 'b', 'c'])
     {
-        if (!$array || ($array instanceof \Traversable && !count($array))) {
+        $elements = $array;
+
+        if ($array instanceof \Traversable) {
+            $elements = iterator_to_array($array, false);
+        }
+
+        if ($elements === []) {
             return null;
         }
-        $elements = static::randomElements($array, 1);
 
-        return $elements[0];
+        $randomElements = static::randomElements($elements, 1);
+
+        return $randomElements[0];
     }
 
     /**
